216169534022fef57bcea26e81c3013c8a0e341e
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-02-17 21:19:32 +0100

    FileLoader: new interface
    Provide an interface that allows to load a file. Any kind of
    strategy that is intended to look up for files and load them
    might use this interface for its consumers.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/util/FileLoader.h
@@ -0,0 +1,21 @@
+#ifndef BEEEON_FILE_LOADER_H
+#define BEEEON_FILE_LOADER_H
+
+namespace Poco {
+
+class File;
+
+}
+
+namespace BeeeOn {
+
+class FileLoader {
+public:
+	virtual ~FileLoader();
+
+	virtual void load(const Poco::File &file) = 0;
+};
+
+}
+
+#endif
