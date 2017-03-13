BeeeOn/base

commit 68d17007c138ff2900e61886a37ca297a813142b

DIApplicationConfigurationLoader: new implementation
Specialize ApplicationConfigurationLoader for dependency-injection.
We do not pass XML files into the Poco::Util::Application via
the loadConfiguration() method. Instead, load the XML files and
merge them into one document. Finally, the merged document can
be inserted into the configuration.

There was an issue with the LayeredConfiguration approach when
loading XML files (and especially the DI configurations). The
LayeredConfiguration overlays each configuration on top of the
previous ones. The XML tags are represented as keys in form
e.g. instance[1], instance[2], ... However, each XML file is
always numbered from 1. So instances from new XML files hides
some from the previous ones.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DIApplicationConfigurationLoader.cpp
@@ -1,0 +1,65 @@
+#include <Poco/File.h>
+#include <Poco/FileStream.h>
+#include <Poco/Path.h>
+#include <Poco/Logger.h>
+#include <Poco/Util/Application.h>
+#include <Poco/Util/XMLConfiguration.h>
+#include <Poco/Util/LayeredConfiguration.h>
+
+#include "di/DIApplicationConfigurationLoader.h"
+#include "di/DIXmlLoader.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Util;
+using namespace BeeeOn;
+
+DIApplicationConfigurationLoader::DIApplicationConfigurationLoader(Application &app):
+	ApplicationConfigurationLoader(app)
+{
+}
+
+DIApplicationConfigurationLoader::~DIApplicationConfigurationLoader()
+{
+}
+
+void DIApplicationConfigurationLoader::load(const File &file)
+{
+	const Path path(file.path());
+
+	if (path.getExtension() != "xml")
+		ApplicationConfigurationLoader::load(file);
+	else
+		loadXml(file);
+}
+
+void DIApplicationConfigurationLoader::loadXml(const File &file)
+{
+	try {
+		FileInputStream fin(file.path());
+
+		if (logger().debug()) {
+			logger().debug("loading XML configuration " + file.path(),
+					__FILE__, __LINE__);
+		}
+
+		m_loader.load(fin);
+	} catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+		e.rethrow();
+	}
+}
+
+void DIApplicationConfigurationLoader::finished()
+{
+	LayeredConfiguration &config = application().config();
+	AutoPtr<XMLConfiguration> xml(new XMLConfiguration);
+
+	if (logger().debug()) {
+		logger().debug("loading merged XML configuration",
+				__FILE__, __LINE__);
+	}
+
+	xml->load(m_loader.document());
+	config.add(xml);
+}

src/CMakeLists.txt
@@ -15,6 +15,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIWrapper.cpp
 	${PROJECT_SOURCE_DIR}/di/DIDaemon.cpp
 	${PROJECT_SOURCE_DIR}/di/DIXmlLoader.cpp
+	${PROJECT_SOURCE_DIR}/di/DIApplicationConfigurationLoader.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp

src/di/DIApplicationConfigurationLoader.h
@@ -1,0 +1,26 @@
+#ifndef BEEEON_DI_APPLICATION_CONFIGURATION_LOADER_H
+#define BEEEON_DI_APPLICATION_CONFIGURATION_LOADER_H
+
+#include "di/DIXmlLoader.h"
+#include "util/ApplicationConfigurationLoader.h"
+
+namespace BeeeOn {
+
+class DIApplicationConfigurationLoader : public ApplicationConfigurationLoader {
+public:
+	DIApplicationConfigurationLoader(Poco::Util::Application &app);
+	virtual ~DIApplicationConfigurationLoader();
+
+	void load(const Poco::File &file) override;
+	void finished() override;
+
+protected:
+	void loadXml(const Poco::File &file);
+
+private:
+	DIXmlLoader m_loader;
+};
+
+}
+
+#endif

commit f40269539d6bf478707d1e01b45ac9ff4d876e14

DIApplicationConfigurationLoader: fix null document access
The DIXmlLoader may return NULL instead of a valid document. Currently,
the application fails with the following message:

Null pointer: pDocument in file "src/XMLConfiguration.cpp", line 162

when no XML file is loaded during the configuration lookup.

Fixes: 68d17007c138 ("DIApplicationConfigurationLoader: new implementation")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DIApplicationConfigurationLoader.cpp
@@ -5,6 +5,7 @@
 #include <Poco/Util/Application.h>
 #include <Poco/Util/XMLConfiguration.h>
 #include <Poco/Util/LayeredConfiguration.h>
+#include <Poco/DOM/Document.h>
 
 #include "di/DIApplicationConfigurationLoader.h"
 #include "di/DIXmlLoader.h"
@@ -12,6 +13,7 @@
 using namespace std;
 using namespace Poco;
 using namespace Poco::Util;
+using namespace Poco::XML;
 using namespace BeeeOn;
 
 DIApplicationConfigurationLoader::DIApplicationConfigurationLoader(Application &app):
@@ -55,11 +57,14 @@ void DIApplicationConfigurationLoader::finished()
 	LayeredConfiguration &config = application().config();
 	AutoPtr<XMLConfiguration> xml(new XMLConfiguration);
 
-	if (logger().debug()) {
-		logger().debug("loading merged XML configuration",
-				__FILE__, __LINE__);
-	}
+	const Document *doc = m_loader.document();
+	if (doc != NULL) {
+		if (logger().debug()) {
+			logger().debug("loading merged XML configuration",
+					__FILE__, __LINE__);
+		}
 
-	xml->load(m_loader.document());
-	config.add(xml);
+		xml->load(m_loader.document());
+		config.add(xml);
+	}
 }