BeeeOn/base

commit 3b43b165e44e6cb0e4dc2037b92c0cb6d03ca856

JsonUtil: new class
The parser simplifies JSON parsing from a string. The template extract
enables to choose a value (int, double or string data type)
on the basis of a defined key.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/JsonUtil.h
@@ -1,0 +1,39 @@
+#ifndef BEEEON_JSON_UTIL_H
+#define BEEEON_JSON_UTIL_H
+
+#include <string>
+
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Parser.h>
+
+namespace BeeeOn {
+
+class JsonUtil {
+public:
+	/*
+	 * Get string from JSON Object.
+	 * @param jsonObject Object which contains parsed JSON message.
+	 * @parma &key JSON attribute which contains value
+	 * @return searched value
+	 */
+	template <typename T>
+	static T extract(
+		Poco::JSON::Object::Ptr jsonObject, const std::string &key)
+	{
+		Poco::Dynamic::Var item;
+		item = jsonObject->get(key);
+
+		return item.convert<T>();
+	}
+
+	/*
+	 * It parses message and return JSON Object.
+	 * @param &data JSON message string
+	 * @return JSON Object
+	 */
+	static Poco::JSON::Object::Ptr parse(const std::string &data);
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -7,6 +7,7 @@ find_library (POCO_CRYPTO PocoCrypto)
 find_library (POCO_UTIL PocoUtil)
 find_library (POCO_NET PocoNet)
 find_library (POCO_XML PocoXML)
+find_library (POCO_JSON PocoJSON)
 
 add_definitions(-std=c++11)
 
@@ -48,6 +49,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/ConfigurationExplorer.cpp
 	${PROJECT_SOURCE_DIR}/util/AutoConfigurationExplorer.cpp
 	${PROJECT_SOURCE_DIR}/util/WithTrace.cpp
+	${PROJECT_SOURCE_DIR}/util/JsonUtil.cpp
 )
 
 include_directories(

src/util/JsonUtil.cpp
@@ -1,0 +1,14 @@
+#include "JsonUtil.h"
+
+using namespace std;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+
+Object::Ptr JsonUtil::parse(const string &data)
+{
+	Parser jsonParser;
+	jsonParser.parse(data);
+	Poco::Dynamic::Var parsedJsonResult = jsonParser.result();
+
+	return parsedJsonResult.extract<Object::Ptr>();
+}

commit b29c7ee654f99767dd686ffdf4cb14c38d3b2118

JsonUtil: fix handling of null for double
The JSON specification (see json.org) does not define format for
INF or Not-a-Number values. Thus, when parsing a double, the null
value might be used to represent those. Define that with JsonUtil,
the null means NaN. Provide a template specialization for double
to handle the null value and return NaN as expected.

Fixes: 3b43b165e44e ("JsonUtil: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/JsonUtil.h
@@ -1,6 +1,7 @@
 #ifndef BEEEON_JSON_UTIL_H
 #define BEEEON_JSON_UTIL_H
 
+#include <cmath>
 #include <string>
 
 #include <Poco/JSON/JSON.h>
@@ -33,6 +34,16 @@ public:
 	 */
 	static Poco::JSON::Object::Ptr parse(const std::string &data);
 };
+
+template <>
+inline double JsonUtil::extract<double>(
+	Poco::JSON::Object::Ptr jsonObject, const std::string &key)
+{
+	if (jsonObject->isNull(key))
+		return std::nan("");
+
+	return jsonObject->get(key).convert<double>();
+}
 
 }
 

test/util/JsonUtilTest.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <string>
 
 #include <cppunit/extensions/HelperMacros.h>
@@ -14,6 +15,7 @@ class JsonUtilTest : public CppUnit::TestFixture {
 public:
 	void testExtractString();
 	void testExtractDouble();
+	void testExtractNullDouble();
 	void testExtractInt();
 	void testExtractInvalidIntValue();
 
@@ -21,6 +23,7 @@ private:
 	CPPUNIT_TEST_SUITE(JsonUtilTest);
 	CPPUNIT_TEST(testExtractString);
 	CPPUNIT_TEST(testExtractDouble);
+	CPPUNIT_TEST(testExtractNullDouble);
 	CPPUNIT_TEST(testExtractInt);
 	CPPUNIT_TEST(testExtractInvalidIntValue);
 	CPPUNIT_TEST_SUITE_END();
@@ -55,6 +58,22 @@ void JsonUtilTest::testExtractDouble()
 	jsonObject = JsonUtil::parse(R"({"value": 11.5})");
 	value = JsonUtil::extract<double>(jsonObject, "value");
 	CPPUNIT_ASSERT(value == 11.5);
+}
+
+/**
+ * In JSON, the values NaN and INF are not defined. Thus, they might
+ * be implemented via passing null. Because, we cannot determine whether
+ * the original value was either NaN or INF, define that null means
+ * NaN.
+ */
+void JsonUtilTest::testExtractNullDouble()
+{
+	Object::Ptr jsonObject;
+	double value;
+
+	jsonObject = JsonUtil::parse(R"({"value": null})");
+	value = JsonUtil::extract<double>(jsonObject, "value");
+	CPPUNIT_ASSERT(std::isnan(value));
 }
 
 /*