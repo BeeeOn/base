BeeeOn/base

commit 571a13fe336ce63e199ad95e7f74b7a040ed75b9

Enum: new template class
To represent "clever" enums in C++, we have to provide some wrapping
classes. This approach tries to minimalize the developer's effort and
reduce coding overhead.

The Enum template provides static methods parse() and fromRaw() to
construct an instance of an Enum. Internally, each value is represented
by a pointer (map iterator) to std::pair. In this way, we can quickly
convert the Enum instance into its raw or string value.

Both parse() and fromRaw() methods work with O(log2(n)) complexity.
To enable this, there must be 2 conversion tables Raw -> string and
string -> Raw.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

src/util/Enum.h
@@ -1,0 +1,211 @@
+#ifndef BEEEON_ENUM_H
+#define BEEEON_ENUM_H
+
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include <map>
+#include <string>
+
+namespace BeeeOn {
+
+/**
+ * Provide enum-specific helper data types.
+ */
+template <typename Raw>
+struct EnumHelper {
+	/**
+	 * ValueMap that each Enum must specify.
+	 */
+	typedef std::map<Raw, std::string> ValueMap;
+	/**
+	 * A representation of Enum value. It points to a ValueMap entry
+	 * to access both the Raw value and its name.
+	 */
+	typedef typename ValueMap::const_iterator Value;
+	/**
+	 * RawMap to lookup Enum instance by Raw value.
+	 */
+	typedef std::map<Raw, Value> RawMap;
+	/**
+	 * NamesMap to lookup Enum instance by name.
+	 */
+	typedef std::map<std::string, Value> NamesMap;
+
+	static RawMap initRawMap(const ValueMap &valueMap)
+	{
+		RawMap rawMap;
+
+		for (Value it = valueMap.begin(); it != valueMap.end(); ++it) {
+			auto result = rawMap.insert(std::make_pair(it->first, it));
+			if (!result.second)
+				throw Poco::ExistsException(
+					"duplicate raw value " + std::to_string(it->first));
+		}
+
+		return rawMap;
+	}
+
+	static NamesMap initNamesMap(const ValueMap &valueMap)
+	{
+		NamesMap namesMap;
+
+		for (Value it = valueMap.begin(); it != valueMap.end(); ++it) {
+			auto result = namesMap.insert(std::make_pair(it->second, it));
+			if (!result.second)
+				throw Poco::ExistsException(
+					"duplicate string value " + it->second);
+		}
+
+		return namesMap;
+	}
+};
+
+/**
+ * Abstract Enum type to simplify defining "clever" enums.
+ * A "clever" enum can be deserialized (parsed) from string
+ * and serialized back (toString).
+ *
+ * Base is the user defined class containing an embedded enum
+ * called Raw. Example:
+ *
+ *   class TestEnum {
+ *   public:
+ *      enum Raw {
+ *         TEST_X0,
+ *         TEST_X1,
+ *         ...
+ *      }
+ *   };
+ *
+ *   typedef Enum<TestEnum> Test;
+ *
+ * Use Test class as the target enum's type.
+ */
+template <typename Base, typename RawType = typename Base::Raw>
+class Enum : public Base {
+public:
+	typedef RawType Raw;
+
+	typedef typename EnumHelper<Raw>::ValueMap ValueMap;
+	typedef typename EnumHelper<Raw>::Value Value;
+
+private:
+	Enum(const Value &value):
+		m_value(value)
+	{
+	}
+
+	Enum &operator =(const Enum &) = delete;
+
+protected:
+	static typename EnumHelper<Raw>::RawMap &rawMap()
+	{
+		static typename EnumHelper<Raw>::RawMap rawMap(
+			EnumHelper<Raw>::initRawMap(Base::valueMap())
+		);
+
+		return rawMap;
+	}
+
+	static typename EnumHelper<Raw>::NamesMap &namesMap()
+	{
+		static typename EnumHelper<Raw>::NamesMap namesMap(
+			EnumHelper<Raw>::initNamesMap(Base::valueMap())
+		);
+
+		return namesMap;
+	}
+
+public:
+	Raw raw() const
+	{
+		return m_value->first;
+	}
+
+	const std::string &toString() const
+	{
+		return m_value->second;
+	}
+
+	static Enum<Base, Raw> parse(const std::string &input)
+	{
+		auto it = namesMap().find(input);
+		if (it == namesMap().end()) {
+			throw Poco::InvalidArgumentException(
+				"failed to parse '" + input + "'");
+		}
+
+		return Enum<Base, Raw>(it->second);
+	}
+
+	static Enum<Base, Raw> fromRaw(const Raw &raw)
+	{
+		auto it = rawMap().find(raw);
+		if (it == rawMap().end()) {
+			throw Poco::InvalidArgumentException(
+				"unrecognized raw value " + std::to_string(raw));
+		}
+
+		return Enum<Base, Raw>(it->second);
+	}
+
+	static Enum<Base, Raw> fromRaw(const unsigned int raw)
+	{
+		return fromRaw(Raw(raw));
+	}
+
+	operator Raw() const
+	{
+		return raw();
+	}
+
+	bool operator ==(const Enum &other) const
+	{
+		return m_value == other.m_value;
+	}
+
+	bool operator !=(const Enum &other) const
+	{
+		return m_value != other.m_value;
+	}
+
+	bool operator <(const Enum &other) const
+	{
+		return m_value->first < other.m_value->first;
+	}
+
+	bool operator >(const Enum &other) const
+	{
+		return m_value->first > other.m_value->first;
+	}
+
+	bool operator <=(const Enum &other) const
+	{
+		return m_value->first <= other.m_value->first;
+	}
+
+	bool operator >=(const Enum &other) const
+	{
+		return m_value->first >= other.m_value->first;
+	}
+
+private:
+	Value m_value;
+};
+
+template <typename Base, typename Raw = typename Base::Raw>
+inline std::string operator +(const std::string &s, const Enum<Base, Raw> &e)
+{
+	return s + e.toString();
+}
+
+template <typename Base, typename Raw = typename Base::Raw>
+inline std::string operator +(const char *s, const Enum<Base, Raw> &e)
+{
+	return s + e.toString();
+}
+
+}
+
+#endif

test/CMakeLists.txt
@@ -24,6 +24,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/model/DeviceIDTest.cpp
 	${PROJECT_SOURCE_DIR}/util/BacktraceTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp
+	${PROJECT_SOURCE_DIR}/util/EnumTest.cpp
 	${PROJECT_SOURCE_DIR}/util/Base64Test.cpp
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParserTest.cpp
 	${PROJECT_SOURCE_DIR}/util/TypesSAXHandlerTest.cpp

test/util/EnumTest.cpp
@@ -1,0 +1,114 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "util/Enum.h"
+#include "cppunit/BetterAssert.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class EnumTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(EnumTest);
+	CPPUNIT_TEST(testParse);
+	CPPUNIT_TEST(testFromRaw);
+	CPPUNIT_TEST(testCompare);
+	CPPUNIT_TEST(testStringConcat);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testParse();
+	void testFromRaw();
+	void testCompare();
+	void testStringConcat();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(EnumTest);
+
+struct TestTypeEnum {
+	enum Raw {
+		TEST_X0 = 0,
+		TEST_X1,
+		TEST_X2,
+		TEST_X3
+	};
+
+	static EnumHelper<Raw>::ValueMap &valueMap();
+};
+
+EnumHelper<TestTypeEnum::Raw>::ValueMap &TestTypeEnum::valueMap()
+{
+	static EnumHelper<TestTypeEnum::Raw>::ValueMap valueMap = {
+		{TestTypeEnum::TEST_X0, "x0"},
+		{TestTypeEnum::TEST_X1, "x1"},
+		{TestTypeEnum::TEST_X2, "x2"},
+		{TestTypeEnum::TEST_X3, "x3"},
+	};
+
+	return valueMap;
+}
+
+typedef Enum<TestTypeEnum> TestType;
+
+void EnumTest::testParse()
+{
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X0, TestType::parse("x0"));
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X1, TestType::parse("x1"));
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X2, TestType::parse("x2"));
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X3, TestType::parse("x3"));
+
+	CPPUNIT_ASSERT_THROW(TestType::parse("x4"), InvalidArgumentException);
+}
+
+void EnumTest::testFromRaw()
+{
+	CPPUNIT_ASSERT_EQUAL("x0", TestType::fromRaw(TestType::TEST_X0).toString());
+	CPPUNIT_ASSERT_EQUAL("x1", TestType::fromRaw(TestType::TEST_X1).toString());
+	CPPUNIT_ASSERT_EQUAL("x2", TestType::fromRaw(TestType::TEST_X2).toString());
+	CPPUNIT_ASSERT_EQUAL("x3", TestType::fromRaw(TestType::TEST_X3).toString());
+
+	CPPUNIT_ASSERT_THROW(TestType::fromRaw(353), InvalidArgumentException);
+}
+
+void EnumTest::testCompare()
+{
+	TestType typeA(TestType::fromRaw(TestType::TEST_X0));
+	TestType typeB(TestType::fromRaw(TestType::TEST_X1));
+	TestType typeC(TestType::fromRaw(TestType::TEST_X0));
+
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X0, typeA);
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X1, typeB);
+	CPPUNIT_ASSERT_EQUAL(TestType::TEST_X0, typeC);
+
+	CPPUNIT_ASSERT(!(TestType::TEST_X1 == typeA));
+	CPPUNIT_ASSERT(TestType::TEST_X1 != typeA);
+	CPPUNIT_ASSERT(typeB != TestType::TEST_X0);
+
+	CPPUNIT_ASSERT_EQUAL(typeA, typeA);
+	CPPUNIT_ASSERT_EQUAL(typeA, typeC);
+
+	CPPUNIT_ASSERT(typeA != typeB);
+	CPPUNIT_ASSERT(typeB != typeC);
+
+	CPPUNIT_ASSERT(typeA < typeB);
+	CPPUNIT_ASSERT(typeC < typeB);
+	CPPUNIT_ASSERT(typeB > typeA);
+	CPPUNIT_ASSERT(typeA <= typeB);
+	CPPUNIT_ASSERT(typeA <= typeA);
+	CPPUNIT_ASSERT(typeB >= typeA);
+	CPPUNIT_ASSERT(typeB >= typeB);
+}
+
+void EnumTest::testStringConcat()
+{
+	TestType a(TestType::fromRaw(TestType::TEST_X2));
+	TestType b(TestType::fromRaw(TestType::TEST_X3));
+
+	CPPUNIT_ASSERT_EQUAL("test: x2", std::string("test: ") + a);
+	CPPUNIT_ASSERT_EQUAL("test: x3", std::string("test: ") + b);
+	CPPUNIT_ASSERT_EQUAL("test: x2, x3", std::string("test: ") + a + ", " + b);
+	CPPUNIT_ASSERT_EQUAL("test: x2", "test: " + a);
+	CPPUNIT_ASSERT_EQUAL("test: x3", "test: " + b);
+	CPPUNIT_ASSERT_EQUAL("test: x2, x3", "test: " + a + ", " + b);
+}
+
+}

commit 0dc80d8efe01ba8d31c1df17571bc2a53c1748a1

Enum: allow assign operator
There is finally no reason to deny calling the assign operator.
The Enum internals are copyable.

Fixes: 571a13fe336c ("Enum: new template class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Enum.h
@@ -97,8 +97,6 @@ private:
 	{
 	}
 
-	Enum &operator =(const Enum &) = delete;
-
 protected:
 	static typename EnumHelper<Raw>::RawMap &rawMap()
 	{