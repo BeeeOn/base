8ddf1d2d1d52600b37642af0db0f8cb1a3a0bf06
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-04-18 16:15:16 +0200

    WorkAccess: introduce Work-specific locking mechanism
    Each Work instance can be (from the locking point of view) in one
    of two states:
    
    * IDLE - it exists, it is somewhere (probably scheduled in some
             execution queue) but not executing.
    
    * EXEC - it was scheduled and now it is executing.
    
    The two states are important to consider when locking the work
    efficiently. Each work is always concurrently readable. If there
    is some writer, reading is not possible. This is ensured by the
    common RWLock (readers-writers lock).
    
    Accessing the Work is thus possible only by using some guard class
    derived from WorkAccess: WorkReading or WorkWriting. Both ensures
    proper locking of the work for single or block atomic operations.
    There is however one aspect to consider, the used RWLock is not
    recursive a thus it must not be locked more then once in a single
    thread.
    
    When a work is in the EXEC state, no writer except of the work's
    execution thread can modify the work internals. During execution,
    the work must not be changed from the outer world otherwise the
    results might be undefined. Especially, it is not possible to cancel
    an executing work. The work leaves the execution as EXECUTED, FAILED
    or FINISHED. If somebody would modify the state during its execution,
    this state change will be probably discarded when leaving the execution.
    
    To avoid such situations, there is another lock called execution lock.
    This lock is recursive and it is held during the work's execution via
    the WorkExecution class. A particular WorkRunner ensures to hold this
    lock before a work is executed. The WorkWriting guard first tries to
    lock the execution lock and then it races for the writing lock.
    
    On the contrary, the WorkReading does not lock the execution lock
    because reading of work is always possible unless there is an active
    writer of the RWLock.
    
    Thus, using of WorkWriting and WorkReading is mandatory (and enforced
    by the Work interface) to assure that locking works as expected.
    Also, specialized Work classes (derived from Work) should use those
    locks to make sure that access of their content does not introduce
    race conditions.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/work/Work.h
+++ src/work/Work.h
@@ -5,13 +5,22 @@
 #include <Poco/Timestamp.h>
 #include <Poco/Timespan.h>
 #include <Poco/Nullable.h>
+#include <Poco/Mutex.h>
+#include <Poco/RWLock.h>
 
 #include "model/Entity.h"
 #include "model/GlobalID.h"
 
 namespace BeeeOn {
 
+class WorkExecuting;
+class WorkWriting;
+class WorkAccess;
+
 class Work : public Entity<GlobalID> {
+	friend WorkExecuting;
+	friend WorkWriting;
+	friend WorkAccess;
 public:
 	typedef Poco::SharedPtr<Work> Ptr;
 
@@ -29,15 +38,19 @@ public:
 	virtual ~Work();
 
 	void setState(State state);
+	void setState(State state, const WorkWriting &guard);
 	State state() const;
+	State state(const WorkAccess &guard) const;
 
 	void setPriority(int priority);
+	void setPriority(int priority, const WorkWriting &guard);
 
 	/**
 	 * Work priority. The higher it is the more probable to execute earlier.
 	 * The default priority is 0. Thus negative priorities potentially slows down.
 	 */
 	int priority() const;
+	int priority(const WorkAccess &guard) const;
 
 	/**
 	 * Return when this work is to be activated.
@@ -46,20 +59,31 @@ public:
 	 * it as close to created() as possible.
 	 */
 	Poco::Nullable<Poco::Timestamp> activationTime() const;
+	Poco::Nullable<Poco::Timestamp> activationTime(const WorkAccess &guard) const;
 
 	void setNoSleepDuration();
+	void setNoSleepDuration(const WorkWriting &guard);
 	void setSleepDuration(const Poco::Timespan &duration);
+	void setSleepDuration(const Poco::Timespan &duration, const WorkWriting &guard);
 	void setSleepDuration(const Poco::Nullable<Poco::Timespan> &duration);
+	void setSleepDuration(const Poco::Nullable<Poco::Timespan> &duration, const WorkWriting &guard);
 	Poco::Nullable<Poco::Timespan> sleepDuration() const;
+	Poco::Nullable<Poco::Timespan> sleepDuration(const WorkAccess &guard) const;
 
 	void setCreated(const Poco::Timestamp &created);
+	void setCreated(const Poco::Timestamp &created, const WorkWriting &guard);
 	Poco::Timestamp created() const;
+	Poco::Timestamp created(const WorkAccess &guard) const;
 
 	void setSuspended(const Poco::Timestamp &suspended);
+	void setSuspended(const Poco::Timestamp &suspended, const WorkWriting &guard);
 	Poco::Timestamp suspended() const;
+	Poco::Timestamp suspended(const WorkAccess &guard) const;
 
 	void setFinished(const Poco::Timestamp &finished);
+	void setFinished(const Poco::Timestamp &finished, const WorkWriting &guard);
 	Poco::Timestamp finished() const;
+	Poco::Timestamp finished(const WorkAccess &guard) const;
 
 	static bool timestampValid(const Poco::Timestamp &tstamp)
 	{
@@ -69,6 +93,24 @@ public:
 protected:
 	Poco::Nullable<Poco::Timestamp> activationTimeUnlocked() const;
 
+	/**
+	 * All members must be read for concurrent access with
+	 * multiple readers. The writer is usually the executor.
+	 */
+	Poco::RWLock &accessLock() const
+	{
+		return m_accessLock;
+	}
+
+	/**
+	 * Execution lock prevents other threads to execute
+	 * the same Work instance simultaneously.
+	 */
+	Poco::Mutex &executionLock() const
+	{
+		return m_executionLock;
+	}
+
 private:
 	State m_state;
 	int m_priority;
@@ -76,6 +118,8 @@ private:
 	Poco::Timestamp m_created;
 	Poco::Timestamp m_suspended;
 	Poco::Timestamp m_finished;
+	mutable Poco::Mutex m_executionLock;
+	mutable Poco::RWLock m_accessLock;
 };
 
 typedef Work::ID WorkID;
