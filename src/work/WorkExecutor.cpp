BeeeOn/base

commit 735b747494eef7cb01bf99aadefdab2b0b014036

WorkExecutor: new class
The Work holds information necessary for a task processing.
The actual Work execution routine is implemented by a WorkExecutor.
There is usually a pair (Work, WorkExecutor) which work together.

The WorkExecutor::accepts() tells whether the given WorkExecutor
instance understands the given Work instance. The WorkExecutor
is usually a singleton with no persisting capabilities. It can
however initiate database transactions or access other resources
in the system.

The WorkExecutor is always executed with Work::executionLock() held.
It MUST lock the accessLock() when accessing the Work contents.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/work/WorkExecutor.cpp
@@ -1,0 +1,45 @@
+#include <Poco/Exception.h>
+#include <Poco/SingletonHolder.h>
+#include <Poco/Timespan.h>
+
+#include "work/WorkExecutor.h"
+#include "work/WorkSuspendThrowable.h"
+
+using namespace Poco;
+using namespace BeeeOn;
+
+WorkExecutor::~WorkExecutor()
+{
+}
+
+void WorkExecutor::suspend(Work::Ptr work)
+{
+	throw WorkSuspendForEventThrowable();
+}
+
+void WorkExecutor::suspend(
+		Work::Ptr work,
+		const Timespan &duration)
+{
+	throw WorkSuspendThrowable(duration);
+}
+
+NullWorkExecutor::NullWorkExecutor()
+{
+}
+
+bool NullWorkExecutor::accepts(const Work::Ptr work) const
+{
+	throw NotImplementedException(__func__);
+}
+
+void NullWorkExecutor::execute(Work::Ptr work)
+{
+	throw NotImplementedException(__func__);
+}
+
+WorkExecutor &NullWorkExecutor::instance()
+{
+	static SingletonHolder<NullWorkExecutor> singleton;
+	return *singleton.get();
+}

src/CMakeLists.txt
@@ -60,6 +60,7 @@ file(GLOB SOURCES
 file(GLOB WORK_SOURCES
 	${PROJECT_SOURCE_DIR}/work/Work.cpp
 	${PROJECT_SOURCE_DIR}/work/WorkAccess.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkExecutor.cpp
 	${PROJECT_SOURCE_DIR}/work/WorkSuspendThrowable.cpp
 )
 

src/work/WorkExecutor.h
@@ -1,0 +1,49 @@
+#ifndef BEEEON_WORK_EXECUTOR_H
+#define BEEEON_WORK_EXECUTOR_H
+
+#include "work/Work.h"
+
+namespace Poco {
+
+class Timespan;
+
+}
+
+namespace BeeeOn {
+
+class WorkRunner;
+class WorkScheduler;
+
+class WorkExecutor {
+public:
+	virtual ~WorkExecutor();
+
+	/**
+	 * Tells whether the executor implementation can
+	 * handle the given work.
+	 */
+	virtual bool accepts(const Work::Ptr work) const = 0;
+
+	/**
+	 * Execute the given work.
+	 */
+	virtual void execute(Work::Ptr work) = 0;
+
+protected:
+	void suspend(Work::Ptr work);
+	void suspend(Work::Ptr work, const Poco::Timespan &duration);
+};
+
+class NullWorkExecutor : public WorkExecutor {
+public:
+	NullWorkExecutor();
+
+	bool accepts(const Work::Ptr work) const override;
+	void execute(Work::Ptr work) override;
+
+	static WorkExecutor &instance();
+};
+
+}
+
+#endif

commit def97fc6872c375efd17565e8284bd779c3e5aa2

work: fix unused parameters
Fixes: 735b7474 ("WorkExecutor: new class")
Fixes: 012352cd ("WorkRunner: new abstract class")
Fixes: bc6af2d5 ("WorkScheduler: new interface")
Fixes: 3b048226 ("BasicProcessorTest: new class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/work/WorkExecutor.cpp
@@ -12,13 +12,13 @@ WorkExecutor::~WorkExecutor()
 {
 }
 
-void WorkExecutor::suspend(Work::Ptr work)
+void WorkExecutor::suspend(Work::Ptr)
 {
 	throw WorkSuspendForEventThrowable();
 }
 
 void WorkExecutor::suspend(
-		Work::Ptr work,
+		Work::Ptr,
 		const Timespan &duration)
 {
 	throw WorkSuspendThrowable(duration);
@@ -28,12 +28,12 @@ NullWorkExecutor::NullWorkExecutor()
 {
 }
 
-bool NullWorkExecutor::accepts(const Work::Ptr work) const
+bool NullWorkExecutor::accepts(const Work::Ptr) const
 {
 	throw NotImplementedException(__func__);
 }
 
-void NullWorkExecutor::execute(Work::Ptr work)
+void NullWorkExecutor::execute(Work::Ptr)
 {
 	throw NotImplementedException(__func__);
 }

src/work/WorkRunner.cpp
@@ -43,7 +43,7 @@ WorkRunnerFactory::~WorkRunnerFactory()
 {
 }
 
-WorkRunner *NullWorkRunnerFactory::create(WorkScheduler &scheduler)
+WorkRunner *NullWorkRunnerFactory::create(WorkScheduler &)
 {
 	throw NotImplementedException(__func__);
 }

src/work/WorkScheduler.cpp
@@ -10,17 +10,17 @@ WorkScheduler::~WorkScheduler()
 {
 }
 
-void NullWorkScheduler::schedule(Work::Ptr work)
+void NullWorkScheduler::schedule(Work::Ptr)
 {
 	throw NotImplementedException(__func__);
 }
 
-void NullWorkScheduler::wakeup(Work::Ptr work)
+void NullWorkScheduler::wakeup(Work::Ptr)
 {
 	throw NotImplementedException(__func__);
 }
 
-void NullWorkScheduler::cancel(Work::Ptr work)
+void NullWorkScheduler::cancel(Work::Ptr)
 {
 	throw NotImplementedException(__func__);
 }

test/work/BasicProcessorTest.cpp
@@ -61,12 +61,12 @@ public:
 
 class NotAcceptingExecutor : public WorkExecutor {
 public:
-	bool accepts(const Work::Ptr work) const override
+	bool accepts(const Work::Ptr) const override
 	{
 		return false;
 	}
 
-	void execute(Work::Ptr work) override
+	void execute(Work::Ptr) override
 	{
 		throw Poco::AssertionViolationException("this must never be called");
 	}