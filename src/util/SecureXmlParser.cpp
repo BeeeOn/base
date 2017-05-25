BeeeOn/base

commit e175707c4d6afd18719517df998392ab7fa34928

Import of code extracted from the server project
This project is intended to contain common BeeeOn code and to built
the library BeeeOnBase. It contains unit tests based on cppunit
library [1]. There are some cppunit extensions included.

[1] http://cppunit.sourceforge.net/doc/cvs/cppunit_cookbook.html

Signed-off-by: Jan Viktorin <viktorin@rehivetech.com>

src/util/SecureXmlParser.cpp
@@ -1,0 +1,84 @@
+#include <Poco/Exception.h>
+#include <Poco/DOM/DOMBuilder.h>
+#include <Poco/SAX/DeclHandler.h>
+#include <Poco/SAX/WhitespaceFilter.h>
+#include <Poco/SAX/LexicalHandler.h>
+
+#include "util/SecureXmlParser.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::XML;
+using namespace BeeeOn;
+
+namespace BeeeOn {
+
+class DenyDTDHandler : public LexicalHandler {
+public:
+	void comment(const XMLChar c[], int start, int length)
+	{
+	}
+
+	void startCDATA()
+	{
+	}
+
+	void endCDATA()
+	{
+	}
+
+	/**
+	 * Throws InvalidArgumentException when called.
+	 */
+	void startDTD(
+		const XMLString &name,
+		const XMLString &publicId,
+		const XMLString &systemId)
+	{
+		throw InvalidArgumentException(
+				"DTD is forbidden for this parser");
+	}
+
+	void endDTD()
+	{
+	}
+
+	void startEntity(const XMLString &name)
+	{
+	}
+
+	void endEntity(const XMLString &name)
+	{
+	}
+};
+
+}
+
+SecureXmlParser::SecureXmlParser()
+{
+}
+
+SecureXmlParser::~SecureXmlParser()
+{
+}
+
+Document *SecureXmlParser::parse(const char *input, std::size_t length)
+{
+	SAXParser parser;
+	WhitespaceFilter filter(&parser);
+	filter.setFeature(XMLReader::FEATURE_NAMESPACES, true);
+	filter.setFeature(XMLReader::FEATURE_NAMESPACE_PREFIXES, true);
+
+	DOMBuilder builder(filter);
+
+	DenyDTDHandler lexicalHandler;
+	filter.setProperty(XMLReader::PROPERTY_LEXICAL_HANDLER,
+		static_cast<Poco::XML::LexicalHandler*>(&lexicalHandler));
+
+	return builder.parseMemoryNP(input, length);
+}
+
+Document *SecureXmlParser::parse(const string &input)
+{
+	return parse(input.c_str(), input.size());
+}

commit d01f8b6d1dc1e02e0a4a6121a1ed4891899251ec

DenyDTDHandler: fix unused parameters
Unused parameters: c, start, length, name, publicId, systemId,
                   systemId, name

Fixes: e175707c ("Import of code extracted from the server project")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/SecureXmlParser.cpp
@@ -16,7 +16,7 @@ namespace BeeeOn {
 
 class DenyDTDHandler : public LexicalHandler {
 public:
-	void comment(const XMLChar c[], int start, int length)
+	void comment(const XMLChar[], int, int)
 	{
 	}
 
@@ -32,9 +32,9 @@ public:
 	 * Throws InvalidArgumentException when called.
 	 */
 	void startDTD(
-		const XMLString &name,
-		const XMLString &publicId,
-		const XMLString &systemId)
+		const XMLString &,
+		const XMLString &,
+		const XMLString &)
 	{
 		throw InvalidArgumentException(
 				"DTD is forbidden for this parser");
@@ -44,11 +44,11 @@ public:
 	{
 	}
 
-	void startEntity(const XMLString &name)
+	void startEntity(const XMLString &)
 	{
 	}
 
-	void endEntity(const XMLString &name)
+	void endEntity(const XMLString &)
 	{
 	}
 };