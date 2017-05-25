BeeeOn/base

commit 5d98f8b599830bbd5acce6896ba837ff9f4e6b8d

AutoConfigurationExplorer: new implementation
Implements the ConfigurationExplorer interface. Its purpose is to
lookup configuration files based on the configuration property defined
by Poco: application.configDir. This property is set by Poco automatically.

We search the ${application.configDir}/config.d for all configuration files
that are acceptable by Poco::Util::Application::loadConfiguration().

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/AutoConfigurationExplorer.cpp
@@ -1,0 +1,141 @@
+#include <string>
+
+#include <Poco/Path.h>
+#include <Poco/File.h>
+#include <Poco/Logger.h>
+#include <Poco/SortedDirectoryIterator.h>
+#include <Poco/Util/AbstractConfiguration.h>
+
+#include "util/FileLoader.h"
+#include "util/AutoConfigurationExplorer.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Util;
+using namespace BeeeOn;
+
+AutoConfigurationExplorer::AutoConfigurationExplorer(
+		AbstractConfiguration &guide):
+	m_guide(guide)
+{
+}
+
+AutoConfigurationExplorer::~AutoConfigurationExplorer()
+{
+}
+
+void AutoConfigurationExplorer::handleLink(
+		const Poco::File &file,
+		FileLoader &loader)
+{
+	if (logger().debug()) {
+		logger().debug("skipping link " + file.path(),
+			__FILE__, __LINE__);
+	}
+}
+
+void AutoConfigurationExplorer::handleSubDirectory(
+		const Poco::File &file,
+		FileLoader &loader)
+{
+	if (logger().debug()) {
+		logger().debug("skipping sub-directory " + file.path(),
+			__FILE__, __LINE__);
+	}
+}
+
+bool AutoConfigurationExplorer::shouldSkip(const Poco::File &file)
+{
+	return Path(file.path()).getExtension().find("_disable") != string::npos;
+}
+
+void AutoConfigurationExplorer::handleFile(
+		const Poco::File &file,
+		FileLoader &loader)
+{
+	if (shouldSkip(file)) {
+		if (logger().debug()) {
+			logger().debug("skipping disabled " + file.path(),
+				__FILE__, __LINE__);
+		}
+
+		return;
+	}
+
+	loader.load(file);
+}
+
+void AutoConfigurationExplorer::exploreDirectory(const Poco::Path &path, FileLoader &loader)
+{
+	const File dir(path);
+
+	if (!path.isDirectory()) {
+		if (logger().debug()) {
+			logger().debug("path " + dir.path() + " is not a directory",
+					__FILE__, __LINE__);
+		}
+
+		return;
+	}
+
+	if (!dir.exists()) {
+		if (logger().debug()) {
+			logger().debug("path " + dir.path() + " does not exist",
+					__FILE__, __LINE__);
+		}
+
+		return;
+	}
+
+	SortedDirectoryIterator it(dir);
+	SortedDirectoryIterator end;
+
+	if (logger().debug()) {
+		logger().debug("exploring directory " + path.toString(),
+				__FILE__, __LINE__);
+	}
+
+	for(; it != end; ++it) {
+		const File current(it.path());
+		exploreOne(current, loader);
+	}
+}
+
+void AutoConfigurationExplorer::exploreOne(const Poco::File &file, FileLoader &loader)
+{
+	if (!file.exists()) {
+		if (logger().debug()) {
+			logger().debug("file " + file.path() + " does not exist",
+					__FILE__, __LINE__);
+		}
+	}
+	else if (file.isDirectory()) {
+		handleSubDirectory(file, loader);
+	}
+	else if (file.isLink()) {
+		handleLink(file, loader);
+	}
+	else if (file.isFile()) {
+		handleFile(file, loader);
+	}
+	else {
+		if (logger().debug()) {
+			logger().debug("skip special " + file.path(),
+				__FILE__, __LINE__);
+		}
+	}
+}
+
+void AutoConfigurationExplorer::explore(FileLoader &loader)
+{
+	const string pocoDir = m_guide.getString("application.configDir", "");
+
+	if (logger().debug()) {
+		logger().debug("application.configDir = " + pocoDir,
+				__FILE__, __LINE__);
+	}
+
+	if (!pocoDir.empty()) {
+		exploreDirectory(Path(pocoDir).pushDirectory("config.d"), loader);
+	}
+}

src/CMakeLists.txt
@@ -43,7 +43,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/FileLoader.cpp
 	${PROJECT_SOURCE_DIR}/util/ApplicationConfigurationLoader.cpp
 	${PROJECT_SOURCE_DIR}/util/ConfigurationExplorer.cpp
-	${PROJECT_SOURCE_DIR}/util/GuidedConfigurationExplorer.cpp
+	${PROJECT_SOURCE_DIR}/util/AutoConfigurationExplorer.cpp
 )
 
 include_directories(

src/util/AutoConfigurationExplorer.h
@@ -1,0 +1,55 @@
+#ifndef BEEEON_GUIDED_CONFIGURATION_EXPLORER_H
+#define BEEEON_GUIDED_CONFIGURATION_EXPLORER_H
+
+#include <string>
+
+#include "util/ConfigurationExplorer.h"
+#include "util/Loggable.h"
+
+namespace Poco {
+
+class File;
+
+namespace Util {
+
+class AbstractConfiguration;
+
+}
+}
+
+namespace BeeeOn {
+
+/**
+ * Explore configuration files guided by the given configuration.
+ * It looks for keys:
+ *
+ * * application.configDir - in this directory, it processes files in config.d/
+ * * application.extended.configDir - processes all files in this directory
+ * * application.extended.configFiles - processes files specifies in this list
+ *              separated by a platform specific separator (e.g. ':')
+ */
+class AutoConfigurationExplorer :
+		public ConfigurationExplorer,
+		Loggable {
+public:
+	AutoConfigurationExplorer(
+		Poco::Util::AbstractConfiguration &guide);
+	virtual ~AutoConfigurationExplorer();
+
+	void explore(FileLoader &loader) override;
+
+protected:
+	void exploreDirectory(const Poco::Path &path, FileLoader &loader);
+	void exploreOne(const Poco::File &file, FileLoader &loader);
+	virtual void handleLink(const Poco::File &file, FileLoader &loader);
+	virtual void handleSubDirectory(const Poco::File &file, FileLoader &loader);
+	virtual void handleFile(const Poco::File &file, FileLoader &loader);
+	virtual bool shouldSkip(const Poco::File &file);
+
+private:
+	Poco::Util::AbstractConfiguration &m_guide;
+};
+
+}
+
+#endif

commit 5d9eea724b352b1580ad7d65fb3fd2b01bf775a2

AutoConfigurationExplorer: unused parameter ‘loader’
Fixes: 5d98f8b5 ("AutoConfigurationExplorer: new implementation")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/AutoConfigurationExplorer.cpp
@@ -26,7 +26,7 @@ AutoConfigurationExplorer::~AutoConfigurationExplorer()
 
 void AutoConfigurationExplorer::handleLink(
 		const Poco::File &file,
-		FileLoader &loader)
+		FileLoader &)
 {
 	if (logger().debug()) {
 		logger().debug("skipping link " + file.path(),
@@ -36,7 +36,7 @@ void AutoConfigurationExplorer::handleLink(
 
 void AutoConfigurationExplorer::handleSubDirectory(
 		const Poco::File &file,
-		FileLoader &loader)
+		FileLoader &)
 {
 	if (logger().debug()) {
 		logger().debug("skipping sub-directory " + file.path(),