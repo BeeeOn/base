BeeeOn/base

commit e175707c4d6afd18719517df998392ab7fa34928

Import of code extracted from the server project
This project is intended to contain common BeeeOn code and to built
the library BeeeOnBase. It contains unit tests based on cppunit
library [1]. There are some cppunit extensions included.

[1] http://cppunit.sourceforge.net/doc/cvs/cppunit_cookbook.html

Signed-off-by: Jan Viktorin <viktorin@rehivetech.com>

src/ssl/RejectCertificateHandler.cpp
@@ -1,0 +1,41 @@
+#include <Poco/Logger.h>
+#include <Poco/DateTimeFormat.h>
+#include <Poco/DateTimeFormatter.h>
+#include <Poco/Net/CertificateHandlerFactory.h>
+#include <Poco/Net/VerificationErrorArgs.h>
+
+#include "ssl/RejectCertificateHandler.h"
+#include "ssl/X509Fingerprint.h"
+
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+BetterRejectCertificateHandler::BetterRejectCertificateHandler(bool server):
+	InvalidCertificateHandler(server)
+{
+}
+
+void BetterRejectCertificateHandler::onInvalidCertificate(
+		const void *sender,
+		VerificationErrorArgs &error)
+{
+	const X509Certificate &cert = error.certificate();
+
+	error.setIgnoreError(false);
+
+	logger().warning(
+		"rejecting certificate:\n"
+		+ format("Name: %s\nIssuer: %s\nSubject: %s\n"
+			"Valid from: %s\nSHA256: %s\ndue to: %s",
+			cert.commonName(),
+			cert.issuerName(),
+			cert.subjectName(),
+			DateTimeFormatter::format(cert.validFrom(),
+				DateTimeFormat::SORTABLE_FORMAT),
+		X509Fingerprint(&cert).digestToHex("SHA256"),
+		error.errorMessage()),
+		__FILE__, __LINE__);
+}
+
+POCO_REGISTER_CHFACTORY(BeeeOn, BetterRejectCertificateHandler)

src/ssl/SSLFacility.cpp
@@ -1,0 +1,215 @@
+#include <Poco/Version.h>
+#include <Poco/String.h>
+#include <Poco/Logger.h>
+#include <Poco/Delegate.h>
+#include <Poco/StringTokenizer.h>
+#include <Poco/NumberParser.h>
+#include <Poco/NumberFormatter.h>
+#include <Poco/Net/Utility.h>
+#include <Poco/Net/SSLManager.h>
+
+#include "ssl/SSLFacility.h"
+#include "ssl/RejectCertificateHandler.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+Mutex PrivateKeyPassphraseProvider::defaultLock;
+
+PrivateKeyPassphraseProvider::PrivateKeyPassphraseProvider(
+		const std::string &passphrase):
+	m_passphrase(passphrase),
+	m_lock(PrivateKeyPassphraseProvider::defaultLock)
+{
+	init();
+}
+
+PrivateKeyPassphraseProvider::PrivateKeyPassphraseProvider(
+		const std::string &passphrase, Mutex &lock):
+	m_passphrase(passphrase),
+	m_lock(lock)
+{
+	init();
+}
+
+void PrivateKeyPassphraseProvider::init()
+{
+	m_lock.lock();
+
+	SSLManager &manager = SSLManager::instance();
+	manager.PrivateKeyPassphraseRequired +=
+		Delegate<PrivateKeyPassphraseProvider, string>(
+			this,
+			&PrivateKeyPassphraseProvider::onRequest
+		);
+}
+
+PrivateKeyPassphraseProvider::~PrivateKeyPassphraseProvider()
+{
+	SSLManager &manager = SSLManager::instance();
+	manager.PrivateKeyPassphraseRequired -=
+		Delegate<PrivateKeyPassphraseProvider, string>(
+			this,
+			&PrivateKeyPassphraseProvider::onRequest
+		);
+
+	m_lock.unlock();
+}
+
+void PrivateKeyPassphraseProvider::onRequest(
+		const void *sender, string &passphrase)
+{
+	passphrase = m_passphrase;
+}
+
+SSLFacility::SSLFacility():
+	m_loadDefaultCA(false),
+	m_verificationMode(VerificationMode::VERIFY_RELAXED),
+	m_verificationDepth(9),
+	m_cipherList("ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH"),
+	m_sessionCache(false),
+	m_disabledProtocols(-1)
+{
+	initializeSSL();
+
+#if POCO_VERSION >= 0x01070000
+	m_disabledProtocols = Context::PROTO_SSLV2 | Context::PROTO_SSLV3;
+#endif
+}
+
+SSLFacility::~SSLFacility()
+{
+	uninitializeSSL();
+}
+
+void SSLFacility::initContext()
+{
+	Mutex::ScopedLock guard(m_lock);
+
+	if (!m_context.isNull())
+		return;
+
+	if (!m_passphrase.empty()) {
+		PrivateKeyPassphraseProvider provider(m_passphrase);
+		m_context = createContext();
+	} else {
+		m_context = createContext();
+	}
+
+#if POCO_VERSION >= 0x01070000
+	m_context->disableProtocols(m_disabledProtocols);
+#else
+	if (m_disabledProtocols)
+		logger().warning("protocols are not disabled, linked against too old Poco library: "
+				+ NumberFormatter::formatHex(POCO_VERSION, 10, true),
+				__FILE__, __LINE__);
+#endif
+
+	logger().information("SSL context initialized",
+				__FILE__, __LINE__);
+
+	logger().debug("caLocation: " + m_caLocation);
+	logger().debug("loadDefaultCA: " + to_string(m_loadDefaultCA));
+	logger().debug("privateKey: " + m_privateKey);
+	logger().debug("passphrase: " + to_string(!m_passphrase.empty()));
+	logger().debug("certificate: " + m_certificate);
+	logger().debug("verificationMode: " + to_string(m_verificationMode));
+	logger().debug("verificationDepth: " + to_string(m_verificationDepth));
+	logger().debug("cihperList: " + m_cipherList);
+	logger().debug("sessionCache: " + to_string(m_sessionCache));
+	logger().debug("disableProtocols: " + NumberFormatter::formatHex(m_disabledProtocols, 10, true));
+}
+
+Context::Ptr SSLFacility::context()
+{
+	initContext();
+	return m_context;
+}
+
+void SSLFacility::setSessionCache(const std::string &enable)
+{
+	m_sessionCache = NumberParser::parseBool(enable);
+}
+
+void SSLFacility::setCALocation(const string &location)
+{
+	m_caLocation = location;
+}
+
+void SSLFacility::setLoadDefaultCA(const string &enable)
+{
+	NumberParser::parseBool(enable);
+}
+
+void SSLFacility::setPrivateKey(const string &file)
+{
+	m_privateKey = file;
+}
+
+void SSLFacility::setPassphrase(const std::string &passphrase)
+{
+	m_passphrase = passphrase;
+}
+
+void SSLFacility::setCertificate(const string &file)
+{
+	m_certificate = file;
+}
+
+void SSLFacility::setVerificationMode(const string &mode)
+{
+	m_verificationMode = Utility::convertVerificationMode(mode);
+}
+
+void SSLFacility::setVerificationDepth(int depth)
+{
+	m_verificationDepth = depth;
+}
+
+void SSLFacility::setCipherList(const string &list)
+{
+	m_cipherList = list;
+}
+
+static unsigned int parseProtocol(const string &protocol)
+{
+#if POCO_VERSION >= 0x01070000
+	if (!icompare(protocol, "sslv2"))
+		return Context::PROTO_SSLV2;
+	else if (!icompare(protocol, "sslv3"))
+		return Context::PROTO_SSLV3;
+	else if (!icompare(protocol, "tlsv1"))
+		return Context::PROTO_TLSV1;
+	else if (!icompare(protocol, "tlsv1_1"))
+		return Context::PROTO_TLSV1_1;
+	else if (!icompare(protocol, "tlsv1_2"))
+		return Context::PROTO_TLSV1_2;
+	else
+#endif
+		return 0;
+}
+
+void SSLFacility::setDisabledProtocols(const string &protocols)
+{
+	StringTokenizer tokenizer(protocols, ",;",
+		StringTokenizer::TOK_IGNORE_EMPTY | StringTokenizer::TOK_TRIM);
+
+	unsigned int disable = 0;
+
+	for (auto tok : tokenizer) {
+		logger().debug("parsing SSL protocol: " + tok,
+				__FILE__, __LINE__);
+
+		unsigned int ret = parseProtocol(tok);
+		if (ret == 0) {
+			logger().warning("failed to recognize SSL protocol: " + tok,
+					__FILE__, __LINE__);
+		}
+
+		disable |= ret;
+	}
+
+	m_disabledProtocols = disable;
+}

src/ssl/X509Fingerprint.cpp
@@ -1,0 +1,282 @@
+#include <sstream>
+
+#include <Poco/Exception.h>
+#include <Poco/Token.h>
+#include <Poco/StreamTokenizer.h>
+#include <Poco/Crypto/DigestEngine.h>
+#include <Poco/Crypto/X509Certificate.h>
+
+#include "util/Base64.h"
+#include "ssl/X509Fingerprint.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Crypto;
+using namespace BeeeOn;
+
+namespace BeeeOn {
+
+class PEMMarkerStartToken : public Poco::Token {
+public:
+	PEMMarkerStartToken();
+
+	bool start(char c, std::istream &in) override;
+	void finish(std::istream &in) override;
+	Class tokenClass() const override;
+
+private:
+	bool m_invalid;
+};
+
+class PEMMarkerToken : public Poco::Token {
+public:
+	PEMMarkerToken(const std::string &marker);
+
+	bool start(char c, std::istream &in) override;
+	void finish(std::istream &in) override;
+	Class tokenClass() const override;
+
+protected:
+	bool finish(const std::string &expect,
+			std::istream &in);
+
+private:
+	std::string m_expect;
+	bool m_invalid;
+};
+
+class Base64Token : public Poco::Token {
+public:
+	Base64Token();
+
+	bool start(char c, std::istream &in) override;
+	void finish(std::istream &in) override;
+	Class tokenClass() const override;
+
+protected:
+	bool isBase64(char c) const;
+
+private:
+	bool m_invalid;
+};
+
+}
+
+PEMMarkerStartToken::PEMMarkerStartToken():
+	m_invalid(false)
+{
+}
+
+bool PEMMarkerStartToken::start(char c, std::istream &in)
+{
+	if (c != '-')
+		return false;
+
+	if (in.peek() != '-')
+		return false;
+
+	_value = c;
+	m_invalid = false;
+	return true;
+}
+
+void PEMMarkerStartToken::finish(istream &in)
+{
+	int c = in.peek();
+
+	for (size_t i = 1; i < 5; ++i) {
+		if (c != '-') {
+			m_invalid = true;
+			return;
+		}
+
+		_value += (char) c;
+		in.get();
+		c = in.peek();
+	}
+}
+
+Token::Class PEMMarkerStartToken::tokenClass() const
+{
+	return m_invalid? Token::INVALID_TOKEN
+			: Token::SPECIAL_COMMENT_TOKEN;
+}
+
+PEMMarkerToken::PEMMarkerToken(const std::string &marker):
+	m_expect(marker),
+	m_invalid(false)
+{
+}
+
+bool PEMMarkerToken::start(char c, std::istream &in)
+{
+	if (c != m_expect[0])
+		return false;
+
+	if (in.peek() != m_expect[1])
+		return false;
+
+	_value = c;
+	m_invalid = false;
+	return true;
+}
+
+bool PEMMarkerToken::finish(const string &expect, istream &in)
+{
+	int c = in.peek();
+
+	for (size_t i = 1; i < m_expect.size(); ++i) {
+		if (c != m_expect[i])
+			return true;
+
+		_value += (char) c;
+		in.get();
+		c = in.peek();
+	}
+
+	return false;
+}
+
+void PEMMarkerToken::finish(std::istream &in)
+{
+	m_invalid = finish(m_expect, in);
+}
+
+Token::Class PEMMarkerToken::tokenClass() const
+{
+	return m_invalid? Token::INVALID_TOKEN
+			: Token::USER_TOKEN;
+}
+
+Base64Token::Base64Token():
+	m_invalid(false)
+{
+}
+
+bool Base64Token::isBase64(char c) const
+{
+	if (c >= 'A' && c <= 'Z')
+		return true;
+	if (c >= 'a' && c <= 'z')
+		return true;
+	if (c >= '0' && c <= '9')
+		return true;
+	if (c == '+' || c == '/')
+		return true;
+
+	return false;
+}
+
+bool Base64Token::start(char c, istream &in)
+{
+	if (!isBase64(c))
+		return false;
+
+	if (!isBase64(in.peek()))
+		return false;
+
+	_value += c;
+	m_invalid = false;
+	return true;
+}
+
+void Base64Token::finish(istream &in)
+{
+	int c;
+
+	c = in.peek();
+	while (c != EOF) {
+		if (c == '\n') {
+			in.get();
+			c = in.peek();
+			continue;
+		}
+
+		if (isBase64(c) || c == '=')
+			_value += c;
+		else
+			break;
+
+		in.get();
+		c = in.peek();
+	}
+}
+
+Token::Class Base64Token::tokenClass() const
+{
+	return Token::STRING_LITERAL_TOKEN;
+}
+
+X509Fingerprint::X509Fingerprint(const X509Certificate *cert):
+	m_cert(cert)
+{
+}
+
+Poco::DigestEngine::Digest X509Fingerprint::digest(
+		const string &algo)
+{
+	Poco::Crypto::DigestEngine engine(algo);
+	performDigest(engine);
+	return engine.digest();
+}
+
+string X509Fingerprint::digestToHex(const string &algo)
+{
+	Poco::Crypto::DigestEngine engine(algo);
+	performDigest(engine);
+	return Poco::DigestEngine::digestToHex(engine.digest());
+}
+
+#define PEM_HEADER "BEGIN CERTIFICATE-----\n"
+#define PEM_FOOTER "END CERTIFICATE-----"
+
+void X509Fingerprint::performDigest(Poco::DigestEngine &engine)
+{
+	stringstream ss;
+	m_cert->save(ss);
+
+	StreamTokenizer pemParser(ss);
+	pemParser.addToken(new PEMMarkerStartToken());
+	pemParser.addToken(new PEMMarkerToken(PEM_HEADER));
+	pemParser.addToken(new PEMMarkerToken(PEM_FOOTER));
+	pemParser.addToken(new Base64Token());
+	pemParser.addToken(new WhitespaceToken());
+
+	const Token *token;
+	
+	token = pemParser.next();
+	if (!token->is(Token::SPECIAL_COMMENT_TOKEN))
+		throw SyntaxException("missing start of PEM header");
+
+	if (token->asString() != "-----")
+		throw SyntaxException("expected start of PEM header");
+
+	token = pemParser.next();
+	if (!token->is(Token::USER_TOKEN))
+		throw SyntaxException("missing PEM header");
+
+	if (token->asString() != PEM_HEADER)
+		throw SyntaxException("expected PEM header");
+
+	token = pemParser.next();
+	if (!token->is(Token::STRING_LITERAL_TOKEN))
+		throw SyntaxException("missing PEM content");
+
+	const string &text = token->asString();
+
+	token = pemParser.next();
+	if (!token->is(Token::SPECIAL_COMMENT_TOKEN))
+		throw SyntaxException("missing start of PEM footer");
+
+	if (token->asString() != "-----")
+		throw SyntaxException("expected start of PEM footer");
+
+	token = pemParser.next();
+	if (!token->is(Token::USER_TOKEN))
+		throw SyntaxException("missing PEM footer");
+
+	if (token->asString() != PEM_FOOTER)
+		throw SyntaxException("expected PEM footer");
+
+	engine.update(Base64::decode(text));
+}

commit 8b3267269e2f0de9beb0d613821065ba7f9f948d

ssl: fix unused parameters
Fixes: e175707c ("Import of code extracted from the server project")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/ssl/RejectCertificateHandler.cpp
@@ -17,7 +17,7 @@ BetterRejectCertificateHandler::BetterRejectCertificateHandler(bool server):
 }
 
 void BetterRejectCertificateHandler::onInvalidCertificate(
-		const void *sender,
+		const void *,
 		VerificationErrorArgs &error)
 {
 	const X509Certificate &cert = error.certificate();

src/ssl/SSLFacility.cpp
@@ -59,7 +59,7 @@ PrivateKeyPassphraseProvider::~PrivateKeyPassphraseProvider()
 }
 
 void PrivateKeyPassphraseProvider::onRequest(
-		const void *sender, string &passphrase)
+		const void *, string &passphrase)
 {
 	passphrase = m_passphrase;
 }

src/ssl/X509Fingerprint.cpp
@@ -121,7 +121,7 @@ bool PEMMarkerToken::start(char c, std::istream &in)
 	return true;
 }
 
-bool PEMMarkerToken::finish(const string &expect, istream &in)
+bool PEMMarkerToken::finish(const string &, istream &in)
 {
 	int c = in.peek();
 