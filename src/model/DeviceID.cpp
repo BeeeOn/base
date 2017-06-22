BeeeOn/base

commit ac930f3c84b27ee36d32f666e7d920dbad71437d

DeviceID: the prefix rewitten in enum
The prefix rewitten from original prefix (uint8_t) to the new enum prefix.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/DeviceID.cpp
@@ -26,7 +26,7 @@ DeviceID::DeviceID(uint64_t value):
 	}
 }
 
-DeviceID::DeviceID(uint8_t prefix, uint64_t ident)
+DeviceID::DeviceID(const DevicePrefix &prefix, uint64_t ident)
 {
 	if (ident & 0xff00000000000000UL) {
 		throw InvalidArgumentException(
@@ -70,16 +70,17 @@ string DeviceID::toString() const
 	return ss.str();
 }
 
-DeviceID DeviceID::random(uint8_t prefix)
+DeviceID DeviceID::random(const DevicePrefix &prefix)
 {
 	Random rnd;
 	rnd.seed();
 
-	prefix = prefix? prefix : rnd.next(256);
-
 	uint64_t ident = rnd.next();
 	ident <<= 24;
 	ident |= rnd.next(1 << 24);
+
+	if (prefix == DevicePrefix::PREFIX_INVALID)
+		return DeviceID(DevicePrefix::random(), ident);
 
 	return DeviceID(prefix, ident);
 }

src/model/DeviceID.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <string>
 
+#include "model/DevicePrefix.h"
+
 namespace BeeeOn {
 
 /**
@@ -32,7 +34,7 @@ public:
 	/**
 	 * Construct 64-bit ID only.
 	 */
-	DeviceID(uint8_t prefix, uint64_t ident);
+	DeviceID(const DevicePrefix &prefix, uint64_t ident);
 
 	bool isNull() const
 	{
@@ -44,9 +46,9 @@ public:
 		return (unsigned long long) m_value;
 	}
 
-	uint8_t prefix() const
+	DevicePrefix prefix() const
 	{
-		return (uint8_t) (m_value >> (is32bit()? 24 : 56));
+		return DevicePrefix::fromRaw((uint8_t) (m_value >> (is32bit()? 24 : 56)));
 	}
 
 	uint64_t ident() const
@@ -98,7 +100,8 @@ public:
 	 * Generate a random ID. If the prefix is zero a new one is
 	 * generated randomly.
 	 */
-	static DeviceID random(uint8_t prefix = 0);
+	static DeviceID random(const DevicePrefix &prefix
+		= DevicePrefix::fromRaw(DevicePrefix::PREFIX_INVALID));
 
 private:
 	uint64_t m_value;

src/util/Enum.h
@@ -3,6 +3,7 @@
 
 #include <Poco/Exception.h>
 #include <Poco/Logger.h>
+#include <Poco/Random.h>
 
 #include <map>
 #include <string>
@@ -148,6 +149,14 @@ public:
 		}
 
 		return Enum<Base, Raw>(it->second);
+	}
+
+	static Enum<Base, Raw> random()
+	{
+		Poco::Random rnd;
+		rnd.seed();
+
+		return fromRaw(rnd.next(rawMap().size()));
 	}
 
 	static Enum<Base, Raw> fromRaw(const unsigned int raw)

commit 0cae0f835cabf98e4b9b9e9c1c159ad7e9b2b3c6

DeviceID: avoid generation of random prefix
The DeviceID does not generate any random prefix. This way, it is
possible to generate a random device ID with any arbitrary prefix.
The caller may generate the prefix at random as well.

This fixes also a potential inconsistence that could happen when
passing PREFIX_INVALID and expect a random valid prefix to be
generated. We could end up again with PREFIX_INVALID.

Fixes: ac930f3c84b2 ("DeviceID: the prefix rewitten in enum")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/model/DeviceID.cpp
@@ -79,8 +79,5 @@ DeviceID DeviceID::random(const DevicePrefix &prefix)
 	ident <<= 24;
 	ident |= rnd.next(1 << 24);
 
-	if (prefix == DevicePrefix::PREFIX_INVALID)
-		return DeviceID(DevicePrefix::random(), ident);
-
 	return DeviceID(prefix, ident);
 }