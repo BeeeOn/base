BeeeOn/base

commit ac930f3c84b27ee36d32f666e7d920dbad71437d

DeviceID: the prefix rewitten in enum
The prefix rewitten from original prefix (uint8_t) to the new enum prefix.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Enum.h
@@ -3,6 +3,7 @@
 
 #include <Poco/Exception.h>
 #include <Poco/Logger.h>
+#include <Poco/Random.h>
 
 #include <map>
 #include <string>
@@ -148,6 +149,14 @@ public:
 		}
 
 		return Enum<Base, Raw>(it->second);
+	}
+
+	static Enum<Base, Raw> random()
+	{
+		Poco::Random rnd;
+		rnd.seed();
+
+		return fromRaw(rnd.next(rawMap().size()));
 	}
 
 	static Enum<Base, Raw> fromRaw(const unsigned int raw)

src/model/DeviceID.cpp
@@ -26,7 +26,7 @@ DeviceID::DeviceID(uint64_t value):
 	}
 }
 
-DeviceID::DeviceID(uint8_t prefix, uint64_t ident)
+DeviceID::DeviceID(const DevicePrefix &prefix, uint64_t ident)
 {
 	if (ident & 0xff00000000000000UL) {
 		throw InvalidArgumentException(
@@ -70,16 +70,17 @@ string DeviceID::toString() const
 	return ss.str();
 }
 
-DeviceID DeviceID::random(uint8_t prefix)
+DeviceID DeviceID::random(const DevicePrefix &prefix)
 {
 	Random rnd;
 	rnd.seed();
 
-	prefix = prefix? prefix : rnd.next(256);
-
 	uint64_t ident = rnd.next();
 	ident <<= 24;
 	ident |= rnd.next(1 << 24);
+
+	if (prefix == DevicePrefix::PREFIX_INVALID)
+		return DeviceID(DevicePrefix::random(), ident);
 
 	return DeviceID(prefix, ident);
 }

src/model/DeviceID.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <string>
 
+#include "model/DevicePrefix.h"
+
 namespace BeeeOn {
 
 /**
@@ -32,7 +34,7 @@ public:
 	/**
 	 * Construct 64-bit ID only.
 	 */
-	DeviceID(uint8_t prefix, uint64_t ident);
+	DeviceID(const DevicePrefix &prefix, uint64_t ident);
 
 	bool isNull() const
 	{
@@ -44,9 +46,9 @@ public:
 		return (unsigned long long) m_value;
 	}
 
-	uint8_t prefix() const
+	DevicePrefix prefix() const
 	{
-		return (uint8_t) (m_value >> (is32bit()? 24 : 56));
+		return DevicePrefix::fromRaw((uint8_t) (m_value >> (is32bit()? 24 : 56)));
 	}
 
 	uint64_t ident() const
@@ -98,7 +100,8 @@ public:
 	 * Generate a random ID. If the prefix is zero a new one is
 	 * generated randomly.
 	 */
-	static DeviceID random(uint8_t prefix = 0);
+	static DeviceID random(const DevicePrefix &prefix
+		= DevicePrefix::fromRaw(DevicePrefix::PREFIX_INVALID));
 
 private:
 	uint64_t m_value;

commit 6f5142e555c55b44409a1eef1e749d7b8ce1f8ef

Enum: fix random generation for sparse enums
In case when the enum is defined as a sequence of sparse values, e.g.:

enum {
	X0 = 5,
	X1 = 10,
	X2 = 11,
	X3 = 200,
	...
}

the random() method fails because it assumes that the values are always
starting at 0 and are contiguously incremented by 1.

Fix this behaviour by iterating over the Base::valueMap() to find the
n'th enum value based on a random number. The random generation thus
has a linear complexity. However, most enums are not so big that this
performance issue would bother us.

A unit test of the random() method is included to show that this
implementation of the method random() is fixed.

Fixes: ac930f3c84b2 ("DeviceID: the prefix rewitten in enum")
Reported-by: Hošala Martin <xhosal00@stud.fit.vutbr.cz>
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/Enum.h
@@ -95,6 +95,16 @@ private:
 	Enum(const Value &value):
 		m_value(value)
 	{
+		if (value == Base::valueMap().end()) {
+			if (Base::valueMap().empty()) {
+				throw Poco::IllegalStateException(
+					"attempt to create enum that is empty");
+			}
+
+			// this is certainly a bug in the Enum class
+			throw Poco::IllegalStateException(
+				"invalid enum value determined");
+		}
 	}
 
 protected:
@@ -154,7 +164,15 @@ public:
 		Poco::Random rnd;
 		rnd.seed();
 
-		return fromRaw(rnd.next(rawMap().size()));
+		const std::size_t index = rnd.next(Base::valueMap().size());
+		Value it = Base::valueMap().begin();
+
+		for (std::size_t k = 0; it != Base::valueMap().end(); ++it, ++k) {
+			if (k == index)
+				break;
+		}
+
+		return Enum<Base, Raw>(it);
 	}
 
 	static Enum<Base, Raw> fromRaw(const unsigned int raw)

test/util/EnumTest.cpp
@@ -14,12 +14,14 @@ class EnumTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testFromRaw);
 	CPPUNIT_TEST(testCompare);
 	CPPUNIT_TEST(testStringConcat);
+	CPPUNIT_TEST(testRandom);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void testParse();
 	void testFromRaw();
 	void testCompare();
 	void testStringConcat();
+	void testRandom();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(EnumTest);
@@ -27,9 +29,9 @@ CPPUNIT_TEST_SUITE_REGISTRATION(EnumTest);
 struct TestTypeEnum {
 	enum Raw {
 		TEST_X0 = 0,
-		TEST_X1,
-		TEST_X2,
-		TEST_X3
+		TEST_X1 = 10,
+		TEST_X2 = 20,
+		TEST_X3 = 100
 	};
 
 	static EnumHelper<Raw>::ValueMap &valueMap();
@@ -109,6 +111,18 @@ void EnumTest::testStringConcat()
 	CPPUNIT_ASSERT_EQUAL("test: x2", "test: " + a);
 	CPPUNIT_ASSERT_EQUAL("test: x3", "test: " + b);
 	CPPUNIT_ASSERT_EQUAL("test: x2, x3", "test: " + a + ", " + b);
+}
+
+/**
+ * This is a probabilistic test. We assume that 1000 attempts is enough
+ * to cover all 4 enum values at least once. The test must not fail even
+ * in case when the used enum (TestType) uses sparse numbers for enum
+ * values. Here we have e.g. TEST_X0 == 0 and TEST_X1 == 10.
+ */
+void EnumTest::testRandom()
+{
+	for (int i = 0; i < 1000; ++i)
+		TestType::random();
 }
 
 }