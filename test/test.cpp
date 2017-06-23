d7061de8a3d082d2a4f0a37a9a7f0075a0e5313d
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-06-23 16:47:38 +0200

    test: support human readable format
    The output format is not configurable from the environment.
    The environment variable TEST_OUTPUT_FORMAT determines whether
    to produce TAP output format or a human readable format.
    The default is the TAP output.
    
    Utilize CompilerOutputter to produce the result report. It is
    very short when successful:
    
    OK (113)
    
    and reports only failed tests, e.g.:
    
    ClassInfoTest.cpp:65:Assertion
    Test name: BeeeOn::ClassInfoTest::testConstruct
    assertion failed
    - Expression: info2 != info4
    
    Failures !!!
    Run: 113   Failure total: 1   Failures: 1   Errors: 0
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/CompilerOutputter.h>
 #include <cppunit/TestRunner.h>
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 
+#include <Poco/Environment.h>
 #include <Poco/Logger.h>
 #include <Poco/AutoPtr.h>
 #include <Poco/File.h>
@@ -51,10 +53,37 @@ static int runWithTapOutput(Test *suite)
 	return tapProducer.wasSuccessful()? 0 : 1;
 }
 
+static int runStandard(Test *suite, const string &format)
+{
+	TestRunner runner;
+	TestResult controller;
+	TestResultCollector collector;
+
+	runner.addTest(suite);
+	controller.addListener(&collector);
+	runner.run(controller);
+
+	if (format == "human") {
+		CompilerOutputter outputter(&collector, cout);
+		outputter.write();
+	}
+	else {
+		cerr << "unsupported output format: " << format << endl;
+		return -1;
+	}
+
+	return collector.wasSuccessful()? 0 : 1;
+}
+
 int main(int argc, char **argv)
 {
 	setupLogger(argc > 1? argv[1] : "logging.ini");
 
 	Test *suite = TestFactoryRegistry::getRegistry().makeTest();
-	return runWithTapOutput(suite);
+
+	const string &format = Environment::get("TEST_OUTPUT_FORMAT", "tap");
+	if (format == "tap")
+		return runWithTapOutput(suite);
+
+	return runStandard(suite, format);
 }
