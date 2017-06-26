BeeeOn/base

commit 1f909a2a6b5e6ac307341d8a55b105375c5dcd08

ClassInfo: new class with test
The ClassInfo provides metadata for a given object or class. It
is a higher layer on top of the type_info and type_index constructs
provided by C++. The main purpose of this class is to move some
useful code out of the Debug.h file.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/ClassInfo.cpp
@@ -1,0 +1,80 @@
+#if defined(__clang__) || defined(__GNUC__)
+#include <cxxabi.h> // abi::__cxa_demangle
+#include <cstdlib>
+#endif
+
+#include "util/ClassInfo.h"
+
+using namespace std;
+using namespace BeeeOn;
+
+ClassInfo::ClassInfo(const type_index &index):
+	m_index(index)
+{
+}
+
+ClassInfo::ClassInfo(const type_info &info):
+	m_index(info)
+{
+}
+
+string ClassInfo::id() const
+{
+	return m_index.name();
+}
+
+#if defined(__clang__) || defined(__GLIBCXX__)
+/**
+ * Use abi::__cxa_demangle to demangle the class name from typeid.
+ * The abi may not be available on all compilers. The clang and
+ * gcc supports this (at least in c++11).
+ *
+ * The displayName method does more then just demangling. It
+ * also drops the template parameters. This leads to clear class
+ * short names useful e.g. for logging configuration. Also occasional
+ * pointer signs (*) are removed. Namespace prefixes are preserved.
+ *
+ * Example:
+ * Test<Other> -> 4TestI5OtherE -> Test
+ * Other -> 5Other -> Other
+ * Other * -> P5Other -> Other
+ * ?? -> PN6BeeeOn18DependencyInjectorE
+ *    -> BeeeOn::DependencyInjector* -> BeeeOn::DependencyInjector
+ *
+ * The ?? comes probably from this pointer.
+ */
+string ClassInfo::name() const
+{
+	char *demangled = abi::__cxa_demangle(m_index.name(), 0, 0, 0);
+	std::string s(demangled);
+	std::free(demangled);
+
+	std::size_t t;
+
+	t = s.find("*<");
+	if (t != std::string::npos)
+		return s.substr(0, t);
+
+	t = s.find("<");
+	if (t != std::string::npos) {
+		std::size_t star = s.find("*");
+		if (star == std::string::npos)
+			return s.substr(0, t);
+
+		return s.substr(0, star < t? star : t);
+	}
+
+	t = s.find("*");
+	return t == std::string::npos? s : s.substr(0, t);
+}
+#else
+string ClassInfo::displayName() const
+{
+	return name();
+}
+#endif
+
+type_index ClassInfo::index() const
+{
+	return m_index;
+}

src/CMakeLists.txt
@@ -34,6 +34,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/ssl/SSLClient.cpp
 	${PROJECT_SOURCE_DIR}/ssl/SSLServer.cpp
 	${PROJECT_SOURCE_DIR}/util/Backtrace.cpp
+	${PROJECT_SOURCE_DIR}/util/ClassInfo.cpp
 	${PROJECT_SOURCE_DIR}/util/CryptoConfig.cpp
 	${PROJECT_SOURCE_DIR}/util/CryptoParams.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMM.cpp

src/util/ClassInfo.h
@@ -1,0 +1,59 @@
+#ifndef BEEEON_CLASS_INFO_H
+#define BEEEON_CLASS_INFO_H
+
+#include <string>
+#include <typeindex>
+#include <type_traits>
+
+namespace BeeeOn {
+
+class ClassInfo {
+public:
+	ClassInfo(const std::type_index &index);
+	ClassInfo(const std::type_info &info);
+
+	template <typename T>
+	ClassInfo(const T &t): m_index(typeid(t))
+	{
+		static_assert(!std::is_pointer<T>::value,
+			"for pointers use forPointer() method");
+	}
+
+	template <typename T>
+	static ClassInfo forPointer(const T *t)
+	{
+		return ClassInfo(*t);
+	}
+
+	template <typename T>
+	static ClassInfo forClass()
+	{
+		return ClassInfo(typeid(T));
+	}
+
+	std::string id() const;
+	std::string name() const;
+	std::type_index index() const;
+
+	bool operator <(const ClassInfo &info) const
+	{
+		return info.index() < m_index;
+	}
+
+	bool operator ==(const ClassInfo &info) const
+	{
+		return info.index() == m_index;
+	}
+
+	bool operator !=(const ClassInfo &info) const
+	{
+		return info.index() != m_index;
+	}
+
+private:
+	std::type_index m_index;
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -26,6 +26,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceIDTest.cpp
 	${PROJECT_SOURCE_DIR}/util/BacktraceTest.cpp
+	${PROJECT_SOURCE_DIR}/util/ClassInfoTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp
 	${PROJECT_SOURCE_DIR}/util/EnumTest.cpp
 	${PROJECT_SOURCE_DIR}/util/Base64Test.cpp

test/util/ClassInfoTest.cpp
@@ -1,0 +1,84 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "cppunit/BetterAssert.h"
+#include "util/ClassInfo.h"
+
+using namespace std;
+
+namespace BeeeOn {
+
+class ClassInfoTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(ClassInfoTest);
+	CPPUNIT_TEST(testEmptyConstructor);
+	CPPUNIT_TEST(testConstruct);
+	CPPUNIT_TEST(testId);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void testEmptyConstructor();
+	void testConstruct();
+	void testId();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ClassInfoTest);
+
+class TestObject0 {
+};
+
+class TestObject1 {
+};
+
+void ClassInfoTest::testEmptyConstructor()
+{
+	ClassInfo info0 = ClassInfo::forClass<TestObject0>();
+	ClassInfo info1 = ClassInfo::forClass<TestObject0>();
+	ClassInfo info2 = ClassInfo::forClass<TestObject1>();
+
+	CPPUNIT_ASSERT(info0 == info1);
+	CPPUNIT_ASSERT(info0 != info2);
+	CPPUNIT_ASSERT(info1 != info2);
+}
+
+void ClassInfoTest::testConstruct()
+{
+	TestObject0 o0;
+	TestObject1 o1;
+
+	ClassInfo info0(typeid(o0));
+	ClassInfo info1(typeid(o1));
+	ClassInfo info2(typeid(TestObject0));
+	ClassInfo info3(typeid(TestObject1));
+	ClassInfo info4(o0);
+	ClassInfo info5(o1);
+	ClassInfo info6 = ClassInfo::forPointer(&o0);
+	ClassInfo info7 = ClassInfo::forPointer(&o1);
+
+	CPPUNIT_ASSERT(info0 == info2);
+	CPPUNIT_ASSERT(info2 == info4);
+	CPPUNIT_ASSERT(info4 == info6);
+
+	CPPUNIT_ASSERT(info1 == info3);
+	CPPUNIT_ASSERT(info3 == info5);
+	CPPUNIT_ASSERT(info5 == info7);
+
+	CPPUNIT_ASSERT(info0 != info1);
+	CPPUNIT_ASSERT(info2 != info3);
+	CPPUNIT_ASSERT(info4 != info5);
+	CPPUNIT_ASSERT(info6 != info7);
+}
+
+void ClassInfoTest::testId()
+{
+	TestObject0 o;
+	ClassInfo info0(typeid(o));
+	ClassInfo info1(typeid(TestObject0));
+	ClassInfo info2(o);
+	ClassInfo info3 = ClassInfo::forPointer(&o);
+
+	CPPUNIT_ASSERT_EQUAL("N6BeeeOn11TestObject0E", info0.id());
+	CPPUNIT_ASSERT_EQUAL("N6BeeeOn11TestObject0E", info1.id());
+	CPPUNIT_ASSERT_EQUAL("N6BeeeOn11TestObject0E", info2.id());
+	CPPUNIT_ASSERT_EQUAL("N6BeeeOn11TestObject0E", info3.id());
+}
+
+}

commit 579cb4e2d1e4d1a9c90b4dffc4709c7b7e2a745c

ClassInfo: fix method name for non-standard environments
Fixes: 1f909a2a6b5e ("ClassInfo: new class with test")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/ClassInfo.cpp
@@ -73,9 +73,9 @@ string ClassInfo::name() const
 	return t == std::string::npos? s : s.substr(0, t);
 }
 #else
-string ClassInfo::displayName() const
+string ClassInfo::name() const
 {
-	return name();
+	return id();
 }
 #endif
 