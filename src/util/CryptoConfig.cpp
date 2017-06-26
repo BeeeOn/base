BeeeOn/base

commit 4eed82d05eaca9a4ea5de4f3b00922c7e00d25ba

CryptoConfig: new class
When cryptography is needed in an application, we have to manage
its configuration. Usually, we encrypt and decrypt many different
data messages that has its specific parameters (CryptoParams).
However, we need to inject the secret passphrase to generate an
encryption key.

This class provides methods for this purpose. Any application class
(usually some service or frontend class) can use the CryptoConfig
class to provide the always required configuration entries. It is
expected that the properties:

* algorithm - name of the cipher
* passphrase - secret passphrase
* iterationCount - number of iterations performed by the cipher

are set once e.g. via dependency injection and the application
object would receive them for using.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Ondřej Hujňák <ihujnak@fit.vutbr.cz>

src/util/CryptoConfig.cpp
@@ -1,0 +1,80 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include "di/Injectable.h"
+#include "util/CryptoConfig.h"
+#include "util/CryptoParams.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, CryptoConfig)
+BEEEON_OBJECT_TEXT("algorithm", &CryptoConfig::setAlgorithm)
+BEEEON_OBJECT_TEXT("passphrase", &CryptoConfig::setPassphrase)
+BEEEON_OBJECT_NUMBER("interationCount", &CryptoConfig::setIterationCount)
+BEEEON_OBJECT_END(BeeeOn, CryptoConfig)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Crypto;
+using namespace BeeeOn;
+
+const std::string CryptoConfig::DEFAULT_ALGORITHM = "aes256";
+
+CryptoConfig::CryptoConfig():
+	m_algorithm(DEFAULT_ALGORITHM),
+	m_iterationCount(CipherKey::DEFAULT_ITERATION_COUNT)
+{
+}
+
+void CryptoConfig::setAlgorithm(const string &name)
+{
+	m_algorithm = name;
+}
+
+void CryptoConfig::setPassphrase(const string &passphrase)
+{
+	m_passphrase = passphrase;
+}
+
+void CryptoConfig::setIterationCount(const int count)
+{
+	if (count < 0)
+		throw InvalidArgumentException("iteration count must be non-negative");
+
+	if (count < CipherKey::DEFAULT_ITERATION_COUNT) {
+		logger().critical(
+			"iteration count "
+			+ to_string(count)
+			+ " is too low for enough security",
+			__FILE__, __LINE__
+		);
+	}
+
+	m_iterationCount = count;
+}
+
+/**
+ * The number of iterations does not have to match because
+ * it is a parameter of crypted data that we are (very probably)
+ * going to decrypt after this call.
+ */
+CipherKey CryptoConfig::createKey(const CryptoParams &params) const
+{
+	if (params.algorithm() != m_algorithm) {
+		throw InvalidArgumentException(
+			"inappropriate algorithm "
+			+ params.algorithm()
+			+ ", required "
+			+ m_algorithm
+		);
+	}
+
+	return params.createKey(m_passphrase);
+}
+
+CryptoParams CryptoConfig::deriveParams(const string &salt) const
+{
+	CryptoParams params = CryptoParams::create(m_algorithm);
+	params.setIterationCount(m_iterationCount);
+	params.setSalt(salt.empty()? salt : CryptoParams::randomString(32));
+
+	return params;
+}

src/CMakeLists.txt
@@ -34,6 +34,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/ssl/SSLClient.cpp
 	${PROJECT_SOURCE_DIR}/ssl/SSLServer.cpp
 	${PROJECT_SOURCE_DIR}/util/Backtrace.cpp
+	${PROJECT_SOURCE_DIR}/util/CryptoConfig.cpp
 	${PROJECT_SOURCE_DIR}/util/CryptoParams.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMM.cpp
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParser.cpp

src/util/CryptoConfig.h
@@ -1,0 +1,50 @@
+#ifndef BEEEON_CRYPTO_CONFIG_H
+#define BEEEON_CRYPTO_CONFIG_H
+
+#include <string>
+
+#include <Poco/Crypto/CipherKey.h>
+
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class CryptoParams;
+
+class CryptoConfig : public Loggable {
+public:
+	static const std::string DEFAULT_ALGORITHM;
+
+	/**
+	 * Construct the config with default parameters:
+	 *
+	 * - algorithm: "aes256",
+	 * - iterationCount: CipherKey::DEFAULT_ITERATION_COUNT
+	 */
+	CryptoConfig();
+
+	void setAlgorithm(const std::string &name);
+	void setPassphrase(const std::string &passphrase);
+	void setIterationCount(const int count);
+
+	/**
+	 * Create appropriate key based on the given params.
+	 * @throws InvalidArgumentException when params are incompatible
+	 */
+	Poco::Crypto::CipherKey createKey(const CryptoParams &params) const;
+
+	/**
+	 * Derive appropriate params based on this configuration.
+	 * If the given salt is empty a random salt is generated.
+	 */
+	CryptoParams deriveParams(const std::string &salt = "") const;
+
+private:
+	std::string m_algorithm;
+	std::string m_passphrase;
+	int m_iterationCount;
+};
+
+}
+
+#endif

commit 170a757e68420c6ad058ff98d5a000223c1e1f8e

CryptoConfig: fix salt generation on deriveParams
This is a security issue because salts are never generated.

Fixes: 4eed82d05eac ("CryptoConfig: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/CryptoConfig.cpp
@@ -74,7 +74,7 @@ CryptoParams CryptoConfig::deriveParams(const string &salt) const
 {
 	CryptoParams params = CryptoParams::create(m_algorithm);
 	params.setIterationCount(m_iterationCount);
-	params.setSalt(salt.empty()? salt : CryptoParams::randomString(32));
+	params.setSalt(salt.empty()? CryptoParams::randomString(32) : salt);
 
 	return params;
 }