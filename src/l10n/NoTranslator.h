b83f88a090e5c51a81059fa48b6e4dbaa91aa076
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-05 16:48:48 +0200

    NoTranslator: new class
    Implementation of Translator that never translates anything. It just
    returns the given key and all arguments in brackets. This may be useful
    for debugging or for situations when no translations are available
    in the initial development stages.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/l10n/NoTranslator.h
@@ -0,0 +1,27 @@
+#ifndef BEEEON_NO_TRANSLATOR_H
+#define BEEEON_NO_TRANSLATOR_H
+
+#include "l10n/Translator.h"
+
+namespace BeeeOn {
+
+/**
+ * The NoTranslator returns the key without any formatting.
+ */
+class NoTranslator : public Translator {
+public:
+	std::string formatImpl(
+		const std::string &key,
+		const std::vector<Poco::Dynamic::Var> &args) override;
+};
+
+class NoTranslatorFactory : public TranslatorFactory {
+public:
+	Translator *create(
+		const Locale &locale,
+		const std::string &name = "") override;
+};
+
+}
+
+#endif
