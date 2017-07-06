9e057105cea3574abdf429a2648dcce651d29ced
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-06 11:34:14 +0200

    Locale: new class
    This class is intended for application use. It allows to hide
    implementation-specific details easily. It does not have to
    be allocated on heap and it is copyable. All access to the
    locale is read-only and thus it can work in threaded environments.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/l10n/Locale.h
@@ -0,0 +1,49 @@
+#ifndef BEEEON_LOCALE_H
+#define BEEEON_LOCALE_H
+
+#include <string>
+
+#include <Poco/Exception.h>
+#include <Poco/SharedPtr.h>
+
+#include "l10n/LocaleImpl.h"
+
+namespace BeeeOn {
+
+class Locale {
+public:
+	/**
+	 * Creates system locale.
+	 */
+	Locale();
+
+	template <typename T>
+	Locale(Poco::SharedPtr<T> impl):
+		m_impl(impl)
+	{
+		if (m_impl.isNull())
+			throw Poco::BadCastException("failed to cast to LocaleImpl");
+	}
+
+	template <typename T>
+	Locale(T *impl):
+		m_impl(impl)
+	{
+	}
+
+	std::string language() const;
+	std::string country() const;
+	std::string displayName() const;
+	std::string toString() const;
+
+	Poco::SharedPtr<LocaleImpl> impl() const;
+
+	static Locale system();
+
+private:
+	Poco::SharedPtr<LocaleImpl> m_impl;
+};
+
+}
+
+#endif
