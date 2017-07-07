BeeeOn/base

commit a981f2f705850ec116c94d90539175d9d5633142

SimpleCalc: new class
Implement a simple math expression evaluator. It implements only
the most basic operations and it does not support operator
precedence resolution. No brackets are supported.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/math/SimpleCalc.cpp
@@ -1,0 +1,202 @@
+#include <cmath>
+
+#include <Poco/Ascii.h>
+#include <Poco/Exception.h>
+#include <Poco/NumberParser.h>
+#include <Poco/String.h>
+
+#include "math/SimpleCalc.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+double SimpleCalc::evaluate(const string &input) const
+{
+	enum State {
+		S_INIT,
+		S_TERM_OP_TERM,
+	};
+
+	State state = S_INIT;
+
+	const string &trimmed = trim(input);
+	auto at = trimmed.begin();
+	const auto end = trimmed.end();
+
+	if (at == end)
+		throw SyntaxException("expected <term> but got nothing");
+
+	double result = NAN;
+	double tmp = NAN;
+	char op = '\0';
+
+	while (at != end) {
+		switch (state) {
+		case S_INIT:
+			result = parseTerm(at, end);
+			break;
+
+		case S_TERM_OP_TERM:
+			tmp = parseTerm(at, end);
+			apply(result, op, tmp);
+			break;
+		}
+
+		op = parseOpOrEOF(at, end);
+		if (op == '\0')
+			return result;
+
+		state = S_TERM_OP_TERM;
+	}
+
+	if (op != '\0')
+		throw SyntaxException("missing <term> after <op>: " + to_string(op));
+
+	return result;
+}
+
+double SimpleCalc::parseTerm(
+	string::const_iterator &at,
+	string::const_iterator end) const
+{
+	skipWhitespace(at, end);
+
+	enum State {
+		S_BEGIN,
+		S_ZERO,
+		S_DIGIT,
+		S_FLOAT
+	};
+
+	const string::const_iterator begin = at;
+	State state = S_BEGIN;
+
+	for (; at != end; ++at) {
+		const char c = *at;
+
+		switch (state) {
+		case S_BEGIN:
+			if (c == '0')
+				state = S_ZERO;
+			else if (Ascii::isDigit(c))
+				state = S_DIGIT;
+			else if (c == '.')
+				state = S_FLOAT;
+			else
+				throw SyntaxException("unexpected character " + to_string(c) + " for <term>");
+
+			break;
+		case S_ZERO:
+			if (c == '0')
+				continue;
+			else if (c == '.')
+				state = S_FLOAT;
+			else if (isWhitespace(c) || isOperator(c))
+				return 0.0;
+			else
+				throw SyntaxException("unexpected content after <term>: " + to_string(c));
+			break;
+		case S_DIGIT:
+			if (Ascii::isDigit(c))
+				continue;
+			else if (c == '.')
+				state = S_FLOAT;
+			else if (isWhitespace(c) || isOperator(c))
+				return NumberParser::parse({begin, at});
+			else
+				throw SyntaxException("unexpected content after <term>: " + to_string(c));
+			break;
+		case S_FLOAT:
+			if (Ascii::isDigit(c))
+				continue;
+			else if (isWhitespace(c) || isOperator(c))
+				return NumberParser::parseFloat({begin, at});
+			else
+				throw SyntaxException("unexpected content after <term>: " + to_string(c));
+			break;
+		default:
+			throw IllegalStateException("no such <term> state, this is a serious bug");
+		}
+	}
+
+	// parse whole begin..end
+	switch (state) {
+	case S_BEGIN:
+		throw SyntaxException("expected <term> but found nothing");
+	case S_ZERO:
+		return 0.0;
+	case S_DIGIT:
+		return NumberParser::parse({begin, at});
+	case S_FLOAT:
+		return NumberParser::parseFloat({begin, at});
+	}
+
+	throw IllegalStateException("no such <term> state, this is a serious bug");
+}
+
+char SimpleCalc::parseOpOrEOF(
+	string::const_iterator &at,
+	string::const_iterator end) const
+{
+	skipWhitespace(at, end);
+
+	if (at == end)
+		return '\0';
+
+	const char op = *at;
+
+	if (isOperator(op)) {
+		++at;
+		return op;
+	}
+
+	throw SyntaxException("unexpected character: " + to_string(op) + " for <op>");
+}
+
+void SimpleCalc::apply(double &result, char op, double tmp) const
+{
+	switch (op) {
+	case '+':
+		result += tmp;
+		break;
+	case '-':
+		result -= tmp;
+		break;
+	case '*':
+		result *= tmp;
+		break;
+	case '/':
+		if (tmp == 0.0)
+			throw IllegalStateException("division by zero detected");
+
+		result /= tmp;
+		break;
+	default:
+		throw IllegalStateException("invalid operation: " + to_string(op) + ", this is a bug");
+	}
+}
+
+void SimpleCalc::skipWhitespace(
+	string::const_iterator &at,
+	string::const_iterator end) const
+{
+	while (at != end && isWhitespace(*at))
+		++at;
+}
+
+bool SimpleCalc::isWhitespace(const char c) const
+{
+	return Ascii::isSpace(c);
+}
+
+bool SimpleCalc::isOperator(const char c) const
+{
+	static string operators = "+-*/";
+	return operators.find(c) != string::npos;
+}
+
+bool SimpleCalc::isTerm(const char c) const
+{
+	return Ascii::isDigit(c) || c == '.';
+}

src/CMakeLists.txt
@@ -26,6 +26,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp
+	${PROJECT_SOURCE_DIR}/math/SimpleCalc.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceID.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceInfo.cpp
 	${PROJECT_SOURCE_DIR}/model/DevicePrefix.cpp

src/math/SimpleCalc.h
@@ -1,0 +1,42 @@
+#ifndef BEEEON_SIMPLE_CALC_H
+#define BEEEON_SIMPLE_CALC_H
+
+namespace BeeeOn {
+
+/**
+ * Very simple evaluator of math expressions. It performs operations
+ * with left-associativity. Thus, there is no operator precedence
+ * applicated. Examples:
+ *
+ *  y = 2 + 5 * 1 - 3 / 2 * 5
+ *    -> 7 * 1 - 3 / 2 * 5
+ *    -> 7 - 3 / 2 * 5
+ *    -> 4 / 2 * 5
+ *    -> 2 * 5
+ *    -> 10
+ */
+class SimpleCalc {
+public:
+	double evaluate(const std::string &input) const;
+
+protected:
+	double parseTerm(
+		std::string::const_iterator &at,
+		std::string::const_iterator end) const;
+	char parseOpOrEOF(
+		std::string::const_iterator &at,
+		std::string::const_iterator end) const;
+	void skipWhitespace(
+		std::string::const_iterator &at,
+		std::string::const_iterator end) const;
+
+	void apply(double &result, char op, double tmp) const;
+
+	bool isWhitespace(const char c) const;
+	bool isOperator(const char c) const;
+	bool isTerm(const char c) const;
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -25,6 +25,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/io/ConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/io/TCPConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunnerTest.cpp
+	${PROJECT_SOURCE_DIR}/math/SimpleCalcTest.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceIDTest.cpp
 	${PROJECT_SOURCE_DIR}/net/IPAddressRangeTest.cpp

test/math/SimpleCalcTest.cpp
@@ -1,0 +1,199 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Exception.h>
+
+#include "cppunit/BetterAssert.h"
+#include "math/SimpleCalc.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class SimpleCalcTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(SimpleCalcTest);
+	CPPUNIT_TEST(testConstant);
+	CPPUNIT_TEST(testTermOpTerm);
+	CPPUNIT_TEST(testLeadingWhitespace);
+	CPPUNIT_TEST(testEndingWhitespace);
+	CPPUNIT_TEST(testLongExpression);
+	CPPUNIT_TEST(testMissingTerm);
+	CPPUNIT_TEST(testGarbage);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testConstant();
+	void testTermOpTerm();
+	void testLeadingWhitespace();
+	void testEndingWhitespace();
+	void testLongExpression();
+	void testMissingTerm();
+	void testGarbage();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SimpleCalcTest);
+
+void SimpleCalcTest::testConstant()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("0"));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("0.0"));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate(".0"));
+	CPPUNIT_ASSERT_EQUAL(1.0, calc.evaluate("1"));
+	CPPUNIT_ASSERT_EQUAL(1234567890.0, calc.evaluate("1234567890"));
+	CPPUNIT_ASSERT_EQUAL(123456789.0, calc.evaluate("123456789.0"));
+	CPPUNIT_ASSERT_EQUAL(12345678.9, calc.evaluate("12345678.90"));
+	CPPUNIT_ASSERT_EQUAL(1234567.89, calc.evaluate("1234567.890"));
+	CPPUNIT_ASSERT_EQUAL(123456.789, calc.evaluate("123456.7890"));
+	CPPUNIT_ASSERT_EQUAL(12345.6789, calc.evaluate("12345.67890"));
+	CPPUNIT_ASSERT_EQUAL(1234.56789, calc.evaluate("1234.567890"));
+	CPPUNIT_ASSERT_EQUAL(123.456789, calc.evaluate("123.4567890"));
+	CPPUNIT_ASSERT_EQUAL(12.3456789, calc.evaluate("12.34567890"));
+	CPPUNIT_ASSERT_EQUAL(1.23456789, calc.evaluate("1.234567890"));
+	CPPUNIT_ASSERT_EQUAL(.123456789, calc.evaluate(".1234567890"));
+}
+
+void SimpleCalcTest::testTermOpTerm()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("0 + 0"));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("0 - 0"));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("0 * 0"));
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("0 / 0"),
+		IllegalStateException
+	);
+
+	CPPUNIT_ASSERT_EQUAL(2.0, calc.evaluate("1 + 1"));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("1 - 1"));
+	CPPUNIT_ASSERT_EQUAL(1.0, calc.evaluate("1 * 1"));
+	CPPUNIT_ASSERT_EQUAL(1.0, calc.evaluate("1 / 1"));
+
+	CPPUNIT_ASSERT_EQUAL(2.5, calc.evaluate("1.5 + 1"));
+	CPPUNIT_ASSERT_EQUAL(0.5, calc.evaluate("1.5 - 1"));
+	CPPUNIT_ASSERT_EQUAL(1.5, calc.evaluate("1.5 * 1"));
+	CPPUNIT_ASSERT_EQUAL(1.5, calc.evaluate("1.5 / 1"));
+}
+
+void SimpleCalcTest::testLeadingWhitespace()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("     0"));
+	CPPUNIT_ASSERT_EQUAL(1.0, calc.evaluate("     1"));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("     .0"));
+	CPPUNIT_ASSERT_EQUAL(1243.0, calc.evaluate("     1243"));
+	CPPUNIT_ASSERT_EQUAL(1243.0, calc.evaluate(" \t  1243"));
+}
+
+void SimpleCalcTest::testEndingWhitespace()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate("0   "));
+	CPPUNIT_ASSERT_EQUAL(1.0, calc.evaluate("1    "));
+	CPPUNIT_ASSERT_EQUAL(0.0, calc.evaluate(".0    "));
+	CPPUNIT_ASSERT_EQUAL(123.4, calc.evaluate("123.4    "));
+	CPPUNIT_ASSERT_EQUAL(1243.0, calc.evaluate("1243     "));
+	CPPUNIT_ASSERT_EQUAL(1243.0, calc.evaluate("1243  \t  "));
+}
+
+void SimpleCalcTest::testLongExpression()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_EQUAL(10.0, calc.evaluate("2 + 5 * 1 - 3 / 2 * 5"));
+	CPPUNIT_ASSERT_EQUAL(10.0, calc.evaluate(" 2  + \t5    *\t\t 1  - 3   /  2  * 5  "));
+	CPPUNIT_ASSERT_EQUAL(10.0, calc.evaluate("2+5*1-3/2*5"));
+
+	CPPUNIT_ASSERT_EQUAL(1.0, calc.evaluate("1000 / 10 / 10 / 10"));
+	CPPUNIT_ASSERT_EQUAL(1000.0, calc.evaluate("10 * 10 * 10"));
+}
+
+void SimpleCalcTest::testMissingTerm()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate(""),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("   "),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("   ."),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("5 +"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("5 + "),
+		SyntaxException
+	);
+}
+
+void SimpleCalcTest::testGarbage()
+{
+	SimpleCalc calc;
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("125q"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("0.r"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("x0"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("*0"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("/0"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("-0"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("+0"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("  0 0"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("123 +1g"),
+		SyntaxException
+	);
+
+	CPPUNIT_ASSERT_THROW(
+		calc.evaluate("123 +1 g"),
+		SyntaxException
+	);
+}
+
+}

commit dfacb9dfb0164f097b6aa731bec5c9e9b53eaf6e

SimpleCalc: fix reporting invalid characters
Previously, the respoted output was an ASCII number instead of
the actual character.

Fixes: a981f2f70585 ("SimpleCalc: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/math/SimpleCalc.cpp
@@ -86,7 +86,7 @@ double SimpleCalc::parseTerm(
 			else if (c == '-')
 				continue;
 			else
-				throw SyntaxException("unexpected character " + to_string(c) + " for <term>");
+				throw SyntaxException("unexpected character " + string(1, c) + " for <term>");
 
 			break;
 		case S_ZERO:
@@ -153,7 +153,7 @@ char SimpleCalc::parseOpOrEOF(
 		return op;
 	}
 
-	throw SyntaxException("unexpected character: " + to_string(op) + " for <op>");
+	throw SyntaxException("unexpected character: " + string(1, op) + " for <op>");
 }
 
 void SimpleCalc::apply(double &result, char op, double tmp) const