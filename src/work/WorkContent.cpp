BeeeOn/base

commit 38e86279e07b830649286c2a0bfcd7c149c7c52d

WorkContent: new class
WorkContent represents the actual work to be executed. It is
basically an object with properties. This implementation
utilizes JSON::Object::Ptr as a backend. To interpret the
contents of the WorkContent, the target must understand it.
To quickly detect the content type, the property type() is
defined.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/work/WorkContent.cpp
@@ -1,0 +1,52 @@
+#include <Poco/Dynamic/Var.h>
+
+#include "work/WorkContent.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+using namespace BeeeOn;
+
+WorkContent::WorkContent()
+{
+}
+
+WorkContent::WorkContent(const WorkContent &copy):
+	m_type(copy.m_type),
+	m_json(new Object(*copy.m_json))
+{
+}
+
+WorkContent::WorkContent(const ClassInfo &type):
+	m_type(type),
+	m_json(new Object)
+{
+	m_json->set("type", type.name());
+}
+
+WorkContent::WorkContent(const Object::Ptr json):
+	m_type(ClassInfo::byName(json->get("type"))),
+	m_json(json)
+{
+}
+
+void WorkContent::setType(const ClassInfo &type)
+{
+	m_type = type;
+	m_json->set("type", type.name());
+}
+
+ClassInfo WorkContent::type() const
+{
+	return m_type;
+}
+
+string WorkContent::toString() const
+{
+	return Dynamic::Var::toString(m_json);
+}
+
+Object::Ptr WorkContent::json() const
+{
+	return m_json;
+}

src/CMakeLists.txt
@@ -77,6 +77,7 @@ file(GLOB WORK_SOURCES
 	${PROJECT_SOURCE_DIR}/work/GenericWorkRunner.cpp
 	${PROJECT_SOURCE_DIR}/work/Work.cpp
 	${PROJECT_SOURCE_DIR}/work/WorkBackup.cpp
+	${PROJECT_SOURCE_DIR}/work/WorkContent.cpp
 	${PROJECT_SOURCE_DIR}/work/WorkExecutor.cpp
 	${PROJECT_SOURCE_DIR}/work/WorkFactory.cpp
 	${PROJECT_SOURCE_DIR}/work/WorkLockManager.cpp

src/work/WorkContent.h
@@ -1,0 +1,34 @@
+#ifndef BEEEON_WORK_CONTENT_H
+#define BEEEON_WORK_CONTENT_H
+
+#include <string>
+
+#include <Poco/JSON/Object.h>
+
+#include "util/ClassInfo.h"
+
+namespace BeeeOn {
+
+class WorkContent {
+public:
+	WorkContent();
+	WorkContent(const WorkContent &copy);
+	WorkContent(const ClassInfo &type);
+	WorkContent(const Poco::JSON::Object::Ptr content);
+
+	void setType(const ClassInfo &type);
+	ClassInfo type() const;
+
+	std::string toString() const;
+
+protected:
+	Poco::JSON::Object::Ptr json() const;
+
+private:
+	ClassInfo m_type;
+	Poco::JSON::Object::Ptr m_json;
+};
+
+}
+
+#endif

commit 358c53ee63438187f6b7f3125bb4a8c5cb6351c8

WorkContent: fix missing initialization of m_json
Instances created with empty constructor are not usable because
of the missing m_json instance.

Fixes: 38e86279e07b ("WorkContent: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/work/WorkContent.cpp
@@ -7,7 +7,8 @@ using namespace Poco;
 using namespace Poco::JSON;
 using namespace BeeeOn;
 
-WorkContent::WorkContent()
+WorkContent::WorkContent():
+	m_json(new Object)
 {
 }
 