d213f0e82fbd641b49670d193b1c9b25d3433b40
David Bednařík <xbedna62@stud.fit.vutbr.cz>
2017-08-08 18:28:24 +0200

    CustomTypeID: new class
    The class is for specific ModuleType of device. When the specific
    ModuleType is chosen then CustomTypeID contains the name of
    specific module.
    
    Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
    Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ src/model/CustomTypeID.h
@@ -0,0 +1,40 @@
+#ifndef BEEEON_CUSTOM_TYPE_ID_H
+#define BEEEON_CUSTOM_TYPE_ID_H
+
+#include <string>
+
+namespace BeeeOn {
+
+/*
+ * @brief The class is intended to contain a specific
+ * ModuleType Type name.
+ */
+class CustomTypeID {
+public:
+	enum {
+		MAX_LENGTH = 64
+	};
+
+	CustomTypeID();
+	CustomTypeID(const std::string &id);
+
+	std::string toString() const;
+
+	static CustomTypeID parse(const std::string &input);
+
+	bool isNull() const;
+
+	bool operator ==(const CustomTypeID &id) const;
+	bool operator !=(const CustomTypeID &id) const;
+	bool operator <(const CustomTypeID &id) const;
+	bool operator >(const CustomTypeID &id) const;
+	bool operator <=(const CustomTypeID &id) const;
+	bool operator >=(const CustomTypeID &id) const;
+
+private:
+	std::string m_value;
+};
+
+}
+
+#endif
