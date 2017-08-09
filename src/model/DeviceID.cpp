BeeeOn/base

commit 19605877e9d0c34f99ddfe579d255d6a3eff1597

DeviceID: utilize NumberParser for parsing
We prefer the NumberParser over std parsing methods in the project.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/model/DeviceID.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 
 #include <Poco/Exception.h>
+#include <Poco/NumberParser.h>
 #include <Poco/Random.h>
 
 #include "model/DeviceID.h"
@@ -44,21 +45,12 @@ DeviceID::DeviceID(const DevicePrefix &prefix, uint64_t ident)
 
 DeviceID DeviceID::parse(const string &s)
 {
-	unsigned long long v;
+	uint64_t v;
 
-	try {
-		v = stoull(s, NULL, 0);
-	}
-	catch (invalid_argument &e) {
-		throw InvalidArgumentException(
-			string("parse device ID: ") + e.what());
-	}
-	catch (std::out_of_range &e) {
-		throw InvalidArgumentException(
-			"device ID is out-of-range");
-	}
+	if (NumberParser::tryParseHex64(s, v))
+		return DeviceID(v);
 
-	return DeviceID((uint64_t) v);
+	return NumberParser::parseUnsigned64(s);
 }
 
 string DeviceID::toString() const

commit b3961b69ca9a06a5818dd187f006d648060a4dba

DeviceID: fix parsing of non-hex values
Fixes: 19605877e9d0 ("DeviceID: utilize NumberParser for parsing")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/model/DeviceID.cpp
@@ -45,10 +45,8 @@ DeviceID::DeviceID(const DevicePrefix &prefix, uint64_t ident)
 
 DeviceID DeviceID::parse(const string &s)
 {
-	uint64_t v;
-
-	if (NumberParser::tryParseHex64(s, v))
-		return DeviceID(v);
+	if (s.size() >= 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+		return NumberParser::parseHex64(s);
 
 	return NumberParser::parseUnsigned64(s);
 }