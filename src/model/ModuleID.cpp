BeeeOn/base

commit 6a0d935e522c0fecba1f5fdb22e78dc8b879dbda

ModuleID: move from gateway to base
Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/ModuleID.cpp
@@ -1,0 +1,40 @@
+#include <climits>
+
+#include <Poco/NumberParser.h>
+#include <Poco/NumberFormatter.h>
+
+#include "model/ModuleID.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+const uint16_t DEFAULT_MODULE_ID = 0;
+
+ModuleID::ModuleID():
+	m_moduleID(DEFAULT_MODULE_ID)
+{
+}
+
+ModuleID::ModuleID(const uint16_t &moduleID):
+	m_moduleID(moduleID)
+{
+}
+
+ModuleID ModuleID::parse(const string &s)
+{
+	unsigned int moduleID;
+
+	moduleID = Poco::NumberParser::parseUnsigned(s);
+	if (moduleID > USHRT_MAX)
+		throw RangeException(
+			"module ID too high: " + to_string(moduleID));
+
+	return ModuleID(moduleID);
+}
+
+string ModuleID::toString() const
+{
+	return Poco::NumberFormatter::formatHex(m_moduleID);
+}
+

src/CMakeLists.txt
@@ -33,6 +33,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/DevicePrefix.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayID.cpp
 	${PROJECT_SOURCE_DIR}/model/GlobalID.cpp
+	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleInfo.cpp
 	${PROJECT_SOURCE_DIR}/model/SimpleID.cpp
 	${PROJECT_SOURCE_DIR}/model/TypeInfo.cpp

src/model/ModuleID.h
@@ -1,0 +1,77 @@
+#ifndef BEEEON_MODULE_ID_H
+#define BEEEON_MODULE_ID_H
+
+#include <cstdint>
+#include <string>
+
+namespace BeeeOn {
+
+/*
+ * Represents the identification of a sensor module, type of a measured value
+ * (temperature, pressure, motion, ...).
+ */
+class ModuleID {
+public:
+	/*
+	 * Construct ModuleID with default ModuleID.
+	 */
+	ModuleID();
+
+	ModuleID(const uint16_t &moduleID);
+
+	uint16_t value() const
+	{
+		return m_moduleID;
+	}
+
+	static ModuleID parse(const std::string &s);
+
+	std::string toString() const;
+
+	bool operator !=(const ModuleID &id) const
+	{
+		return m_moduleID != id.m_moduleID;
+	}
+
+	bool operator ==(const ModuleID &id) const
+	{
+		return m_moduleID == id.m_moduleID;
+	}
+
+	bool operator <(const ModuleID &id) const
+	{
+		return m_moduleID < id.m_moduleID;
+	}
+
+	bool operator >(const ModuleID &id) const
+	{
+		return m_moduleID > id.m_moduleID;
+	}
+
+	bool operator <=(const ModuleID &id) const
+	{
+		return m_moduleID <= id.m_moduleID;
+	}
+
+	bool operator >=(const ModuleID &id) const
+	{
+		return m_moduleID >= id.m_moduleID;
+	}
+
+	operator unsigned short() const
+	{
+		return m_moduleID;
+	}
+
+	operator uint16_t()
+	{
+		return m_moduleID;
+	}
+
+private:
+	uint16_t m_moduleID;
+};
+
+}
+
+#endif

commit 0f43687f85be17d185c3a42d23213091db850141

ModuleID: fix coding style
Removed unnecessary empty line.

Fixes: 6a0d935e522c ("ModuleID: move from gateway to base")

Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/ModuleID.cpp
@@ -37,4 +37,3 @@ string ModuleID::toString() const
 {
 	return Poco::NumberFormatter::formatHex(m_moduleID);
 }
-