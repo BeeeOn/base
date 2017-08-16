BeeeOn/base

commit acdcf365d1cee08f34d6d655afcf310070d2f9a2

TCPConsole: new Console implementation
Implements Console API for TCP communication. Note, that the TCPConsole
does not implement any security layer. Thus, it is not suitable for a
serious network communication. The purpose of this class is to enable
a debugging or testing support for applications that cannot communicate
via stdin and stdout directly (background processes).

The TCPConsole listens on 127.0.0.1:6000 as default.

Note that the TCPConsoleTest is very limited due to a non-deterministic
behaviour of socket connections in some environment.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/io/TCPConsole.cpp
@@ -1,0 +1,152 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+
+#include <Poco/Net/ServerSocket.h>
+#include <Poco/Net/SocketAddress.h>
+
+#include "di/Injectable.h"
+#include "io/TCPConsole.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, TCPConsole)
+BEEEON_OBJECT_CASTABLE(Console)
+BEEEON_OBJECT_TEXT("address", &TCPConsole::setAddress)
+BEEEON_OBJECT_NUMBER("port", &TCPConsole::setPort)
+BEEEON_OBJECT_NUMBER("sendTimeout", &TCPConsole::setSendTimeout)
+BEEEON_OBJECT_NUMBER("recvTimeout", &TCPConsole::setRecvTimeout)
+BEEEON_OBJECT_NUMBER("backlog", &TCPConsole::setBacklog)
+BEEEON_OBJECT_TEXT("eol", &TCPConsole::setEol)
+BEEEON_OBJECT_TEXT("skipEol", &TCPConsole::setSkipEol)
+BEEEON_OBJECT_TEXT("prompt", &TCPConsole::setPrompt)
+BEEEON_OBJECT_HOOK("done", &TCPConsole::startListen)
+BEEEON_OBJECT_END(BeeeOn, TCPConsole)
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace BeeeOn;
+
+TCPConsoleSessionImpl::TCPConsoleSessionImpl(Socket &socket):
+	m_socket(socket),
+	m_peerAddress(socket.peerAddress()),
+	m_input(socket),
+	m_output(socket)
+{
+	logger().notice("accepted connection from "
+			+ m_peerAddress.toString(),
+			__FILE__, __LINE__);
+
+}
+
+TCPConsoleSessionImpl::~TCPConsoleSessionImpl()
+{
+	logger().notice("closing connection from "
+			+ m_peerAddress.toString(),
+			__FILE__, __LINE__);
+
+	m_input.close();
+	m_output.close();
+	m_socket.close();
+}
+
+string TCPConsoleSessionImpl::readUntil(const char c)
+{
+	return iosReadUntil(m_input, c);
+}
+
+string TCPConsoleSessionImpl::readBytes(const unsigned int length)
+{
+	return iosReadBytes(m_input, length);
+}
+
+void TCPConsoleSessionImpl::print(const string &text, bool newline)
+{
+	iosPrint(m_output, text, newline);
+}
+
+bool TCPConsoleSessionImpl::eof()
+{
+	return m_input.eof();
+}
+
+TCPConsole::TCPConsole():
+	m_address("127.0.0.1"),
+	m_port(DEFAULT_PORT),
+	m_sendTimeout(DEFAULT_TIMEOUT_MS * Timespan::MILLISECONDS),
+	m_recvTimeout(DEFAULT_TIMEOUT_MS * Timespan::MILLISECONDS),
+	m_backlog(DEFAULT_PORT)
+{
+}
+
+void TCPConsole::setAddress(const string &address)
+{
+	m_address = IPAddress(address);
+}
+
+void TCPConsole::setPort(int port)
+{
+	if (port < 0 || port > 0x0ffff)
+		throw InvalidArgumentException("port must be in range 0..65535");
+
+	m_port = port;
+}
+
+void TCPConsole::setSendTimeout(int ms)
+{
+	if (ms < 0)
+		throw InvalidArgumentException("sendTimeout must be non-negative");
+
+	m_sendTimeout = Timespan(ms * Timespan::MILLISECONDS);
+}
+
+void TCPConsole::setRecvTimeout(int ms)
+{
+	if (ms < 0)
+		throw InvalidArgumentException("recvTimeout must be non-negative");
+
+	m_recvTimeout = Timespan(ms * Timespan::MILLISECONDS);
+}
+
+
+void TCPConsole::setBacklog(int backlog)
+{
+	if (backlog <= 0)
+		throw InvalidArgumentException("backlog must be greater then 0");
+
+	m_backlog = backlog;
+}
+
+void TCPConsole::startListen()
+{
+	FastMutex::ScopedLock guard(m_lock);
+	startListenUnlocked();
+}
+
+void TCPConsole::startListenUnlocked()
+{
+	if (!m_serverSocket.isNull())
+		return;
+
+	m_serverSocket = new ServerSocket;
+
+	SocketAddress address(m_address, m_port);
+	m_serverSocket->bind(address, true);
+	m_serverSocket->listen(m_backlog);
+
+	logger().information("listening at " + address.toString(),
+			__FILE__, __LINE__);
+}
+
+ConsoleSessionImpl::Ptr TCPConsole::openSession()
+{
+	FastMutex::ScopedLock guard(m_lock);
+	startListenUnlocked();
+
+	logger().information("waiting for connection...",
+			__FILE__, __LINE__);
+
+	Socket socket = m_serverSocket->acceptConnection();
+	socket.setSendTimeout(m_sendTimeout);
+	socket.setReceiveTimeout(m_recvTimeout);
+
+	return new TCPConsoleSessionImpl(socket);
+}

src/io/TCPConsole.h
@@ -1,0 +1,86 @@
+#ifndef BEEEON_TCP_CONSOLE_H
+#define BEEEON_TCP_CONSOLE_H
+
+#include <iosfwd>
+
+#include <Poco/Mutex.h>
+#include <Poco/Semaphore.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+
+#include <Poco/Net/IPAddress.h>
+#include <Poco/Net/Socket.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Net/SocketStream.h>
+
+#include "io/Console.h"
+#include "util/Loggable.h"
+
+namespace Poco {
+namespace Net {
+
+class ServerSocket;
+
+}
+}
+
+namespace BeeeOn {
+
+class TCPConsoleSessionImpl : public ConsoleSessionImpl, protected Loggable {
+public:
+	typedef Poco::SharedPtr<TCPConsoleSessionImpl> Ptr;
+
+	TCPConsoleSessionImpl(Poco::Net::Socket &socket);
+	~TCPConsoleSessionImpl();
+
+	std::string readUntil(const char c) override;
+	std::string readBytes(const unsigned int length) override;
+	void print(const std::string &text, bool newline = true) override;
+	bool eof() override;
+
+private:
+	Poco::Net::Socket m_socket;
+	Poco::Net::SocketAddress m_peerAddress;
+	Poco::Net::SocketInputStream m_input;
+	Poco::Net::SocketOutputStream m_output;
+};
+
+/**
+ * Console over TCP protocol. It allows to open multiple console
+ * connections in parallel if the upper layer can handle them.
+ */
+class TCPConsole : public Console, protected Loggable {
+public:
+	TCPConsole();
+
+	enum {
+		DEFAULT_PORT = 6000,
+		DEFAULT_BACKLOG = 64,
+		DEFAULT_TIMEOUT_MS = 500,
+	};
+
+	void setAddress(const std::string &address);
+	void setPort(int port);
+	void setSendTimeout(int ms);
+	void setRecvTimeout(int ms);
+	void setBacklog(int backlog);
+
+	void startListen();
+
+protected:
+	ConsoleSessionImpl::Ptr openSession() override;
+	void startListenUnlocked();
+
+private:
+	Poco::Net::IPAddress m_address;
+	unsigned int m_port;
+	Poco::Timespan m_sendTimeout;
+	Poco::Timespan m_recvTimeout;
+	unsigned int m_backlog;
+	Poco::SharedPtr<Poco::Net::ServerSocket> m_serverSocket;
+	Poco::FastMutex m_lock;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -20,6 +20,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIApplicationConfigurationLoader.cpp
 	${PROJECT_SOURCE_DIR}/io/Console.cpp
 	${PROJECT_SOURCE_DIR}/io/StdConsole.cpp
+	${PROJECT_SOURCE_DIR}/io/TCPConsole.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp

test/CMakeLists.txt
@@ -23,6 +23,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIWrapperTest.cpp
 	${PROJECT_SOURCE_DIR}/di/DIDaemonTest.cpp
 	${PROJECT_SOURCE_DIR}/io/ConsoleTest.cpp
+	${PROJECT_SOURCE_DIR}/io/TCPConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunnerTest.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceIDTest.cpp

test/io/TCPConsoleTest.cpp
@@ -1,0 +1,165 @@
+#include <sstream>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/Environment.h>
+#include <Poco/Event.h>
+#include <Poco/NumberParser.h>
+#include <Poco/Thread.h>
+#include <Poco/RunnableAdapter.h>
+
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Net/StreamSocket.h>
+
+#include "cppunit/BetterAssert.h"
+#include "io/TCPConsole.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+
+namespace BeeeOn {
+
+#define TIMEOUT_SECS  4
+#define TIMEOUT_MSECS (TIMEOUT_SECS * 1000)
+
+/**
+ * Multiple tests of sockets is unreliable on some machines
+ * or operating systems and thus there is only a single test
+ * that seems to work.
+ */
+class TCPConsoleTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(TCPConsoleTest);
+	CPPUNIT_TEST(testReadLine);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	TCPConsoleTest();
+
+	void testReadLine();
+
+protected:
+	void connect(StreamSocket &socket);
+	void setupConsole(TCPConsole &console) const;
+	void echoLoop();
+	void reconnectingLoop();
+
+private:
+	Thread m_thread;
+	RunnableAdapter<TCPConsoleTest> m_echoRunnable;
+	Event m_listening;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TCPConsoleTest);
+
+TCPConsoleTest::TCPConsoleTest():
+	m_echoRunnable(*this, &TCPConsoleTest::echoLoop)
+{
+}
+
+void TCPConsoleTest::setupConsole(TCPConsole &console) const
+{
+	console.setPort(
+		NumberParser::parse(
+			Environment::get("TEST_TCP_CONSOLE_PORT", "6000")
+		)
+	);
+
+	console.setSendTimeout(TIMEOUT_MSECS);
+	console.setRecvTimeout(TIMEOUT_MSECS);
+}
+
+/**
+ * Echo loop running in a separate thread.
+ */
+void TCPConsoleTest::echoLoop()
+{
+	TCPConsole console;
+	setupConsole(console);
+	console.startListen();
+
+	// notify tests that we have started to listen
+	m_listening.set();
+
+	ConsoleSession session(console);
+
+	while (!session.eof()) {
+		const string &input = session.readLine();
+		session.print(input);
+	}
+}
+
+void TCPConsoleTest::connect(StreamSocket &socket)
+{
+	SocketAddress address(
+		"127.0.0.1",
+		NumberParser::parse(
+			Environment::get("TEST_TCP_CONSOLE_PORT", "6000")
+		)
+	);
+
+	socket.connect(address, Timespan(TIMEOUT_SECS * Timespan::SECONDS));
+	socket.setReceiveTimeout(Timespan(TIMEOUT_SECS * Timespan::SECONDS));
+}
+
+void TCPConsoleTest::testReadLine()
+{
+	m_thread.start(m_echoRunnable);
+	CPPUNIT_ASSERT_NO_THROW(
+		m_listening.wait(TIMEOUT_MSECS)
+	);
+
+	StreamSocket socket;
+	connect(socket);
+
+	SocketInputStream in(socket);
+	SocketOutputStream out(socket);
+
+	CPPUNIT_ASSERT_EQUAL('>', in.get());
+	CPPUNIT_ASSERT_EQUAL(' ', in.get());
+
+	out << "first\n" << flush;
+
+	CPPUNIT_ASSERT_EQUAL('f', in.get());
+	CPPUNIT_ASSERT_EQUAL('i', in.get());
+	CPPUNIT_ASSERT_EQUAL('r', in.get());
+	CPPUNIT_ASSERT_EQUAL('s', in.get());
+	CPPUNIT_ASSERT_EQUAL('t', in.get());
+	CPPUNIT_ASSERT_EQUAL('\n', in.get());
+
+	CPPUNIT_ASSERT_EQUAL('>', in.get());
+	CPPUNIT_ASSERT_EQUAL(' ', in.get());
+
+	out << "second\n" << flush;
+
+	CPPUNIT_ASSERT_EQUAL('s', in.get());
+	CPPUNIT_ASSERT_EQUAL('e', in.get());
+	CPPUNIT_ASSERT_EQUAL('c', in.get());
+	CPPUNIT_ASSERT_EQUAL('o', in.get());
+	CPPUNIT_ASSERT_EQUAL('n', in.get());
+	CPPUNIT_ASSERT_EQUAL('d', in.get());
+	CPPUNIT_ASSERT_EQUAL('\n', in.get());
+
+	CPPUNIT_ASSERT_EQUAL('>', in.get());
+	CPPUNIT_ASSERT_EQUAL(' ', in.get());
+
+	out << "third\\\nx\n" << flush;
+
+	CPPUNIT_ASSERT_EQUAL('t', in.get());
+	CPPUNIT_ASSERT_EQUAL('h', in.get());
+	CPPUNIT_ASSERT_EQUAL('i', in.get());
+	CPPUNIT_ASSERT_EQUAL('r', in.get());
+	CPPUNIT_ASSERT_EQUAL('d', in.get());
+	CPPUNIT_ASSERT_EQUAL('x', in.get());
+	CPPUNIT_ASSERT_EQUAL('\n', in.get());
+
+	CPPUNIT_ASSERT_EQUAL('>', in.get());
+	CPPUNIT_ASSERT_EQUAL(' ', in.get());
+
+	socket.close();
+
+	CPPUNIT_ASSERT_NO_THROW(
+		m_thread.join(TIMEOUT_MSECS)
+	);
+}
+
+}

commit 770d15096c57f5b0d08ff2c80740ec65a9e6fb8d

TCPConsole: define destructor
Without the TCPConsole destructor, the ServerSocket is not freed.

This fixes 1 missing free in the test-suite-base.

Fixes: acdcf365d1ce ("TCPConsole: new Console implementation")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Tested-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

src/io/TCPConsole.cpp
@@ -77,6 +77,10 @@ TCPConsole::TCPConsole():
 {
 }
 
+TCPConsole::~TCPConsole()
+{
+}
+
 void TCPConsole::setAddress(const string &address)
 {
 	m_address = IPAddress(address);

src/io/TCPConsole.h
@@ -52,6 +52,7 @@ private:
 class TCPConsole : public Console, protected Loggable {
 public:
 	TCPConsole();
+	~TCPConsole();
 
 	enum {
 		DEFAULT_PORT = 6000,