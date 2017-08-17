BeeeOn/base

commit 26eff0e214ed7c03e50debfffc1d3d4b37c7677e

Translator: new interface
The Translator interface provides a mechanism for localization of
texts and messages hardcoded in the application. The backend should
translate the given text and arguments into the locale that applies
to the target Translator implementation.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/l10n/Translator.cpp
@@ -1,0 +1,7 @@
+#include "l10n/Translator.h"
+
+using namespace BeeeOn;
+
+Translator::~Translator()
+{
+}

src/l10n/Translator.h
@@ -1,0 +1,79 @@
+#ifndef BEEEON_TRANSLATOR_H
+#define BEEEON_TRANSLATOR_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <Poco/Dynamic/Var.h>
+
+namespace BeeeOn {
+
+class Locale;
+
+class Translator {
+public:
+	virtual ~Translator();
+
+	/**
+	 * Format the message identified by given key.
+	 * The method calls formatImpl internally.
+	 */
+	template <typename... Rest>
+	std::string format(
+		const std::string &key,
+		const Rest & ... rest)
+	{
+		std::vector<Poco::Dynamic::Var> tmp;
+		formatCollect(tmp, rest...);
+		return formatImpl(key, tmp);
+	}
+
+protected:
+	void prepareArg(std::vector<Poco::Dynamic::Var> &tmp, const char *first)
+	{
+		tmp.push_back(std::string(first));
+	}
+
+	template <typename Type>
+	void prepareArg(std::vector<Poco::Dynamic::Var> &tmp, const Type &value)
+	{
+		tmp.push_back(value);
+	}
+
+	void formatCollect(std::vector<Poco::Dynamic::Var> &)
+	{
+	}
+
+	template <typename First, typename... Rest>
+	void formatCollect(std::vector<Poco::Dynamic::Var> &tmp,
+		const First &first,
+		const Rest & ... rest)
+	{
+		prepareArg(tmp, first);
+		formatCollect(tmp, rest...);
+	}
+
+	/**
+	 * Implements the actual key lookup and formatting.
+	 */
+	virtual std::string formatImpl(
+		const std::string &key,
+		const std::vector<Poco::Dynamic::Var> &args) = 0;
+
+};
+
+class TranslatorFactory {
+public:
+	/**
+	 * Create an instance of Translator for the given locale
+	 * and package name.
+	 */
+	virtual Translator *create(
+			const Locale &locale,
+			const std::string &name = "") = 0;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -26,6 +26,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/io/TCPConsole.cpp
 	${PROJECT_SOURCE_DIR}/l10n/Locale.cpp
 	${PROJECT_SOURCE_DIR}/l10n/LocaleImpl.cpp
+	${PROJECT_SOURCE_DIR}/l10n/Translator.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp

commit eafef4d0b7a9d8abbf8da0f329b9363248c4adde

TranslatorFactory: define virtual destructor
Fixes: 26eff0e214ed ("Translator: new interface")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/l10n/Translator.cpp
@@ -5,3 +5,7 @@ using namespace BeeeOn;
 Translator::~Translator()
 {
 }
+
+TranslatorFactory::~TranslatorFactory()
+{
+}

src/l10n/Translator.h
@@ -65,6 +65,8 @@ protected:
 
 class TranslatorFactory {
 public:
+	virtual ~TranslatorFactory();
+
 	/**
 	 * Create an instance of Translator for the given locale
 	 * and package name.