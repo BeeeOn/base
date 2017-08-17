BeeeOn/base

commit 95b2ea0aa6855c58edc84d4c8d676c5fe1246072

LocaleImpl: new interface
Implementation-specific representation of the locale. The implementation
would depend on some third-party technology. This class provides the most
important methods that would be provided by all locale implementations.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/l10n/LocaleImpl.cpp
@@ -1,0 +1,47 @@
+#include <locale>
+#include <string>
+
+#include "l10n/LocaleImpl.h"
+
+using namespace std;
+using namespace BeeeOn;
+
+string SystemLocaleImpl::language() const
+{
+	locale loc;
+	const string &name = loc.name();
+
+	size_t pos = name.find("_");
+	if (pos == string::npos)
+		return name;
+
+	return name.substr(0, pos - 1);
+}
+
+string SystemLocaleImpl::country() const
+{
+	locale loc;
+	const string &name = loc.name();
+
+	size_t pos = name.find("_");
+	if (pos == string::npos)
+		return "";
+
+	size_t dot = name.find(".", pos);
+	if (dot == string::npos)
+		return name.substr(pos);
+
+	return name.substr(pos, (dot - pos) - 1);
+}
+
+string SystemLocaleImpl::displayName() const
+{
+	locale loc;
+	return loc.name();
+}
+
+string SystemLocaleImpl::toString() const
+{
+	locale loc;
+	return loc.name();
+}

src/l10n/LocaleImpl.h
@@ -1,0 +1,49 @@
+#ifndef BEEEON_LOCALE_IMPL_H
+#define BEEEON_LOCALE_IMPL_H
+
+namespace BeeeOn {
+
+/**
+ * Abstract representation of locale. This makes us independent
+ * on the target locale implementation.
+ */
+class LocaleImpl {
+public:
+	/**
+	 * Returns language code of the locale.
+	 */
+	virtual std::string language() const = 0;
+
+	/**
+	 * Returns country code of the locale.
+	 */
+	virtual std::string country() const = 0;
+
+	/**
+	 * Name to display to user.
+	 *
+	 * It displays its name via its locale. Thus if the locale is
+	 * en_US, you would get English (USA). If the locale is cs_CZ,
+	 * the result would be Čeština (Česká republika).
+	 */
+	virtual std::string displayName() const = 0;
+
+	/**
+	 * String representation of the locale.
+	 * It is usually a code like en_US.
+	 */
+	virtual std::string toString() const = 0;
+};
+
+class SystemLocaleImpl : public LocaleImpl {
+public:
+	std::string language() const override;
+	std::string country() const override;
+	std::string displayName() const override;
+	std::string toString() const override;
+};
+
+}
+
+#endif
+

src/CMakeLists.txt
@@ -24,6 +24,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/io/SimpleConsole.cpp
 	${PROJECT_SOURCE_DIR}/io/StdConsole.cpp
 	${PROJECT_SOURCE_DIR}/io/TCPConsole.cpp
+	${PROJECT_SOURCE_DIR}/l10n/LocaleImpl.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp

test/CMakeLists.txt
@@ -25,6 +25,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/io/AutoCloseTest.cpp
 	${PROJECT_SOURCE_DIR}/io/ConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/io/TCPConsoleTest.cpp
+	${PROJECT_SOURCE_DIR}/l10n/SystemLocaleImplTest.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunnerTest.cpp
 	${PROJECT_SOURCE_DIR}/math/SimpleCalcTest.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp

test/l10n/SystemLocaleImplTest.cpp
@@ -1,0 +1,52 @@
+#include <locale>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "cppunit/BetterAssert.h"
+#include "l10n/LocaleImpl.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class SystemLocaleImplTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(SystemLocaleImplTest);
+	CPPUNIT_TEST(testLanguage);
+	CPPUNIT_TEST(testCountry);
+	CPPUNIT_TEST(testDisplayName);
+	CPPUNIT_TEST(testToString);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testLanguage();
+	void testCountry();
+	void testDisplayName();
+	void testToString();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SystemLocaleImplTest);
+
+void SystemLocaleImplTest::testLanguage()
+{
+	SystemLocaleImpl locale;
+	CPPUNIT_ASSERT_EQUAL("C", locale.language());
+}
+
+void SystemLocaleImplTest::testCountry()
+{
+	SystemLocaleImpl locale;
+	CPPUNIT_ASSERT_EQUAL("", locale.country());
+}
+
+void SystemLocaleImplTest::testDisplayName()
+{
+	SystemLocaleImpl locale;
+	CPPUNIT_ASSERT_EQUAL("C", locale.displayName());
+}
+
+void SystemLocaleImplTest::testToString()
+{
+	SystemLocaleImpl locale;
+	CPPUNIT_ASSERT_EQUAL("C", locale.toString());
+}
+
+}

commit f33739b52062856dc8f7f4ade75e4403099a14d2

LocaleImpl: define virtual destructor
Fixes: 95b2ea0aa685 ("LocaleImpl: new interface")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/l10n/LocaleImpl.cpp
@@ -6,6 +6,10 @@
 using namespace std;
 using namespace BeeeOn;
 
+LocaleImpl::~LocaleImpl()
+{
+}
+
 string SystemLocaleImpl::language() const
 {
 	locale loc;

src/l10n/LocaleImpl.h
@@ -9,6 +9,8 @@ namespace BeeeOn {
  */
 class LocaleImpl {
 public:
+	virtual ~LocaleImpl();
+
 	/**
 	 * Returns language code of the locale.
 	 */