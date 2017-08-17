BeeeOn/base

commit ecc6043f207a12023d167b478f9b0b60aa5718c8

LocaleManager: new interface
Provide a way how to parse locale representations and create the appropriate
AbstractLocale instances. This makes the application independent on the target
locale library.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/l10n/LocaleManager.h
@@ -1,0 +1,17 @@
+#ifndef BEEEON_LOCALE_MANAGER_H
+#define BEEEON_LOCALE_MANAGER_H
+
+#include <string>
+
+#include "l10n/Locale.h"
+
+namespace BeeeOn {
+
+class LocaleManager {
+public:
+	virtual Locale parse(const std::string &input) = 0;
+};
+
+}
+
+#endif

commit 9810b541e36262359ade16605dc7ac83ef9a919b

LocaleManager: define virtual destructor
Fixes: ecc6043f207a ("LocaleManager: new interface")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/CMakeLists.txt
@@ -27,6 +27,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/io/TCPConsole.cpp
 	${PROJECT_SOURCE_DIR}/l10n/Locale.cpp
 	${PROJECT_SOURCE_DIR}/l10n/LocaleImpl.cpp
+	${PROJECT_SOURCE_DIR}/l10n/LocaleManager.cpp
 	${PROJECT_SOURCE_DIR}/l10n/NoTranslator.cpp
 	${PROJECT_SOURCE_DIR}/l10n/SystemLocaleManager.cpp
 	${PROJECT_SOURCE_DIR}/l10n/Translator.cpp

src/l10n/LocaleManager.cpp
@@ -1,0 +1,7 @@
+#include "l10n/LocaleManager.h"
+
+using namespace BeeeOn;
+
+LocaleManager::~LocaleManager()
+{
+}

src/l10n/LocaleManager.h
@@ -9,6 +9,8 @@ namespace BeeeOn {
 
 class LocaleManager {
 public:
+	virtual ~LocaleManager();
+
 	virtual Locale parse(const std::string &input) = 0;
 };
 