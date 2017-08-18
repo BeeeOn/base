e7e799547aef492770f9d66383e9be5d89399c3f
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-08-18 13:34:31 +0200

    LocaleManager: define method chooseBest()
    Introduce method to choose the best Locale from the given list
    of inputs. The list should be sorted in a way that the more appropriate
    locales are at the beginning.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/l10n/LocaleManager.h
+++ src/l10n/LocaleManager.h
@@ -2,6 +2,7 @@
 #define BEEEON_LOCALE_MANAGER_H
 
 #include <string>
+#include <vector>
 
 #include "l10n/Locale.h"
 
@@ -12,6 +13,7 @@ public:
 	virtual ~LocaleManager();
 
 	virtual Locale parse(const std::string &input) = 0;
+	virtual Locale chooseBest(const std::vector<std::string> &input) = 0;
 };
 
 }
