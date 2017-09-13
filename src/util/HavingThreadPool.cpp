BeeeOn/base

commit 886dd9b34115b6880ffbf13c89ba3dff37420a22

HavingThreadPool: new class
Certain classes might manage a Poco::ThreadPool instance internally.
We usually want to make the ThreadPool configurable from outside
(via dependency injection). The configuration code is always the some.

The HavingThreadPool class avoid reinventing the same principle again
for each such implementation. It provides setters and initialization
code of the ThreadPool.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/HavingThreadPool.cpp
@@ -1,0 +1,71 @@
+#include <Poco/Logger.h>
+
+#include "util/HavingThreadPool.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+HavingThreadPool::HavingThreadPool():
+	m_minThreads(1),
+	m_maxThreads(16),
+	m_threadIdleTime(100)
+{
+}
+
+HavingThreadPool::~HavingThreadPool()
+{
+}
+
+void HavingThreadPool::setMinThreads(int min)
+{
+	if (min < 0)
+		throw InvalidArgumentException("minThreads must be non-negative");
+
+	m_minThreads = min;
+}
+
+void HavingThreadPool::setMaxThreads(int max)
+{
+	if (max < 0)
+		throw InvalidArgumentException("maxThreads must be non-negative");
+
+	m_maxThreads = max;
+}
+
+void HavingThreadPool::setThreadIdleTime(int ms)
+{
+	if (ms <= 0)
+		throw InvalidArgumentException("threadIdleTime must be greater then zero");
+
+	if (ms > 0 && ms < 1000) {
+		logger().warning("threadIdleTime's granularity is 1000 ms, treating "
+				+ to_string(ms) + " as zero",
+				__FILE__, __LINE__);
+	}
+
+	m_threadIdleTime = ms / 1000;
+}
+
+void HavingThreadPool::initPool()
+{
+	if (m_pool.isNull()) {
+		logger().notice("creating thread pool min: "
+			+ to_string(m_minThreads)
+			+ " max: "
+			+ to_string(m_maxThreads),
+			__FILE__, __LINE__);
+
+		m_pool = new ThreadPool(
+			m_minThreads,
+			m_maxThreads,
+			m_threadIdleTime
+		);
+	}
+}
+
+ThreadPool &HavingThreadPool::pool()
+{
+	initPool();
+	return *m_pool;
+}

src/CMakeLists.txt
@@ -50,6 +50,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/AutoConfigurationExplorer.cpp
 	${PROJECT_SOURCE_DIR}/util/WithTrace.cpp
 	${PROJECT_SOURCE_DIR}/util/JsonUtil.cpp
+	${PROJECT_SOURCE_DIR}/util/HavingThreadPool.cpp
 )
 
 include_directories(

src/util/HavingThreadPool.h
@@ -1,0 +1,34 @@
+#ifndef BEEEON_HAVING_THREAD_POOL_H
+#define BEEEON_HAVING_THREAD_POOL_H
+
+#include <Poco/SharedPtr.h>
+#include <Poco/ThreadPool.h>
+
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+class HavingThreadPool : protected virtual Loggable {
+public:
+	HavingThreadPool();
+	virtual ~HavingThreadPool();
+
+	void setMinThreads(int min);
+	void setMaxThreads(int max);
+	void setThreadIdleTime(int ms);
+
+	Poco::ThreadPool &pool();
+
+protected:
+	void initPool();
+
+private:
+	int m_minThreads;
+	int m_maxThreads;
+	int m_threadIdleTime;
+	Poco::SharedPtr<Poco::ThreadPool> m_pool;
+};
+
+}
+
+#endif

commit 7763516cc261d8185aadce51be73075b45fedb3e

HavingThreadPool: fix default m_threadIdleTime
The m_threadIdleTime is in seconds and not milliseconds.

Fixes: 886dd9b34115 ("HavingThreadPool: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/util/HavingThreadPool.cpp
@@ -9,7 +9,7 @@ using namespace BeeeOn;
 HavingThreadPool::HavingThreadPool():
 	m_minThreads(1),
 	m_maxThreads(16),
-	m_threadIdleTime(100)
+	m_threadIdleTime(5)
 {
 }
 