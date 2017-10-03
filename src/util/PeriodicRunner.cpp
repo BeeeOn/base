BeeeOn/base

commit 98ae97d4c6951ecb4c80c0ee48d4d3319e91e599

PeriodicRunner: new class
Wrap the Poco::Timer inside the PeriodicRunner to simplify coding when
a periodic execution is needed.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/PeriodicRunner.cpp
@@ -1,0 +1,41 @@
+#include <Poco/Exception.h>
+
+#include "util/PeriodicRunner.h"
+
+using namespace Poco;
+using namespace BeeeOn;
+
+PeriodicRunner::PeriodicRunner():
+	m_invoke(*this, &PeriodicRunner::onStart)
+{
+}
+
+PeriodicRunner::~PeriodicRunner()
+{
+}
+
+void PeriodicRunner::start(const Callback &callback)
+{
+	Timer::stop();
+
+	m_callback = callback;
+	Timer::start(m_invoke);
+}
+
+void PeriodicRunner::stop()
+{
+	Timer::stop();
+}
+
+void PeriodicRunner::setInterval(const Timespan &interval)
+{
+	if (interval.totalMilliseconds() <= 0)
+		throw InvalidArgumentException("too small interval for period invocations");
+
+	Timer::setPeriodicInterval(interval.totalMilliseconds());
+}
+
+void PeriodicRunner::onStart(Timer &)
+{
+	m_callback();
+}

src/util/PeriodicRunner.h
@@ -1,0 +1,52 @@
+#ifndef BEEEON_PERIODIC_RUNNER_H
+#define BEEEON_PERIODIC_RUNNER_H
+
+#include <functional>
+
+#include <Poco/Timer.h>
+#include <Poco/Timespan.h>
+
+namespace BeeeOn {
+
+/**
+ * @brief Executes some function periodacally based on the given interval.
+ *
+ * The class simplifies coding of simple periodical tasks like generation
+ * of statistics, sampling data, etc.
+ */
+class PeriodicRunner : Poco::Timer {
+public:
+	typedef std::function<void()> Callback;
+
+	PeriodicRunner();
+	virtual ~PeriodicRunner();
+
+	/**
+	 * Starts the runner to executed repeatedly the given callback.
+	 */
+	void start(const Callback &callback);
+
+	/**
+	 * Stops the runner.
+	 */
+	void stop();
+
+	/**
+	 * Set interval of executions.
+	 */
+	void setInterval(const Poco::Timespan &interval);
+
+private:
+	/**
+	 * Executes the callback. This is called periodically by the timer.
+	 */
+	void onStart(Poco::Timer &timer);
+
+private:
+	Poco::TimerCallback<PeriodicRunner> m_invoke;
+	Callback m_callback;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -99,6 +99,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/LambdaTimerTask.cpp
 	${PROJECT_SOURCE_DIR}/util/Loggable.cpp
 	${PROJECT_SOURCE_DIR}/util/Occasionally.cpp
+	${PROJECT_SOURCE_DIR}/util/PeriodicRunner.cpp
 	${PROJECT_SOURCE_DIR}/util/PosixSignal.cpp
 	${PROJECT_SOURCE_DIR}/util/SAXHelper.cpp
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParser.cpp

commit 56c6e2dc9997c28ccdfc332485c571e9576720cc

PeriodicRunner: set interval before each start()
It seems that the interval of periodic execution is not preserved
after the stop() is called.

Fixes: 98ae97d4c695 ("PeriodicRunner: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/PeriodicRunner.cpp
@@ -19,6 +19,7 @@ void PeriodicRunner::start(const Callback &callback)
 	Timer::stop();
 
 	m_callback = callback;
+	Timer::setPeriodicInterval(m_interval.totalMilliseconds());
 	Timer::start(m_invoke);
 }
 
@@ -32,7 +33,7 @@ void PeriodicRunner::setInterval(const Timespan &interval)
 	if (interval.totalMilliseconds() <= 0)
 		throw InvalidArgumentException("too small interval for period invocations");
 
-	Timer::setPeriodicInterval(interval.totalMilliseconds());
+	m_interval = interval;
 }
 
 void PeriodicRunner::onStart(Timer &)

src/util/PeriodicRunner.h
@@ -45,6 +45,7 @@ private:
 private:
 	Poco::TimerCallback<PeriodicRunner> m_invoke;
 	Callback m_callback;
+	Poco::Timespan m_interval;
 };
 
 }