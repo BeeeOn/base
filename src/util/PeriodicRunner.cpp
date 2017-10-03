BeeeOn/base

commit 98ae97d4c6951ecb4c80c0ee48d4d3319e91e599

PeriodicRunner: new class
Wrap the Poco::Timer inside the PeriodicRunner to simplify coding when
a periodic execution is needed.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/PeriodicRunner.cpp
@@ -1,0 +1,41 @@
+#include <Poco/Exception.h>
+
+#include "util/PeriodicRunner.h"
+
+using namespace Poco;
+using namespace BeeeOn;
+
+PeriodicRunner::PeriodicRunner():
+	m_invoke(*this, &PeriodicRunner::onStart)
+{
+}
+
+PeriodicRunner::~PeriodicRunner()
+{
+}
+
+void PeriodicRunner::start(const Callback &callback)
+{
+	Timer::stop();
+
+	m_callback = callback;
+	Timer::start(m_invoke);
+}
+
+void PeriodicRunner::stop()
+{
+	Timer::stop();
+}
+
+void PeriodicRunner::setInterval(const Timespan &interval)
+{
+	if (interval.totalMilliseconds() <= 0)
+		throw InvalidArgumentException("too small interval for period invocations");
+
+	Timer::setPeriodicInterval(interval.totalMilliseconds());
+}
+
+void PeriodicRunner::onStart(Timer &)
+{
+	m_callback();
+}

src/util/PeriodicRunner.h
@@ -1,0 +1,52 @@
+#ifndef BEEEON_PERIODIC_RUNNER_H
+#define BEEEON_PERIODIC_RUNNER_H
+
+#include <functional>
+
+#include <Poco/Timer.h>
+#include <Poco/Timespan.h>
+
+namespace BeeeOn {
+
+/**
+ * @brief Executes some function periodacally based on the given interval.
+ *
+ * The class simplifies coding of simple periodical tasks like generation
+ * of statistics, sampling data, etc.
+ */
+class PeriodicRunner : Poco::Timer {
+public:
+	typedef std::function<void()> Callback;
+
+	PeriodicRunner();
+	virtual ~PeriodicRunner();
+
+	/**
+	 * Starts the runner to executed repeatedly the given callback.
+	 */
+	void start(const Callback &callback);
+
+	/**
+	 * Stops the runner.
+	 */
+	void stop();
+
+	/**
+	 * Set interval of executions.
+	 */
+	void setInterval(const Poco::Timespan &interval);
+
+private:
+	/**
+	 * Executes the callback. This is called periodically by the timer.
+	 */
+	void onStart(Poco::Timer &timer);
+
+private:
+	Poco::TimerCallback<PeriodicRunner> m_invoke;
+	Callback m_callback;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -99,6 +99,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/util/LambdaTimerTask.cpp
 	${PROJECT_SOURCE_DIR}/util/Loggable.cpp
 	${PROJECT_SOURCE_DIR}/util/Occasionally.cpp
+	${PROJECT_SOURCE_DIR}/util/PeriodicRunner.cpp
 	${PROJECT_SOURCE_DIR}/util/PosixSignal.cpp
 	${PROJECT_SOURCE_DIR}/util/SAXHelper.cpp
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParser.cpp

commit febb3edc90936c7e7baf24f392c36ddf9d53f2c2

PeriodicRunner: catch exceptions from callback
When the callback throws an exception, the next start() of the PeriodicRunner
would not continue as expected. Thus, catch all such cases and always return
from the onStart() method properly.

Fixes: 98ae97d4c695 ("PeriodicRunner: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/PeriodicRunner.cpp
@@ -1,4 +1,5 @@
 #include <Poco/Exception.h>
+#include <Poco/Logger.h>
 
 #include "util/PeriodicRunner.h"
 
@@ -38,5 +39,16 @@ void PeriodicRunner::setInterval(const Timespan &interval)
 
 void PeriodicRunner::onStart(Timer &)
 {
-	m_callback();
+	try {
+		m_callback();
+	}
+	catch (const Exception &e) {
+		logger().log(e, __FILE__, __LINE__);
+	}
+	catch (const std::exception &e) {
+		logger().critical(e.what(), __FILE__, __LINE__);
+	}
+	catch (...) {
+		logger().critical("unknown error", __FILE__, __LINE__);
+	}
 }

src/util/PeriodicRunner.h
@@ -6,6 +6,8 @@
 #include <Poco/Timer.h>
 #include <Poco/Timespan.h>
 
+#include "util/Loggable.h"
+
 namespace BeeeOn {
 
 /**
@@ -14,7 +16,7 @@ namespace BeeeOn {
  * The class simplifies coding of simple periodical tasks like generation
  * of statistics, sampling data, etc.
  */
-class PeriodicRunner : Poco::Timer {
+class PeriodicRunner : Poco::Timer, protected Loggable {
 public:
 	typedef std::function<void()> Callback;
 