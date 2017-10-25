BeeeOn/base

commit 77f5eb4ca9d679d09b7cea5dd654e6cffc28d8fd

ModuleType: move from gateway to base
Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/ModuleType.cpp
@@ -1,0 +1,57 @@
+#include "model/ModuleType.h"
+
+using namespace BeeeOn;
+using namespace std;
+
+EnumHelper<ModuleType::AttributeEnum::Raw>::ValueMap &ModuleType::AttributeEnum::valueMap()
+{
+	static EnumHelper<ModuleType::AttributeEnum::Raw>::ValueMap valueMap = {
+		{ModuleType::AttributeEnum::TYPE_INNER, "inner"},
+		{ModuleType::AttributeEnum::TYPE_OUTER, "outer"},
+	};
+
+	return valueMap;
+}
+
+EnumHelper<ModuleType::TypeEnum::Raw>::ValueMap &ModuleType::TypeEnum::valueMap()
+{
+	static EnumHelper<ModuleType::TypeEnum::Raw>::ValueMap valueMap = {
+		{ModuleType::TypeEnum::TYPE_BATTERY, "battery"},
+		{ModuleType::TypeEnum::TYPE_CO2, "co2"},
+		{ModuleType::TypeEnum::TYPE_HUMIDITY, "humidity"},
+		{ModuleType::TypeEnum::TYPE_MOTION, "motion"},
+		{ModuleType::TypeEnum::TYPE_NOISE, "noise"},
+		{ModuleType::TypeEnum::TYPE_ON_OFF, "on-off"},
+		{ModuleType::TypeEnum::TYPE_PRESSURE, "pressure"},
+		{ModuleType::TypeEnum::TYPE_TEMPERATURE, "temperature"},
+	};
+
+	return valueMap;
+}
+
+ModuleType::ModuleType(const ModuleType::Type &type,
+		const set<ModuleType::Attribute> &attributes):
+	m_type(type),
+	m_attributes(attributes)
+{
+}
+
+void ModuleType::setType(const ModuleType::Type &type)
+{
+	m_type = type;
+}
+
+ModuleType::Type ModuleType::type() const
+{
+	return m_type;
+}
+
+void ModuleType::setAttributes(const set<ModuleType::Attribute> &attributes)
+{
+	m_attributes = attributes;
+}
+
+set<ModuleType::Attribute> ModuleType::attributes() const
+{
+	return m_attributes;
+}

src/model/ModuleType.h
@@ -1,0 +1,62 @@
+#ifndef BEEEON_MODULE_TYPE_H
+#define BEEEON_MODULE_TYPE_H
+
+#include <set>
+
+#include "util/Enum.h"
+
+namespace BeeeOn {
+
+/**
+ * Representation of value type that device can send. Each value
+ * consists of two parts: Type and Attribute.
+ *
+ * The attribute is optional. The type is high level data type
+ * and the attribute is additional information, for example
+ * location (inner, outer).
+ */
+class ModuleType {
+public:
+	struct AttributeEnum {
+		enum Raw {
+			TYPE_INNER,
+			TYPE_OUTER,
+		};
+
+		static EnumHelper<Raw>::ValueMap &valueMap();
+	};
+
+	struct TypeEnum {
+		enum Raw {
+			TYPE_BATTERY,
+			TYPE_CO2,
+			TYPE_HUMIDITY,
+			TYPE_MOTION,
+			TYPE_NOISE,
+			TYPE_ON_OFF,
+			TYPE_PRESSURE,
+			TYPE_TEMPERATURE,
+		};
+
+		static EnumHelper<Raw>::ValueMap &valueMap();
+	};
+
+	typedef Enum<TypeEnum> Type;
+	typedef Enum<AttributeEnum> Attribute;
+
+	ModuleType(const Type &type, const std::set<Attribute> &attributes);
+
+	void setType(const Type &type);
+	Type type() const;
+
+	void setAttributes(const std::set<Attribute> &attributes);
+	std::set<Attribute> attributes() const;
+
+private:
+	Type m_type;
+	std::set<Attribute> m_attributes;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -35,6 +35,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/GlobalID.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleID.cpp
 	${PROJECT_SOURCE_DIR}/model/ModuleInfo.cpp
+	${PROJECT_SOURCE_DIR}/model/ModuleType.cpp
 	${PROJECT_SOURCE_DIR}/model/SensorValue.cpp
 	${PROJECT_SOURCE_DIR}/model/SimpleID.cpp
 	${PROJECT_SOURCE_DIR}/model/TypeInfo.cpp

commit 6ddf7bacf0bd364f54861e9f614b00063c4dd02d

ModuleType: rename attribute prefix to ATTR_
The attributes should not share the TYPE_ prefix with the enum Type.
It is either confusing and error-prone.

Fixes: 77f5eb4ca9d6 ("ModuleType: move from gateway to base")
Fixes: 96e95a141d7b ("ModuleType: new class")

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/model/ModuleType.cpp
@@ -10,10 +10,10 @@ using namespace std;
 EnumHelper<ModuleType::AttributeEnum::Raw>::ValueMap &ModuleType::AttributeEnum::valueMap()
 {
 	static EnumHelper<ModuleType::AttributeEnum::Raw>::ValueMap valueMap = {
-		{ModuleType::AttributeEnum::TYPE_INNER, "inner"},
-		{ModuleType::AttributeEnum::TYPE_MANUAL_ONLY, "manual-only"},
-		{ModuleType::AttributeEnum::TYPE_OUTER, "outer"},
-		{ModuleType::AttributeEnum::TYPE_CONTROLLABLE, "controllable"},
+		{ModuleType::AttributeEnum::ATTR_INNER, "inner"},
+		{ModuleType::AttributeEnum::ATTR_MANUAL_ONLY, "manual-only"},
+		{ModuleType::AttributeEnum::ATTR_OUTER, "outer"},
+		{ModuleType::AttributeEnum::ATTR_CONTROLLABLE, "controllable"},
 	};
 
 	return valueMap;
@@ -129,13 +129,13 @@ bool ModuleType::hasCombination(
 void ModuleType::assureValidAttributes(const set<Attribute> &attributes)
 {
 	if (hasCombination(attributes,
-			{Attribute::TYPE_INNER, Attribute::TYPE_OUTER})) {
+			{Attribute::ATTR_INNER, Attribute::ATTR_OUTER})) {
 		throw InvalidArgumentException(
 			"inner and outer cannot be set at once");
 	}
 
-	if (attributes.find(Attribute::TYPE_MANUAL_ONLY) != attributes.end()) {
-		if (attributes.find(Attribute::TYPE_CONTROLLABLE) == attributes.end()) {
+	if (attributes.find(Attribute::ATTR_MANUAL_ONLY) != attributes.end()) {
+		if (attributes.find(Attribute::ATTR_CONTROLLABLE) == attributes.end()) {
 			throw InvalidArgumentException(
 				"manual-only attribute requires attribute controllable");
 		}
@@ -144,7 +144,7 @@ void ModuleType::assureValidAttributes(const set<Attribute> &attributes)
 
 bool ModuleType::isControllable() const
 {
-	return m_attributes.find(Attribute::TYPE_CONTROLLABLE) != m_attributes.end();
+	return m_attributes.find(Attribute::ATTR_CONTROLLABLE) != m_attributes.end();
 }
 
 void ModuleType::setCustomTypeID(CustomTypeID id)

src/model/ModuleType.h
@@ -23,22 +23,22 @@ public:
 			/**
 			 * The sensor/actuator is located inside (a building).
 			 */
-			TYPE_INNER = 1,
+			ATTR_INNER = 1,
 			/**
 			 * The sensor enables to set (to modify) value only from sensor.
 			 * This attribute makes sense only in combination with
-			 * the TYPE_CONTROLLABLE.
+			 * the ATTR_CONTROLLABLE.
 			 */
-			TYPE_MANUAL_ONLY = 2,
+			ATTR_MANUAL_ONLY = 2,
 			/**
 			 * The sensor/actuator is located outside.
 			 */
-			TYPE_OUTER = 3,
+			ATTR_OUTER = 3,
 			/**
 			 * The sensor of this type is controllable (it is an
 			 * actuator).
 			 */
-			TYPE_CONTROLLABLE = 4,
+			ATTR_CONTROLLABLE = 4,
 		};
 
 		static EnumHelper<Raw>::ValueMap &valueMap();

test/gwmessage/GWMessageTest.cpp
@@ -471,8 +471,8 @@ void GWMessageTest::testParseNewDevice()
 
 	const set<ModuleType::Attribute> &attributes = types.rbegin()->attributes();
 	CPPUNIT_ASSERT(attributes.size() == 3);
-	CPPUNIT_ASSERT(attributes.find(ModuleType::Attribute::TYPE_OUTER) != attributes.end());
-	CPPUNIT_ASSERT(attributes.find(ModuleType::Attribute::TYPE_MANUAL_ONLY) != attributes.end());
+	CPPUNIT_ASSERT(attributes.find(ModuleType::Attribute::ATTR_OUTER) != attributes.end());
+	CPPUNIT_ASSERT(attributes.find(ModuleType::Attribute::ATTR_MANUAL_ONLY) != attributes.end());
 }
 
 void GWMessageTest::testCreateNewDevice()
@@ -485,14 +485,14 @@ void GWMessageTest::testCreateNewDevice()
 	request->setRefreshTime(Timespan(30, 0));
 
 	set<ModuleType::Attribute> attributes1;
-	attributes1.emplace(ModuleType::Attribute::TYPE_INNER);
+	attributes1.emplace(ModuleType::Attribute::ATTR_INNER);
 	ModuleType type1(ModuleType::Type::fromRaw(
 		ModuleType::Type::TYPE_HUMIDITY), attributes1);
 
 	set<ModuleType::Attribute> attributes2;
-	attributes2.emplace(ModuleType::Attribute::TYPE_MANUAL_ONLY);
-	attributes2.emplace(ModuleType::Attribute::TYPE_OUTER);
-	attributes2.emplace(ModuleType::Attribute::TYPE_CONTROLLABLE);
+	attributes2.emplace(ModuleType::Attribute::ATTR_MANUAL_ONLY);
+	attributes2.emplace(ModuleType::Attribute::ATTR_OUTER);
+	attributes2.emplace(ModuleType::Attribute::ATTR_CONTROLLABLE);
 	ModuleType type2(ModuleType::Type::fromRaw(
 		ModuleType::Type::TYPE_PRESSURE), attributes2);
 