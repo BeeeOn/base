BeeeOn/base

commit 032bd184aa30740332ef2d978ec8aece8ec9c363

DIWrapper: check cast is possible during build
When extracting an instance from the given DIWrapper, we assume
that if the wrapper casts to AbstractDIWrapper<T> then the instance
of class T can also be casted to its parent class B.

If T cannot be casted to B, the build should fail here. It would
otherwise throw a bad_cast exception during runtime if executed.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

src/di/DIWrapper.h
@@ -521,6 +521,13 @@ private:
 template <typename T, typename B>
 B &DIWMethodHelper::extractInstance(DIWrapper &w)
 {
+	static_assert(
+		std::is_polymorphic<T>::value
+		&&
+		std::is_base_of<B, T>::value,
+		"Dynamic casting is impossible here"
+	);
+
 	AbstractDIWrapper<T> &wrapper = dynamic_cast<AbstractDIWrapper<T> &>(w);
 	Poco::SharedPtr<T> instance = wrapper.instance();
 	return dynamic_cast<B &>(*instance.get());

commit e8167ecc744b773e12b7db1665f58a1eae582498

DIWrapper: fix static check of dynamic cast
The check is too strict and does not allow situation when B == T.

Fixes: 032bd184aa30 ("DIWrapper: check cast is possible during build")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DIWrapper.h
@@ -522,9 +522,9 @@ template <typename T, typename B>
 B &DIWMethodHelper::extractInstance(DIWrapper &w)
 {
 	static_assert(
-		std::is_polymorphic<T>::value
-		&&
-		std::is_base_of<B, T>::value,
+		std::is_same<B, T>::value
+		||
+		(std::is_polymorphic<T>::value && std::is_base_of<B, T>::value),
 		"Dynamic casting is impossible here"
 	);
 