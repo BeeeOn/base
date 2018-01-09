BeeeOn/base

commit f871346063836b119e4837cc9c2e7903e8d07b5f

AsyncExecutor: new class
This class can be filled with tasks, which are executed in the separate
thread in FIFO order. It can be particulary useful for time non-critical
tasks, such as collecting data for statistical purpose, so thread creating new task
can do more important tasks.

Also contains unit tests.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/AsyncExecutor.h
@@ -1,0 +1,42 @@
+#pragma once
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/Event.h>
+#include <Poco/Mutex.h>
+
+#include <functional>
+#include <queue>
+
+#include "loop/StoppableRunnable.h"
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * Class for asynchronous executing tasks.
+ */
+class AsyncExecutor :
+	public StoppableRunnable,
+	public Loggable {
+public:
+	AsyncExecutor();
+	~AsyncExecutor();
+
+	/**
+	 * Add task to queue for executing
+	 *
+	 */
+	void invoke(std::function<void()> f);
+
+	void run() override;
+	void stop() override;
+
+private:
+	void execute(std::function<void()> task);
+	std::queue<std::function<void()>> m_taskQueue;
+	Poco::Event m_wakeupEvent;
+	Poco::FastMutex m_queueMutex;
+	Poco::AtomicCounter m_stopRequested;
+};
+
+}

src/CMakeLists.txt
@@ -51,6 +51,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/ssl/X509Fingerprint.cpp
 	${PROJECT_SOURCE_DIR}/util/ApplicationConfigurationLoader.cpp
 	${PROJECT_SOURCE_DIR}/util/ArgsParser.cpp
+	${PROJECT_SOURCE_DIR}/util/AsyncExecutor.cpp
 	${PROJECT_SOURCE_DIR}/util/AutoConfigurationExplorer.cpp
 	${PROJECT_SOURCE_DIR}/util/Backtrace.cpp
 	${PROJECT_SOURCE_DIR}/util/ClassInfo.cpp

src/util/AsyncExecutor.cpp
@@ -1,0 +1,81 @@
+#include <Poco/Logger.h>
+#include <Poco/ScopedLock.h>
+
+#include "di/Injectable.h"
+#include "util/AsyncExecutor.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, AsyncExecutor)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_END(BeeeOn, AsyncExecutor)
+
+using namespace BeeeOn;
+
+AsyncExecutor::AsyncExecutor() :
+	m_stopRequested(false)
+{
+}
+
+AsyncExecutor::~AsyncExecutor()
+{
+	if (!m_taskQueue.empty()) {
+		poco_warning(logger(),
+			"deleting object with non empty queue, tasks count: "
+			+ m_taskQueue.size());
+	}
+}
+
+void AsyncExecutor::invoke(std::function<void()> f)
+{
+	Poco::FastMutex::ScopedLock lock(m_queueMutex);
+	m_taskQueue.push(f);
+	m_wakeupEvent.set();
+}
+
+void AsyncExecutor::run()
+{
+	std::function<void()> task;
+
+	while (!m_stopRequested) {
+		Poco::ScopedLockWithUnlock<Poco::FastMutex> guard(m_queueMutex);
+
+		if (m_taskQueue.empty()) {
+			guard.unlock();
+			m_wakeupEvent.wait();
+			continue;
+		}
+
+		task = m_taskQueue.front();
+		m_taskQueue.pop();
+
+		guard.unlock();
+		execute(task);
+	}
+
+	m_stopRequested = false;
+
+	if (!m_taskQueue.empty()) {
+		poco_warning(logger(), "exiting thread with non empty queue");
+	}
+}
+
+void AsyncExecutor::execute(std::function<void()> task)
+{
+	try {
+		task();
+	}
+	catch(Poco::Exception &ex) {
+		logger().log(ex, __FILE__, __LINE__);
+	}
+	catch(std::exception &ex) {
+		poco_critical(logger(), ex.what());
+	}
+	catch(...) {
+		poco_critical(logger(), "unknown error");
+	}
+}
+
+void AsyncExecutor::stop()
+{
+	m_stopRequested = true;
+	m_wakeupEvent.set();
+}

test/CMakeLists.txt
@@ -33,6 +33,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/net/IPAddressRangeTest.cpp
 	${PROJECT_SOURCE_DIR}/net/MACAddressTest.cpp
 	${PROJECT_SOURCE_DIR}/util/ArgsParserTest.cpp
+	${PROJECT_SOURCE_DIR}/util/AsyncExecutorTest.cpp
 	${PROJECT_SOURCE_DIR}/util/BacktraceTest.cpp
 	${PROJECT_SOURCE_DIR}/util/ClassInfoTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp

test/util/AsyncExecutorTest.cpp
@@ -1,0 +1,172 @@
+#include <Poco/Event.h>
+#include <Poco/Exception.h>
+#include <Poco/Thread.h>
+
+#include <exception>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "util/AsyncExecutor.h"
+
+#define MAX_WAIT_TIME 10000 // 10 seconds in ms
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class AsyncExecutorTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(AsyncExecutorTest);
+	CPPUNIT_TEST(testRunEmpty);
+	CPPUNIT_TEST(testRunTwice);
+	CPPUNIT_TEST(testOneTask);
+	CPPUNIT_TEST(testOneThenRun);
+	CPPUNIT_TEST(testThousandTasks);
+	CPPUNIT_TEST(testThousandTasksThenRun);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testRunEmpty();
+	void testRunTwice();
+	void testOneTask();
+	void testOneThenRun();
+	void testThousandTasks();
+	void testThousandTasksThenRun();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(AsyncExecutorTest);
+
+/**
+ * testRunEmpty - create, start and stop executor instance without
+ * inserting any tasks to find any potential memory leaks using valgrind tool.
+ */
+void AsyncExecutorTest::testRunEmpty()
+{
+	Thread t;
+	AsyncExecutor executor;
+	t.start(executor);
+	executor.stop();
+	t.join();
+}
+
+/**
+ * testRunTwice - create then start and stop executor twice without
+ * inserting any tasks to find any potential memory leaks using valgrind tool.
+ */
+void AsyncExecutorTest::testRunTwice()
+{
+	Thread t;
+	AsyncExecutor executor;
+	t.start(executor);
+	executor.stop();
+	t.join();
+
+	t.start(executor);
+	executor.stop();
+	t.join();
+}
+
+/**
+ * testOneTask - create and start executor instance. Invoke it with one task and
+ * wait for it to finish.
+ * If task takes more then MAX_WAIT_TIME to finish its considered as failure.
+ */
+void AsyncExecutorTest::testOneTask()
+{
+	Thread t;
+	AsyncExecutor executor;
+	Event finishEvent;
+
+	t.start(executor);
+
+	executor.invoke([&finishEvent]() {finishEvent.set();});
+
+	CPPUNIT_ASSERT(finishEvent.tryWait(MAX_WAIT_TIME));
+
+	executor.stop();
+	t.join();
+}
+
+/**
+ * testOneThenRun - create executor instance, invoke it with one task then start its thread.
+ * Task should be able to finish within given time.
+ * If task takes more then MAX_WAIT_TIME to finish its considered as failure.
+ */
+void AsyncExecutorTest::testOneThenRun()
+{
+	Thread t;
+	AsyncExecutor executor;
+	Event finishEvent;
+
+	executor.invoke([&finishEvent]() {finishEvent.set();});
+
+	t.start(executor);
+
+	CPPUNIT_ASSERT(finishEvent.tryWait(MAX_WAIT_TIME));
+
+	executor.stop();
+	t.join();
+}
+
+/**
+ * testThousandTasks - create executor instance and start it.
+ * Then invoke it thousand times and wait for tasks to finish.
+ * If task takes more then MAX_WAIT_TIME to finish its considered as failure.
+ */
+void AsyncExecutorTest::testThousandTasks()
+{
+	Thread t;
+	AsyncExecutor executor;
+	Event finishEvent;
+	int results[1000];
+
+	t.start(executor);
+
+	for(int i=0; i<1000; i++){
+		executor.invoke([&results, i]() {results[i] = i;});
+	}
+	executor.invoke([&finishEvent]() {finishEvent.set();});
+
+	CPPUNIT_ASSERT(finishEvent.tryWait(MAX_WAIT_TIME));
+
+	for(int i=0; i<1000; i++)
+		CPPUNIT_ASSERT(results[i] == i);
+
+	executor.stop();
+	t.join();
+}
+
+/**
+ * testThousandTasks - create executor instance then invoke it thousand times.
+ * After invoke-ing start its thread and wait for tasks to finish.
+ * If task takes more then MAX_WAIT_TIME to finish its considered as failure.
+ */
+void AsyncExecutorTest::testThousandTasksThenRun()
+{
+	Thread t;
+	AsyncExecutor executor;
+	Event finishEvent;
+	int results[1000];
+
+	for (int i=0; i<1000; i++){
+		results[i] = 0;
+		executor.invoke([&results, i]() {results[i] = i;});
+	}
+
+	for (int i=0; i<1000; i++) {
+		CPPUNIT_ASSERT(results[i] == 0);
+	}
+
+	executor.invoke([&finishEvent]() {finishEvent.set();});
+
+	t.start(executor);
+
+	CPPUNIT_ASSERT(finishEvent.tryWait(MAX_WAIT_TIME));
+
+	for(int i=0; i<1000; i++)
+		CPPUNIT_ASSERT(results[i] == i);
+
+	executor.stop();
+	t.join();
+}
+
+} // namespace BeeeOn

commit c6537f56859b0f220dfb00c61a33f30b03b63f57

AsyncExecutor: fix order of includes
Fixes: f87134606383 ("AsyncExecutor: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/AsyncExecutor.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include <functional>
+#include <queue>
+
 #include <Poco/AtomicCounter.h>
 #include <Poco/Event.h>
 #include <Poco/Mutex.h>
-
-#include <functional>
-#include <queue>
 
 #include "loop/StoppableRunnable.h"
 #include "util/Loggable.h"