BeeeOn/base

commit d0105f30c74c345945ba95f35476a9dc46a03a48

HTTPUtil: new class
The class provides methods to send HTTP request. It also allows send request over SSL.
The intent of the class is unify HTTP communication throughout the project.

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/net/HTTPUtil.cpp
@@ -1,0 +1,78 @@
+#include <Poco/Net/HTTPSClientSession.h>
+
+#include "net/HTTPUtil.h"
+
+using namespace BeeeOn;
+using namespace Poco;
+using namespace Poco::Net;
+using namespace std;
+
+HTTPEntireResponse HTTPUtil::makeRequest(
+	Poco::Net::HTTPRequest& request,
+	const Poco::URI& uri,
+	const std::string& msg,
+	const Poco::Timespan& timeout)
+{
+	return makeRequest(request, uri, msg, nullptr, timeout);
+}
+
+HTTPEntireResponse HTTPUtil::makeRequest(
+	Poco::Net::HTTPRequest& request,
+	const Poco::URI& uri,
+	const std::string& msg,
+	Poco::SharedPtr<SSLClient> sslConfig,
+	const Poco::Timespan& timeout)
+{
+	const SocketAddress address(uri.getHost(), uri.getPort());
+
+	request.setURI(uri.getPathAndQuery());
+	request.setHost(uri.getHost(), uri.getPort());
+
+	return makeRequest(request, address, msg, sslConfig, timeout);
+}
+
+HTTPEntireResponse HTTPUtil::makeRequest(
+	Poco::Net::HTTPRequest& request,
+	const Poco::Net::SocketAddress& address,
+	const std::string& msg,
+	const Poco::Timespan& timeout)
+{
+	return makeRequest(request, address, msg, nullptr, timeout);
+}
+
+HTTPEntireResponse HTTPUtil::makeRequest(
+	Poco::Net::HTTPRequest& request,
+	const Poco::Net::SocketAddress& address,
+	const std::string& msg,
+	Poco::SharedPtr<SSLClient> sslConfig,
+	const Poco::Timespan& timeout)
+{
+	SharedPtr<HTTPClientSession> session;
+
+	if (sslConfig.isNull())
+		session = new HTTPClientSession(
+			address.host().toString(), address.port());
+	else
+		session = new HTTPSClientSession(
+			address.host().toString(), address.port(), sslConfig->context());
+
+	if (timeout >= 0)
+		session->setTimeout(timeout);
+
+	return sendRequest(session, request, msg);
+}
+
+HTTPEntireResponse HTTPUtil::sendRequest(
+	SharedPtr<HTTPClientSession> session, HTTPRequest& request, const string& msg)
+{
+	if (!msg.empty())
+		session->sendRequest(request) << msg;
+	else
+		session->sendRequest(request);
+
+	HTTPEntireResponse response;
+	istream& input = session->receiveResponse(response);
+	response.readBody(input);
+
+	return response;
+}

src/CMakeLists.txt
@@ -68,6 +68,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/model/SimpleID.cpp
 	${PROJECT_SOURCE_DIR}/model/TokenID.cpp
 	${PROJECT_SOURCE_DIR}/net/HTTPEntireResponse.cpp
+	${PROJECT_SOURCE_DIR}/net/HTTPUtil.cpp
 	${PROJECT_SOURCE_DIR}/net/IPAddressRange.cpp
 	${PROJECT_SOURCE_DIR}/net/MACAddress.cpp
 	${PROJECT_SOURCE_DIR}/ssl/RejectCertificateHandler.cpp

src/net/HTTPUtil.h
@@ -1,0 +1,80 @@
+#ifndef BEEEON_HTTP_UTIL_H
+#define BEEEON_HTTP_UTIL_H
+
+#include <string>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+#include <Poco/URI.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/SocketAddress.h>
+
+#include "net/HTTPEntireResponse.h"
+#include "ssl/SSLClient.h"
+
+namespace BeeeOn {
+
+class HTTPUtil {
+public:
+	HTTPUtil() = delete;
+	HTTPUtil(const HTTPUtil &) = delete;
+	HTTPUtil(const HTTPUtil &&) = delete;
+	~HTTPUtil() = delete;
+
+	/**
+	 * @brief Sends HTTP request to target defined by URI.
+	 * It also sets URI path to the request. If the timeout
+	 * is negative, it is not set.
+	 */
+	static HTTPEntireResponse makeRequest(
+		Poco::Net::HTTPRequest& request,
+		const Poco::URI& uri,
+		const std::string& msg,
+		const Poco::Timespan& timeout = -1);
+
+	/**
+	 * @brief Sends HTTP request over SSL to target defined
+	 * by URI. It also sets URI path to the request. If the
+	 * timeout is negative, it is not set.
+	 */
+	static HTTPEntireResponse makeRequest(
+		Poco::Net::HTTPRequest& request,
+		const Poco::URI& uri,
+		const std::string& msg,
+		Poco::SharedPtr<SSLClient> sslConfig,
+		const Poco::Timespan& timeout = -1);
+
+	/**
+	 * @brief Sends HTTP request to target defined by
+	 * socket address. If the timeout is negative,
+	 * it is not set.
+	 */
+	static HTTPEntireResponse makeRequest(
+		Poco::Net::HTTPRequest& request,
+		const Poco::Net::SocketAddress& address,
+		const std::string& msg,
+		const Poco::Timespan& timeout = -1);
+
+	/**
+	 * @brief Sends HTTP request over SSL to target defined
+	 * by socket address. If the timeout is negative, it is
+	 * not set.
+	 */
+	static HTTPEntireResponse makeRequest(
+		Poco::Net::HTTPRequest& request,
+		const Poco::Net::SocketAddress& address,
+		const std::string& msg,
+		Poco::SharedPtr<SSLClient> sslConfig,
+		const Poco::Timespan& timeout = -1);
+
+private:
+	static HTTPEntireResponse sendRequest(
+		Poco::SharedPtr<Poco::Net::HTTPClientSession> session,
+		Poco::Net::HTTPRequest& request,
+		const std::string& msg);
+};
+
+}
+
+#endif

commit e3fa0d1f3bc1d57a1a0704864a82c413b7d051c2

HTTPUtil: do not set URI to request if it is already set
Fixes: d0105f30 ("HTTPUtil: new class")

Signed-off-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/net/HTTPUtil.cpp
@@ -25,8 +25,9 @@ HTTPEntireResponse HTTPUtil::makeRequest(
 {
 	const SocketAddress address(uri.getHost(), uri.getPort());
 
-	request.setURI(uri.getPathAndQuery());
 	request.setHost(uri.getHost(), uri.getPort());
+	if (request.getURI() == "/")
+		request.setURI(uri.getPathAndQuery());
 
 	return makeRequest(request, address, msg, sslConfig, timeout);
 }