BeeeOn/base

commit 24c7e0b19447b09b23f2920d6796d9ab81452d8c

Console: introduce a new concept
The Console holds some common properties of a console and
provides mechanisms for session creation. When a session
is created, it can be used for communication with the user.

Handlers of a console input work with the ConsoleSession.
Console implementations can support multiple parallel sessions
or multiple sequentially allocated sessions.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/io/Console.cpp
@@ -1,0 +1,134 @@
+#include <iostream>
+
+#include <Poco/String.h>
+
+#include "io/Console.h"
+
+using namespace std;
+using namespace BeeeOn;
+using namespace Poco;
+
+ConsoleSessionImpl::~ConsoleSessionImpl()
+{
+}
+
+bool ConsoleSessionImpl::eof()
+{
+	return false;
+}
+
+string ConsoleSessionImpl::iosReadUntil(istream &in, const char c)
+{
+	string tmp;
+	getline(in, tmp, c);
+	return tmp;
+}
+
+string ConsoleSessionImpl::iosReadBytes(istream &in, const unsigned int length)
+{
+	char *buffer = new char[length];
+
+	try {
+		in.read(buffer, length);
+		string result(buffer, length);
+		delete buffer;
+		return result;
+	} catch (...) {
+		delete buffer;
+		throw;
+	}
+}
+
+void ConsoleSessionImpl::iosPrint(ostream &out, const string &text, bool newline)
+{
+	out << text;
+	if (newline)
+		out << endl;
+
+	out << flush;
+}
+
+ConsoleSession::ConsoleSession(Console &console):
+	m_impl(console.openSession()),
+	m_eol(console.eol()),
+	m_skipEol(console.skipEol()),
+	m_prompt(console.prompt())
+{
+}
+
+string ConsoleSession::readLine()
+{
+	m_impl->print(m_prompt, false);
+
+	string line;
+
+	while (!m_impl->eof()) {
+		string tmp = m_impl->readUntil(m_eol);
+		trimRightInPlace(tmp);
+
+		if (tmp.empty())
+			break;
+
+		if (tmp.back() != m_skipEol) {
+			line += tmp;
+			break;
+		}
+
+		// read another line because of skipEol
+		line += tmp.substr(0, tmp.size() - 1);
+	}
+
+	return line;
+}
+
+string ConsoleSession::readBytes(const unsigned int length)
+{
+	return m_impl->readBytes(length);
+}
+
+void ConsoleSession::print(const std::string &text, bool newline)
+{
+	return m_impl->print(text, newline);
+}
+
+bool ConsoleSession::eof()
+{
+	return m_impl->eof();
+}
+
+Console::Console():
+	m_eol('\n'),
+	m_skipEol('\\'),
+	m_prompt("> ")
+{
+}
+
+void Console::setEol(const char eol)
+{
+	m_eol = eol;
+}
+
+char Console::eol() const
+{
+	return m_eol;
+}
+
+void Console::setSkipEol(const char skipEol)
+{
+	m_skipEol = skipEol;
+}
+
+char Console::skipEol() const
+{
+	return m_skipEol;
+}
+
+void Console::setPrompt(const string &prompt)
+{
+	m_prompt = prompt;
+}
+
+string Console::prompt() const
+{
+	return m_prompt;
+}

src/CMakeLists.txt
@@ -18,6 +18,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIDaemon.cpp
 	${PROJECT_SOURCE_DIR}/di/DIXmlLoader.cpp
 	${PROJECT_SOURCE_DIR}/di/DIApplicationConfigurationLoader.cpp
+	${PROJECT_SOURCE_DIR}/io/Console.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp

src/io/Console.h
@@ -1,0 +1,133 @@
+#ifndef BEEEON_CONSOLE_H
+#define BEEEON_CONSOLE_H
+
+#include <iosfwd>
+
+#include <Poco/SharedPtr.h>
+
+namespace BeeeOn {
+
+/**
+ * Implementation specific console session. The session can represent
+ * a user that has logged into the system, an open socket or some other
+ * context.
+ */
+class ConsoleSessionImpl {
+public:
+	typedef Poco::SharedPtr<ConsoleSessionImpl> Ptr;
+
+	virtual ~ConsoleSessionImpl();
+
+	/**
+	 * Read and return data until the character c is reached.
+	 */
+	virtual std::string readUntil(const char c) = 0;
+
+	/**
+	 * Read the given amount of bytes.
+	 */
+	virtual std::string readBytes(const unsigned int length) = 0;
+
+	/**
+	 * Print the given string. The newline can be optionally appended.
+	 */
+	virtual void print(const std::string &text, bool newline = true) = 0;
+
+	/**
+	 * Return true if the session cannot read any data anymore.
+	 */
+	virtual bool eof();
+
+protected:
+	/**
+	 * Implementation of the readUntil for std::istream.
+	 */
+	std::string iosReadUntil(std::istream &in, const char c);
+
+	/**
+	 * Implementation of the readBytes for std::istream.
+	 */
+	std::string iosReadBytes(std::istream &in, const unsigned int length);
+
+	/**
+	 * Implementation of the print for std::ostream.
+	 */
+	void iosPrint(std::ostream &out, const std::string &text, bool newline);
+};
+
+/**
+ * Forward declaration for ConsoleSession.
+ */
+class Console;
+
+/**
+ * ConsoleSession represents a single session of a user. The exact
+ * semantics of a session is defined by the ConsoleSessionImpl.
+ * This class is a universal wrapper around the implementation
+ * and provides a public API to be widely used.
+ */
+class ConsoleSession {
+public:
+	ConsoleSession(Console &console);
+
+	/**
+	 * Print prompt and read the next line from the input.
+	 * The call is always blocking.
+	 */
+	std::string readLine();
+
+	/**
+	 * Read up to length bytes from input.
+	 * The call is always blocking.
+	 */
+	std::string readBytes(const unsigned int length);
+
+	/**
+	 * Print the given text to the output. The newline
+	 * can be optionally appended.
+	 */
+	void print(const std::string &text, bool newline = true);
+
+	/**
+	 * Return true if there will be no input from the console.
+	 */
+	bool eof();
+
+private:
+	ConsoleSessionImpl::Ptr m_impl;
+	char m_eol;
+	char m_skipEol;
+	std::string m_prompt;
+};
+
+/**
+ * Console represents a common settings for a certain console
+ * implementation. It creates the ConsoleSessionImpl based
+ * on its settings.
+ */
+class Console {
+	friend ConsoleSession;
+public:
+	Console();
+
+	void setEol(const char c);
+	char eol() const;
+
+	void setSkipEol(const char c);
+	char skipEol() const;
+
+	void setPrompt(const std::string &prompt);
+	std::string prompt() const;
+
+protected:
+	virtual ConsoleSessionImpl::Ptr openSession() = 0;
+
+private:
+	char m_eol;
+	char m_skipEol;
+	std::string m_prompt;
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -22,6 +22,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/di/DependencyInjectorTest.cpp
 	${PROJECT_SOURCE_DIR}/di/DIWrapperTest.cpp
 	${PROJECT_SOURCE_DIR}/di/DIDaemonTest.cpp
+	${PROJECT_SOURCE_DIR}/io/ConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunnerTest.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceIDTest.cpp

test/io/ConsoleTest.cpp
@@ -1,0 +1,135 @@
+#include <sstream>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "cppunit/BetterAssert.h"
+#include "io/Console.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class ConsoleTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(ConsoleTest);
+	CPPUNIT_TEST(testReadLine);
+	CPPUNIT_TEST(testReadMultiLine);
+	CPPUNIT_TEST(testEof);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testReadLine();
+	void testReadMultiLine();
+	void testEof();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ConsoleTest);
+
+class FakeConsoleSessionImpl : public ConsoleSessionImpl {
+public:
+	typedef SharedPtr<FakeConsoleSessionImpl> Ptr;
+
+	string readUntil(const char c) override
+	{
+		return iosReadUntil(m_in, c);
+	}
+
+	string readBytes(const unsigned int length) override
+	{
+		return iosReadBytes(m_in, length);
+	}
+
+	void print(const string &text, bool newline = true) override
+	{
+		iosPrint(m_out, text, newline);
+	}
+
+	bool eof() override
+	{
+		return m_in.eof();
+	}
+
+	istringstream &in()
+	{
+		return m_in;
+	}
+
+	ostringstream &out()
+	{
+		return m_out;
+	}
+
+private:
+	istringstream m_in;
+	ostringstream m_out;
+};
+
+class TestConsole : public Console {
+public:
+	TestConsole(ConsoleSessionImpl::Ptr impl):
+		m_impl(impl)
+	{
+	}
+
+	ConsoleSessionImpl::Ptr openSession() override
+	{
+		return m_impl;
+	}
+
+private:
+	ConsoleSessionImpl::Ptr m_impl;
+};
+
+void ConsoleTest::testReadLine()
+{
+	FakeConsoleSessionImpl::Ptr impl(new FakeConsoleSessionImpl);
+	TestConsole console(impl);
+
+	impl->in().str("first line\nsecond line\nthird line\n");
+
+	ConsoleSession session(console);
+
+	CPPUNIT_ASSERT_EQUAL("first line", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> ", impl->out().str());
+	CPPUNIT_ASSERT_EQUAL("second line", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> > ", impl->out().str());
+	CPPUNIT_ASSERT_EQUAL("third line", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> > > ", impl->out().str());
+	CPPUNIT_ASSERT_EQUAL("", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> > > > ", impl->out().str());
+}
+
+void ConsoleTest::testReadMultiLine()
+{
+	FakeConsoleSessionImpl::Ptr impl(new FakeConsoleSessionImpl);
+	TestConsole console(impl);
+
+	impl->in().str("first line\\\nsecond line\nthird line\n");
+
+	ConsoleSession session(console);
+
+	CPPUNIT_ASSERT_EQUAL("first linesecond line", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> ", impl->out().str());
+	CPPUNIT_ASSERT_EQUAL("third line", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> > ", impl->out().str());
+	CPPUNIT_ASSERT_EQUAL("", session.readLine());
+	CPPUNIT_ASSERT_EQUAL("> > > ", impl->out().str());
+}
+
+void ConsoleTest::testEof()
+{
+	FakeConsoleSessionImpl::Ptr impl(new FakeConsoleSessionImpl);
+	TestConsole console(impl);
+
+	impl->in().str("line\n");
+
+	ConsoleSession session(console);
+
+	CPPUNIT_ASSERT(!session.eof());
+
+	CPPUNIT_ASSERT_EQUAL("line", session.readLine());
+	CPPUNIT_ASSERT(!session.eof());
+
+	CPPUNIT_ASSERT_EQUAL("", session.readLine());
+	CPPUNIT_ASSERT(session.eof());
+}
+
+}

commit 7e62e70298024592d1148995a5397e94d1683673

Console: fix deallocation of memory
Momory is assigned as array:

char *buffer = new char[length];

so it means that it must be freed as array.

Fixes: 24c7e0b ("Console: introduce a new concept")

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/io/Console.cpp
@@ -31,10 +31,10 @@ string ConsoleSessionImpl::iosReadBytes(istream &in, const unsigned int length)
 	try {
 		in.read(buffer, length);
 		string result(buffer, length);
-		delete buffer;
+		delete[] buffer;
 		return result;
 	} catch (...) {
-		delete buffer;
+		delete[] buffer;
 		throw;
 	}
 }