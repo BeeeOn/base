BeeeOn/base

commit 105f438b530951489fc37ead3cce18bd890484d5

FdStream: new class
Wrap file descriptor into std::istream and std::ostream interfaces. Thus,
it is now possible to work with file descriptors in the C++ way. Also
non-blocking approach is possible, a non-blocking read sets eof() when
there is nothing to read.

The FdStream can be used in select/poll/epoll approaches and it directly
supports the poll call without any other complex coding. The poll error
events are converted to a FileException.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/io/FdStream.h
@@ -1,0 +1,76 @@
+#ifndef BEEEON_FD_STREAM_H
+#define BEEEON_FD_STREAM_H
+
+#include <iostream>
+
+#include <Poco/BufferedStreamBuf.h>
+#include <Poco/Timespan.h>
+
+struct pollfd;
+
+namespace BeeeOn {
+
+class FdStreamBuf : public Poco::BufferedStreamBuf {
+public:
+	FdStreamBuf(std::streamsize size, openmode mode);
+	FdStreamBuf(int fd, std::streamsize size, openmode mode);
+	~FdStreamBuf();
+
+	void setFd(int fd);
+	int fd() const;
+
+	int writeToDevice(const char *buffer, std::streamsize size) override;
+	int readFromDevice(char *buffer, std::streamsize capacity) override;
+
+	void close();
+
+private:
+	int m_fd;
+};
+
+class FdStreamIOS : public virtual std::ios {
+public:
+	enum {
+		POLL_READABLE = 0x01,
+		POLL_WRITABLE = 0x02,
+		POLL_ERROR    = 0x04,
+		POLL_HUP      = 0x08,
+		POLL_INVALID  = 0x10,
+		POLL_TIMEOUT  = 0x20,
+	};
+
+	FdStreamIOS(std::streamsize size, openmode mode);
+	FdStreamIOS(int fd, std::streamsize size, openmode mode);
+
+	int fd() const;
+	void setBlocking(bool blocking);
+	bool blocking() const;
+	void assign(int fd, bool closeOld = true);
+	void close();
+
+protected:
+	bool poll(struct pollfd &fd, const Poco::Timespan &timeout) const;
+
+protected:
+	FdStreamBuf m_buf;
+};
+
+class FdInputStream : public virtual FdStreamIOS, public std::istream {
+public:
+	FdInputStream(std::size_t size = 512);
+	FdInputStream(int fd, std::size_t size = 512);
+
+	bool poll(const Poco::Timespan &timeout) const;
+};
+
+class FdOutputStream : public virtual FdStreamIOS, public std::ostream {
+public:
+	FdOutputStream(std::size_t size = 512);
+	FdOutputStream(int fd, std::size_t size = 512);
+
+	bool poll(const Poco::Timespan &timeout) const;
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -19,6 +19,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIWrapper.cpp
 	${PROJECT_SOURCE_DIR}/di/DIXmlLoader.cpp
 	${PROJECT_SOURCE_DIR}/io/Console.cpp
+	${PROJECT_SOURCE_DIR}/io/FdStream.cpp
 	${PROJECT_SOURCE_DIR}/io/Printable.cpp
 	${PROJECT_SOURCE_DIR}/io/SerialPort.cpp
 	${PROJECT_SOURCE_DIR}/io/SimpleConsole.cpp

src/io/FdStream.cpp
@@ -1,0 +1,234 @@
+#include <cerrno>
+#include <climits>
+#include <cstring>
+#include <fcntl.h>
+#include <poll.h>
+#include <unistd.h>
+
+#include <string>
+
+#include <Poco/Exception.h>
+
+#include "io/FdStream.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+FdStreamBuf::FdStreamBuf(streamsize size, openmode mode):
+	BufferedStreamBuf(size, mode),
+	m_fd(-1)
+{
+}
+
+FdStreamBuf::FdStreamBuf(int fd, streamsize size, openmode mode):
+	BufferedStreamBuf(size, mode),
+	m_fd(fd)
+{
+}
+
+FdStreamBuf::~FdStreamBuf()
+{
+	close();
+}
+
+void FdStreamBuf::setFd(int fd)
+{
+	m_fd = fd;
+}
+
+int FdStreamBuf::fd() const
+{
+	return m_fd;
+}
+
+void FdStreamBuf::close()
+{
+	if (m_fd != 0) {
+		::close(m_fd);
+		m_fd = -1;
+	}
+}
+
+int FdStreamBuf::writeToDevice(const char *buffer, streamsize size)
+{
+	if (m_fd < 0)
+		return -1;
+
+	if (size > INT_MAX) {
+		throw InvalidArgumentException(
+			"size " + to_string(size) + " would overflow int"
+		);
+	}
+
+	ssize_t wlen = ::write(m_fd, buffer, size);
+	if (wlen >= 0)
+		return wlen;
+
+	if (errno == EAGAIN)
+		return 0;
+
+	if (errno == EINTR)
+		throw SignalException(string("write: ") + ::strerror(errno));
+
+	throw IOException(string("write: ") + ::strerror(errno));
+}
+
+int FdStreamBuf::readFromDevice(char *buffer, streamsize capacity)
+{
+	if (m_fd < 0)
+		return -1;
+
+	if (capacity > INT_MAX) {
+		throw InvalidArgumentException(
+			"size " + to_string(capacity) + " would overflow int"
+		);
+	}
+
+	ssize_t rlen = ::read(m_fd, buffer, capacity);
+	if (rlen >= 0)
+		return rlen;
+
+	if (errno == EAGAIN)
+		return 0;
+
+	if (errno == EINTR)
+		throw SignalException(string("write: ") + ::strerror(errno));
+
+	throw IOException(string("read: ") + ::strerror(errno));
+}
+
+FdStreamIOS::FdStreamIOS(streamsize size, openmode mode):
+	m_buf(size, mode)
+{
+}
+
+FdStreamIOS::FdStreamIOS(int fd, streamsize size, openmode mode):
+	m_buf(fd, size, mode)
+{
+}
+
+int FdStreamIOS::fd() const
+{
+	return m_buf.fd();
+}
+
+void FdStreamIOS::setBlocking(bool blocking)
+{
+	const int fd = m_buf.fd();
+	int fl = ::fcntl(fd, F_GETFL);
+	if (fl < 0)
+		throw SystemException(string("fcntl: ") + ::strerror(errno));
+
+	if (blocking) {
+		if (!(fl & O_NONBLOCK))
+			return;
+
+		fl &= ~O_NONBLOCK;
+	}
+	else {
+		if (fl & O_NONBLOCK)
+			return;
+
+		fl |= O_NONBLOCK;
+	}
+
+	if (::fcntl(fd, F_SETFL, fl) < 0)
+		throw SystemException(string("fcntl: ") + ::strerror(errno));
+}
+
+bool FdStreamIOS::blocking() const
+{
+	const int fd = m_buf.fd();
+	int fl = ::fcntl(fd, F_GETFL);
+	if (fl < 0)
+		throw SystemException(string("fcntl: ") + ::strerror(errno));
+
+	return fd & O_NONBLOCK;
+}
+
+void FdStreamIOS::assign(int fd, bool closeOld)
+{
+	const int oldfd = m_buf.fd();
+	m_buf.setFd(fd);
+
+	if (closeOld && oldfd >= 0)
+		::close(oldfd);
+}
+
+void FdStreamIOS::close()
+{
+	setstate(ios::eofbit);
+	m_buf.close();
+}
+
+bool FdStreamIOS::poll(struct pollfd &fd, const Timespan &timeout) const
+{
+	fd.revents = 0;
+
+	int ret = ::poll(&fd, 1, timeout.totalMilliseconds());
+	if (ret < 0) {
+		throw SystemException(
+			string("poll: ") + strerror(errno));
+	}
+
+	if (ret == 0)
+		return false;
+
+	if (fd.revents & POLLNVAL)
+		throw FileException("invalid file descriptor to poll (POLLNVAL)");
+	if (fd.revents & POLLERR)
+		throw FileException("special file is broken or closed (POLLERR)");
+
+	// POLLHUP is ignored as it would be detected by reaching EOF
+
+	return true;
+}
+
+FdInputStream::FdInputStream(size_t size):
+	FdStreamIOS(size, std::ios::in),
+	istream(&m_buf)
+{
+}
+
+FdInputStream::FdInputStream(int fd, size_t size):
+	FdStreamIOS(fd, size, std::ios::in),
+	istream(&m_buf)
+{
+}
+
+bool FdInputStream::poll(const Timespan &timeout) const
+{
+	pollfd pollfd;
+	pollfd.fd = fd();
+	pollfd.events = POLLIN | POLLRDNORM;
+
+	if (!FdStreamIOS::poll(pollfd, timeout))
+		return false;
+
+	return pollfd.revents & (POLLIN | POLLRDNORM | POLLHUP);
+}
+
+FdOutputStream::FdOutputStream(size_t size):
+	FdStreamIOS(size, std::ios::out),
+	ostream(&m_buf)
+{
+}
+
+FdOutputStream::FdOutputStream(int fd, size_t size):
+	FdStreamIOS(fd, size, std::ios::out),
+	ostream(&m_buf)
+{
+}
+
+bool FdOutputStream::poll(const Timespan &timeout) const
+{
+	pollfd pollfd;
+	pollfd.fd = fd();
+	pollfd.events = POLLOUT | POLLWRNORM;
+
+	if (!FdStreamIOS::poll(pollfd, timeout))
+		return false;
+
+	return pollfd.revents & (POLLOUT | POLLWRNORM | POLLHUP);
+}

test/CMakeLists.txt
@@ -24,6 +24,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIDaemonTest.cpp
 	${PROJECT_SOURCE_DIR}/io/AutoCloseTest.cpp
 	${PROJECT_SOURCE_DIR}/io/ConsoleTest.cpp
+	${PROJECT_SOURCE_DIR}/io/FdStreamTest.cpp
 	${PROJECT_SOURCE_DIR}/io/TCPConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/l10n/SystemLocaleImplTest.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunnerTest.cpp

test/io/FdStreamTest.cpp
@@ -1,0 +1,201 @@
+#include <unistd.h>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Event.h>
+#include <Poco/RunnableAdapter.h>
+#include <Poco/Thread.h>
+#include <Poco/Timestamp.h>
+
+#include "cppunit/BetterAssert.h"
+#include "io/FdStream.h"
+
+using namespace std;
+using namespace Poco;
+
+namespace BeeeOn {
+
+class FdStreamTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(FdStreamTest);
+	CPPUNIT_TEST(testNonBlockingRead);
+	CPPUNIT_TEST(testBlockingRead);
+	CPPUNIT_TEST(testPollPipe);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testNonBlockingRead();
+	void testBlockingRead();
+	void testPollPipe();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FdStreamTest);
+
+/**
+ * Create pipe descriptors and transfer some data
+ * through the pipe. The non-blocking read would
+ * report eof() (and sometimes fail()) when no data
+ * is immediately available.
+ */
+void FdStreamTest::testNonBlockingRead()
+{
+	int pipefd[2];
+	CPPUNIT_ASSERT(::pipe(pipefd) >= 0);
+
+	FdInputStream in(pipefd[0]);
+	FdOutputStream out(pipefd[1]);
+
+	in.setBlocking(false);
+
+	CPPUNIT_ASSERT(!in.eof());
+	CPPUNIT_ASSERT(!in.fail());
+
+	string buffer;
+
+	in >> buffer;
+	CPPUNIT_ASSERT(buffer.empty());
+	CPPUNIT_ASSERT(!in.bad());
+	CPPUNIT_ASSERT(in.eof());
+	// immediatelly eof -> fail
+	CPPUNIT_ASSERT(in.fail());
+
+	in.clear();
+
+	out << "12345678" << flush;
+
+	in >> buffer;
+	CPPUNIT_ASSERT_EQUAL(8, buffer.size());
+	CPPUNIT_ASSERT(in.eof());
+	// read successful but with eof -> no fail
+	CPPUNIT_ASSERT(!in.fail());
+
+	CPPUNIT_ASSERT_EQUAL("12345678", buffer);
+}
+
+class AsyncWriter {
+public:
+	AsyncWriter(FdOutputStream &output):
+		m_output(output),
+		m_written(0)
+	{
+	}
+
+	void doWrite()
+	{
+		m_wait.wait(10000);
+		Thread::sleep(100); // give some change for the read to block
+		m_output << "async data$" << flush;
+		m_written = true;
+	}
+
+	Event m_wait;
+	FdOutputStream &m_output;
+	AtomicCounter m_written;
+};
+
+/**
+ * Test reading in blocking mode. Read until the '$'
+ * character to avoid further blocking. The 100 ms
+ * timeout is used to make some space for the read
+ * to really block. This is however non-deterministic.
+ */
+void FdStreamTest::testBlockingRead()
+{
+	int pipefd[2];
+	CPPUNIT_ASSERT(::pipe(pipefd) >= 0);
+
+	FdInputStream in(pipefd[0]);
+	FdOutputStream out(pipefd[1]);
+
+	in.setBlocking(true);
+
+	AsyncWriter asyncWriter(out);
+	RunnableAdapter<AsyncWriter> writer(
+		asyncWriter, &AsyncWriter::doWrite);
+	Thread writerThread;
+
+	CPPUNIT_ASSERT(!in.eof());
+	CPPUNIT_ASSERT(!in.fail());
+
+	writerThread.start(writer);
+
+	CPPUNIT_ASSERT(!asyncWriter.m_written);
+
+	Timestamp before;
+	asyncWriter.m_wait.set();
+
+	string buffer;
+	getline(in, buffer, '$');
+
+	CPPUNIT_ASSERT(before.elapsed() >= 100 * Timespan::MILLISECONDS);
+
+	CPPUNIT_ASSERT_EQUAL(10, buffer.size());
+	CPPUNIT_ASSERT(!in.fail());
+	CPPUNIT_ASSERT(!in.eof());
+
+	CPPUNIT_ASSERT_EQUAL("async data", buffer);
+
+	CPPUNIT_ASSERT(writerThread.tryJoin(10000));
+}
+
+void FdStreamTest::testPollPipe()
+{
+	const Timespan DONT_BLOCK = 0;
+
+	int pipefd[2];
+	CPPUNIT_ASSERT(::pipe(pipefd) >= 0);
+
+	FdInputStream in(pipefd[0]);
+	FdOutputStream out(pipefd[1]);
+
+	in.setBlocking(false);
+	out.setBlocking(false);
+
+	CPPUNIT_ASSERT(!in.eof());
+	CPPUNIT_ASSERT(!in.fail());
+	CPPUNIT_ASSERT(!out.fail());
+
+	// nothing to read
+	CPPUNIT_ASSERT(!in.poll(DONT_BLOCK));
+	// possible to write
+	CPPUNIT_ASSERT(out.poll(DONT_BLOCK));
+
+	out << "123" << flush;
+	CPPUNIT_ASSERT(!out.fail());
+
+	// pipe is readable now
+	CPPUNIT_ASSERT(in.poll(DONT_BLOCK));
+	// pipe is still writable
+	CPPUNIT_ASSERT(out.poll(DONT_BLOCK));
+
+	string data;
+	in >> data;
+
+	CPPUNIT_ASSERT(in.eof());
+	CPPUNIT_ASSERT(!in.fail());
+	CPPUNIT_ASSERT_EQUAL("123", data);
+
+	// nothing to read
+	CPPUNIT_ASSERT(!in.poll(DONT_BLOCK));
+	// possible to write
+	CPPUNIT_ASSERT(out.poll(DONT_BLOCK));
+
+	out.close();
+	CPPUNIT_ASSERT(out.eof());
+	CPPUNIT_ASSERT(!out.bad());
+
+	// not readable but POLLHUP should wake as up
+	CPPUNIT_ASSERT(in.poll(DONT_BLOCK));
+
+	in >> data;
+	CPPUNIT_ASSERT(in.eof());
+	CPPUNIT_ASSERT(!in.bad());
+	CPPUNIT_ASSERT_EQUAL("123", data);
+
+	// not sure way it returns false even when the fd is -1
+	CPPUNIT_ASSERT(!out.poll(DONT_BLOCK));
+
+	out << "test" << flush;
+	CPPUNIT_ASSERT(out.eof());
+	CPPUNIT_ASSERT(!out.bad());
+}
+
+}

commit 9f48bb308132ba3def063e5c766926e649828257

FdStream: fix name of enum value
POLL_HUP is macro defined in signal.h.
In code is used as name for value in enum.

generated error:
base/src/io/FdStream.h:37:3: error: expected identifier
                POLL_HUP      = 0x08,

Fixes: 105f438 ("FdStream: new class")

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/io/FdStream.h
@@ -34,7 +34,7 @@ public:
 		POLL_READABLE = 0x01,
 		POLL_WRITABLE = 0x02,
 		POLL_ERROR    = 0x04,
-		POLL_HUP      = 0x08,
+		POLL_HANGUP   = 0x08,
 		POLL_INVALID  = 0x10,
 		POLL_TIMEOUT  = 0x20,
 	};