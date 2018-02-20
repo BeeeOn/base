BeeeOn/base

commit 0043aa584c8a96a9e9dbee51dcce79b799367559

Backtrace: new class
Backtrace allows to collect the function call backtrace independently
on the compiler, libc and platform. If there is no backtrace support
available, it provides no information and Backtrace::support is zero.

Currently, only glibc library is supported. However, the backtrace(3)
function might be avaiable in other libraries as well (e.g. uclibc).
As there is currently no way how to test it, no other libc library is
currently considered.

On ARM, the backtrace may behave in a different way so the tests must
be fixed for this architecture.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Martin Sakin <xsakin00@stud.fit.vutbr.cz>

src/util/Backtrace.cpp
@@ -1,0 +1,92 @@
+#include <new>
+
+#ifdef __GLIBC__
+#include <execinfo.h>
+#include <unistd.h>
+#endif
+
+#include "util/Backtrace.h"
+
+using namespace std;
+using namespace BeeeOn;
+
+Backtrace::Backtrace(bool empty)
+{
+	if (empty) {
+#ifdef __GLIBC__
+		m_backtrace_size = 0;
+#endif
+		return;
+	}
+
+#ifdef __GLIBC__
+	m_backtrace_size = backtrace(m_backtrace,
+			sizeof(m_backtrace) / sizeof(m_backtrace[0]));
+#endif
+}
+
+Backtrace::~Backtrace()
+{
+}
+
+unsigned int Backtrace::size() const
+{
+#ifdef __GLIBC__
+	return m_backtrace_size;
+#else
+	return 0;
+#endif
+}
+
+void Backtrace::fatal() const
+{
+#ifdef __GLIBC__
+	// malloc for backtrace has failed?
+	// there is something really very very bad
+	backtrace_symbols_fd(m_backtrace, m_backtrace_size, STDOUT_FILENO);
+#endif
+
+	throw bad_alloc();
+}
+
+string Backtrace::at(unsigned int i) const
+{
+#ifdef __GLIBC__
+	if (i >= m_backtrace_size)
+		return "";
+
+	char **b = backtrace_symbols(m_backtrace + i, 1);
+	if (b == NULL)
+		fatal();
+
+	const string text(b[0]);
+	free(b);
+	return text;
+#else
+	return "";
+#endif
+}
+
+string Backtrace::toString(const string &indent) const
+{
+#ifdef __GLIBC__
+	if (m_backtrace_size == 0)
+		return "";
+
+	char **b = backtrace_symbols(m_backtrace, m_backtrace_size);
+	if (b == NULL)
+		fatal();
+
+	string text;
+	for (unsigned int i = 0; i < m_backtrace_size; ++i) {
+		text += indent;
+		text += b[i];
+		text += "\n";
+	}
+
+	free(b);
+	return text;
+#else
+	return "";
+#endif
+}

src/CMakeLists.txt
@@ -31,6 +31,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/ssl/SSLFacility.cpp
 	${PROJECT_SOURCE_DIR}/ssl/SSLClient.cpp
 	${PROJECT_SOURCE_DIR}/ssl/SSLServer.cpp
+	${PROJECT_SOURCE_DIR}/util/Backtrace.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMM.cpp
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParser.cpp
 	${PROJECT_SOURCE_DIR}/util/SAXHelper.cpp

src/util/Backtrace.h
@@ -1,0 +1,48 @@
+#ifndef BEEEON_BACKTRACE_H
+#define BEEEON_BACKTRACE_H
+
+#include <string>
+
+#ifdef __GLIBC__
+#define BEEEON_BACKTRACE_GLIBC(name, size) \
+	void *name[16];                    \
+	unsigned int size
+#else
+#define BEEEON_BACKTRACE_GLIBC(name, size)
+#endif
+
+namespace BeeeOn {
+
+class Backtrace {
+public:
+	enum {
+#ifdef __GLIBC__
+	supported = 1
+#else
+	supported = 0
+#endif
+	};
+
+	Backtrace(bool empty = false);
+	~Backtrace();
+
+	unsigned int size() const;
+
+	std::string operator[] (unsigned int i) const
+	{
+		return at(i);
+	}
+
+	std::string at(unsigned int i) const;
+	std::string toString(const std::string &indent = "") const;
+
+protected:
+	void fatal() const;
+
+private:
+BEEEON_BACKTRACE_GLIBC(m_backtrace, m_backtrace_size);
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -21,6 +21,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIWrapperTest.cpp
 	${PROJECT_SOURCE_DIR}/model/GatewayIDTest.cpp
 	${PROJECT_SOURCE_DIR}/model/DeviceIDTest.cpp
+	${PROJECT_SOURCE_DIR}/util/BacktraceTest.cpp
 	${PROJECT_SOURCE_DIR}/util/DAMMTest.cpp
 	${PROJECT_SOURCE_DIR}/util/Base64Test.cpp
 	${PROJECT_SOURCE_DIR}/util/SecureXmlParserTest.cpp

test/util/BacktraceTest.cpp
@@ -1,0 +1,69 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "util/Backtrace.h"
+
+using namespace std;
+
+namespace BeeeOn {
+
+class BacktraceTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(BacktraceTest);
+	CPPUNIT_TEST(testBacktrace);
+	CPPUNIT_TEST(testHasNames);
+	CPPUNIT_TEST(testEmpty);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testBacktrace();
+	void testHasNames();
+	void testEmpty();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(BacktraceTest);
+
+void BacktraceTest::testBacktrace()
+{
+	Backtrace trace;
+
+	if (!Backtrace::supported) {
+		CPPUNIT_ASSERT(trace.size() == 0);
+		CPPUNIT_ASSERT(trace[0].empty());
+		return;
+	}
+
+	CPPUNIT_ASSERT(trace.size() > 2); // somebody has to call us
+
+	for (unsigned int i = 0; i < trace.size(); ++i)
+		CPPUNIT_ASSERT(!trace[i].empty());
+}
+
+void testing_function()
+{
+	Backtrace trace;
+
+	CPPUNIT_ASSERT(trace.size() > 3);
+
+	CPPUNIT_ASSERT(trace[0].find("Backtrace") != string::npos);
+	CPPUNIT_ASSERT(trace[1].find("testing_function") != string::npos);
+
+#ifndef __arm__
+	CPPUNIT_ASSERT(trace[2].find("testHasNames") != string::npos);
+#endif
+}
+
+void BacktraceTest::testHasNames()
+{
+	if (!Backtrace::supported)
+		return;
+
+	testing_function();
+}
+
+void BacktraceTest::testEmpty()
+{
+	Backtrace trace(true);
+
+	CPPUNIT_ASSERT(trace.size() == 0);
+	CPPUNIT_ASSERT(trace[0].empty());
+}
+
+}

commit 87cb7b36ace62433528b8a925199e5babfb58db5

Backtrace: fix unwanted semicolon
NO_BACKTRACE_MSG is ment to unwrap in function arguments parts
so it means that semicolon is not wanted.

error:
base/src/util/Backtrace.cpp:97:12: error: expected ')'
        write(fd, NO_BACKTRACE_MSG, sizeof(NO_BACKTRACE_MSG));

Fixes: 0043aa5 ("Backtrace: new class")

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Backtrace.cpp
@@ -93,7 +93,7 @@ void Backtrace::dump(int fd) const
 #ifdef __GLIBC__
 	backtrace_symbols_fd(m_backtrace, m_backtrace_size, fd);
 #else
-#define NO_BACKTRACE_MSG "(no backtrace available)\n";
+#define NO_BACKTRACE_MSG "(no backtrace available)\n"
 	write(fd, NO_BACKTRACE_MSG, sizeof(NO_BACKTRACE_MSG));
 #endif
 	fsync(fd);