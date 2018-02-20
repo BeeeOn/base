BeeeOn/base

commit a37361b8048c5145771e78a9552f338b6ffb1b59

Backtrace: call dump() from fatal()
Implement fatal() by calling dump() which is intended for this purpose.
The method still throws bad_alloc as we are handling a really very very
bad situation.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Backtrace.cpp
@@ -40,12 +40,9 @@ unsigned int Backtrace::size() const
 
 void Backtrace::fatal() const
 {
-#ifdef __GLIBC__
 	// malloc for backtrace has failed?
 	// there is something really very very bad
-	backtrace_symbols_fd(m_backtrace, m_backtrace_size, STDOUT_FILENO);
-#endif
-
+	dump(STDOUT_FILENO);
 	throw bad_alloc();
 }
 

commit 0decf5b303398f8d60a53a99554305b2535fb260

Backtrace: fix include of header
unistd.h is used in parts of code that are out
of glibc definition.
Example code:
        dump(STDOUT_FILENO);

STDOUT_FILENO is constant defined in unistd.h

Fixes: a37361b ("Backtrace: new method dump()")

Signed-off-by: Peter Kubov <xkubov06@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Backtrace.cpp
@@ -1,8 +1,8 @@
 #include <new>
+#include <unistd.h>
 
 #ifdef __GLIBC__
 #include <execinfo.h>
-#include <unistd.h>
 #endif
 
 #include "util/Backtrace.h"