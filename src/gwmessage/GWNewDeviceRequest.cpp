BeeeOn/base

commit 716ce911aea17ba260d58422799a3de00dc4d83d

GWNewDeviceRequest: new class
This message is sent by the gateway to the server after discovering
new device in the sensoric network. The message contains unique
device identification, its product name along with vendor, refresh
time and types of modules.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/gwmessage/GWNewDeviceRequest.cpp
@@ -1,0 +1,109 @@
+#include "gwmessage/GWNewDeviceRequest.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+GWNewDeviceRequest::GWNewDeviceRequest():
+	GWRequest(GWMessageType::NEW_DEVICE_REQUEST)
+{
+}
+
+GWNewDeviceRequest::GWNewDeviceRequest(const JSON::Object::Ptr object):
+	GWRequest(object)
+{
+}
+
+void GWNewDeviceRequest::setDeviceID(const DeviceID &deviceID)
+{
+	json()->set("device_id", deviceID.toString());
+}
+
+DeviceID GWNewDeviceRequest::deviceID() const
+{
+	return DeviceID::parse(json()->getValue<string>("device_id"));
+}
+
+void GWNewDeviceRequest::setProductName(const string &name)
+{
+	json()->set("product_name", name);
+}
+
+string GWNewDeviceRequest::productName() const
+{
+	return json()->getValue<string>("product_name");
+}
+
+void GWNewDeviceRequest::setVendor(const string &vendor)
+{
+	json()->set("vendor", vendor);
+}
+
+string GWNewDeviceRequest::vendor() const
+{
+	return json()->getValue<string>("vendor");
+}
+
+void GWNewDeviceRequest::setModuleTypes(const list<ModuleType> &types)
+{
+	JSON::Array::Ptr arrayOfTypes(new JSON::Array);
+
+	for (const auto &type : types) {
+		JSON::Object::Ptr typeObject(new JSON::Object);
+		typeObject->set("type", type.type().toString());
+
+		JSON::Array::Ptr arrayOfAttributes(new JSON::Array);
+
+		for (const auto &attribute : type.attributes()){
+			JSON::Object::Ptr attributeObject(new JSON::Object);
+			attributeObject->set("attribute", attribute.toString());
+
+			arrayOfAttributes->add(Dynamic::Var(attributeObject));
+		}
+
+		typeObject->set("attributes", arrayOfAttributes);
+
+		arrayOfTypes->add(Dynamic::Var(typeObject));
+	}
+
+	json()->set("module_types", arrayOfTypes);
+}
+
+list<ModuleType> GWNewDeviceRequest::moduleTypes() const
+{
+	list<ModuleType> types;
+
+	JSON::Array::Ptr arrayOfTypes = json()->getArray("module_types");
+
+	for (size_t i = 0; i < arrayOfTypes->size(); i++) {
+		const JSON::Object::Ptr typeObject = arrayOfTypes->getObject(i);
+
+		ModuleType::Type type(ModuleType::Type::parse(
+			typeObject->getValue<string>("type")));
+
+		set<ModuleType::Attribute> attributes;
+
+		JSON::Array::Ptr arrayOfAttributes = typeObject->getArray("attributes");
+
+		for(size_t j = 0; j < arrayOfAttributes->size(); j++) {
+			const JSON::Object::Ptr attributeObject = arrayOfAttributes->getObject(j);
+
+			attributes.emplace(ModuleType::Attribute::parse(
+				attributeObject->getValue<string>("attribute")));
+		}
+
+		types.push_back(ModuleType(type, attributes));
+	}
+
+	return types;
+}
+
+void GWNewDeviceRequest::setRefreshTime(const Poco::Timespan &time)
+{
+	json()->set("refresh_time", time.totalSeconds());
+}
+
+Poco::Timespan GWNewDeviceRequest::refreshTime() const
+{
+	return json()->getValue<unsigned int>("refresh_time") * Timespan::SECONDS;
+}

src/CMakeLists.txt
@@ -23,6 +23,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/gwmessage/GWGatewayRegister.cpp
 	${PROJECT_SOURCE_DIR}/gwmessage/GWMessage.cpp
 	${PROJECT_SOURCE_DIR}/gwmessage/GWMessageType.cpp
+	${PROJECT_SOURCE_DIR}/gwmessage/GWNewDeviceRequest.cpp
 	${PROJECT_SOURCE_DIR}/gwmessage/GWRequest.cpp
 	${PROJECT_SOURCE_DIR}/gwmessage/GWResponse.cpp
 	${PROJECT_SOURCE_DIR}/gwmessage/GWResponseWithAck.cpp

src/gwmessage/GWMessage.cpp
@@ -1,6 +1,7 @@
 #include "gwmessage/GWMessage.h"
 #include "gwmessage/GWGatewayRegister.h"
 #include "gwmessage/GWGatewayAccepted.h"
+#include "gwmessage/GWNewDeviceRequest.h"
 #include "gwmessage/GWResponse.h"
 #include "gwmessage/GWAck.h"
 #include "gwmessage/GWResponseWithAck.h"
@@ -59,6 +60,8 @@ GWMessage::Ptr GWMessage::fromJSON(Poco::JSON::Object::Ptr object)
 		return new GWResponse(object);
 	case GWMessageType::GENERIC_ACK:
 		return new GWAck(object);
+	case GWMessageType::NEW_DEVICE_REQUEST:
+		return new GWNewDeviceRequest(object);
 	case GWMessageType::RESPONSE_WITH_ACK:
 		return new GWResponseWithAck(object);
 	case GWMessageType::SENSOR_DATA_CONFIRM:

src/gwmessage/GWMessageType.cpp
@@ -9,6 +9,7 @@ EnumHelper<GWMessageTypeEnum::Raw>::ValueMap &GWMessageTypeEnum::valueMap()
 		{GATEWAY_ACCEPTED, "gateway_accepted"},
 		{GENERIC_RESPONSE, "generic_response"},
 		{GENERIC_ACK, "generic_ack"},
+		{NEW_DEVICE_REQUEST, "new_device_request"},
 		{RESPONSE_WITH_ACK, "response_with_ack"},
 		{SENSOR_DATA_CONFIRM, "sensor_data_confirm"},
 		{SENSOR_DATA_EXPORT, "sensor_data_export"},

src/gwmessage/GWMessageType.h
@@ -11,6 +11,7 @@ struct GWMessageTypeEnum {
 		GATEWAY_ACCEPTED,
 		GENERIC_RESPONSE,
 		GENERIC_ACK,
+		NEW_DEVICE_REQUEST,
 		RESPONSE_WITH_ACK,
 		SENSOR_DATA_CONFIRM,
 		SENSOR_DATA_EXPORT,

src/gwmessage/GWNewDeviceRequest.h
@@ -1,0 +1,51 @@
+#ifndef BEEEON_GW_MESSAGE_NEW_DEVICE_REQUEST_H
+#define BEEEON_GW_MESSAGE_NEW_DEVICE_REQUEST_H
+
+#include <list>
+#include <string>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/Timespan.h>
+#include <Poco/JSON/Object.h>
+
+#include "gwmessage/GWRequest.h"
+#include "model/DeviceID.h"
+#include "model/ModuleType.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief Represents a message sent by the gateway to the server after
+ * discovering new device in the sensoric network.
+ *
+ * The message contains unique device identification, its product name along
+ * with vendor, refresh time and types of device modules.
+ *
+ * Response to this request is the generic response GWResponse.
+ */
+class GWNewDeviceRequest : public GWRequest {
+public:
+	typedef Poco::SharedPtr<GWNewDeviceRequest> Ptr;
+
+	GWNewDeviceRequest();
+	GWNewDeviceRequest(const Poco::JSON::Object::Ptr object);
+
+	void setDeviceID(const DeviceID &deviceID);
+	DeviceID deviceID() const;
+
+	void setProductName(const std::string &name);
+	std::string productName() const;
+
+	void setVendor(const std::string &vendor);
+	std::string vendor() const;
+
+	void setModuleTypes(const std::list<ModuleType> &types);
+	std::list<ModuleType> moduleTypes() const;
+
+	void setRefreshTime(const Poco::Timespan &time);
+	Poco::Timespan refreshTime() const;
+};
+
+}
+
+#endif

test/gwmessage/GWMessageTest.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <Poco/Exception.h>
@@ -13,6 +15,7 @@
 #include "gwmessage/GWResponseWithAck.h"
 #include "gwmessage/GWSensorDataConfirm.h"
 #include "gwmessage/GWSensorDataExport.h"
+#include "gwmessage/GWNewDeviceRequest.h"
 #include "model/GlobalID.h"
 #include "util/JsonUtil.h"
 
@@ -38,6 +41,8 @@ class GWMessageTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testParseSensorDataExport);
 	CPPUNIT_TEST(testCreateSensorDataExport);
 	CPPUNIT_TEST(testGetConfirmFromSensorDataExport);
+	CPPUNIT_TEST(testParseNewDevice);
+	CPPUNIT_TEST(testCreateNewDevice);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void testParseEmpty();
@@ -54,7 +59,8 @@ public:
 	void testParseSensorDataExport();
 	void testCreateSensorDataExport();
 	void testGetConfirmFromSensorDataExport();
-
+	void testParseNewDevice();
+	void testCreateNewDevice();
 protected:
 	string jsonReformat(const string &json);
 };
@@ -380,6 +386,109 @@ void GWMessageTest::testGetConfirmFromSensorDataExport()
 	CPPUNIT_ASSERT(!confirm.isNull());
 	CPPUNIT_ASSERT_EQUAL(GWMessageType::SENSOR_DATA_CONFIRM, confirm->type().raw());
 	CPPUNIT_ASSERT_EQUAL("4a41d041-eb1e-4e9c-9528-1bbe74f54d59", confirm->id().toString());
+}
+
+void GWMessageTest::testParseNewDevice()
+{
+	GWMessage::Ptr message = GWMessage::fromJSON(
+	R"({
+			"message_type" : "new_device_request",
+			"id" : "4a41d041-eb1e-4e9c-9528-1bbe74f54d59",
+			"device_id" : "0xfe01020304050607",
+			"vendor" : "Good Company",
+			"product_name" : "Nice Product",
+			"refresh_time" : 30,
+			"module_types" : [
+				{
+					"type" : "humidity",
+					"attributes" : [
+						{"attribute" : "inner"}
+					]
+				},
+				{
+					"type" : "pressure",
+					"attributes" : [
+						{"attribute" : "outer"},
+						{"attribute" : "manual-only"}
+					]
+				}
+			]
+	})");
+
+	CPPUNIT_ASSERT_EQUAL(GWMessageType::NEW_DEVICE_REQUEST, message->type().raw());
+	CPPUNIT_ASSERT(!message.cast<GWNewDeviceRequest>().isNull());
+
+	GWNewDeviceRequest::Ptr request = message.cast<GWNewDeviceRequest>();
+	CPPUNIT_ASSERT_EQUAL("4a41d041-eb1e-4e9c-9528-1bbe74f54d59", request->id().toString());
+	CPPUNIT_ASSERT_EQUAL("0xfe01020304050607", request->deviceID().toString());
+	CPPUNIT_ASSERT_EQUAL("Good Company", request->vendor());
+	CPPUNIT_ASSERT_EQUAL("Nice Product", request->productName());
+	CPPUNIT_ASSERT(Timespan(30, 0) == request->refreshTime());
+
+	const list<ModuleType> &types = request->moduleTypes();
+
+	CPPUNIT_ASSERT_EQUAL("humidity", types.begin()->type().toString());
+	CPPUNIT_ASSERT_EQUAL("inner", types.begin()->attributes().begin()->toString());
+	CPPUNIT_ASSERT_EQUAL("pressure", types.rbegin()->type().toString());
+
+	const set<ModuleType::Attribute> &attributes = types.rbegin()->attributes();
+	CPPUNIT_ASSERT(attributes.size() == 2);
+	CPPUNIT_ASSERT(attributes.find(ModuleType::Attribute::TYPE_OUTER) != attributes.end());
+	CPPUNIT_ASSERT(attributes.find(ModuleType::Attribute::TYPE_MANUAL_ONLY) != attributes.end());
+}
+
+void GWMessageTest::testCreateNewDevice()
+{
+	GWNewDeviceRequest::Ptr request(new GWNewDeviceRequest);
+	request->setID(GlobalID::parse("4a41d041-eb1e-4e9c-9528-1bbe74f54d59"));
+	request->setDeviceID(DeviceID::parse("0xfe01020304050607"));
+	request->setVendor("Good Company");
+	request->setProductName("Nice Product");
+	request->setRefreshTime(Timespan(30, 0));
+
+	set<ModuleType::Attribute> attributes1;
+	attributes1.emplace(ModuleType::Attribute::TYPE_INNER);
+	ModuleType type1(ModuleType::Type::fromRaw(
+		ModuleType::Type::TYPE_HUMIDITY), attributes1);
+
+	set<ModuleType::Attribute> attributes2;
+	attributes2.emplace(ModuleType::Attribute::TYPE_MANUAL_ONLY);
+	attributes2.emplace(ModuleType::Attribute::TYPE_OUTER);
+	ModuleType type2(ModuleType::Type::fromRaw(
+		ModuleType::Type::TYPE_PRESSURE), attributes2);
+
+	list<ModuleType> types;
+	types.push_back(type1);
+	types.push_back(type2);
+
+	request->setModuleTypes(types);
+
+	CPPUNIT_ASSERT_EQUAL(
+		jsonReformat(R"({
+			"message_type" : "new_device_request",
+			"id" : "4a41d041-eb1e-4e9c-9528-1bbe74f54d59",
+			"device_id" : "0xfe01020304050607",
+			"vendor" : "Good Company",
+			"product_name" : "Nice Product",
+			"refresh_time" : 30,
+			"module_types" : [
+				{
+					"type" : "humidity",
+					"attributes" : [
+						{"attribute" : "inner"}
+					]
+				},
+				{
+					"type" : "pressure",
+					"attributes" : [
+						{"attribute" : "manual-only"},
+						{"attribute" : "outer"}
+					]
+				}
+			]
+		})"),
+		request->toString()
+	);
 }
 
 }

commit 6c5b6ef2a78b9e8810bf690c2390776372c5a689

GWNewDeviceRequest: fix setting negative refresh time
Setting refresh_time property to time.totalSeconds() is bad because
if somebody just sets the refresh time to -1, the value converted
to Poco::Timespan becomes -1 microsecond. Calling totalSeconds()
would however return 0 and thus we loose information.

Fixes: 716ce911aea1 ("GWNewDeviceRequest: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/gwmessage/GWNewDeviceRequest.cpp
@@ -100,7 +100,10 @@ list<ModuleType> GWNewDeviceRequest::moduleTypes() const
 
 void GWNewDeviceRequest::setRefreshTime(const Poco::Timespan &time)
 {
-	json()->set("refresh_time", time.totalSeconds());
+	if (time < 0)
+		json()->set("refresh_time", -1);
+	else
+		json()->set("refresh_time", time.totalSeconds());
 }
 
 Poco::Timespan GWNewDeviceRequest::refreshTime() const