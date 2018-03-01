BeeeOn/base

commit c6612b6cc0ad401565ea1d7e8836061c279df6d8

AsyncExecutor: extract interface
Now, we have an interface AsyncExecutor and its implementation
SequentialAsyncExecutor. Code instantiating AsyncExecutor must
be updated.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/AsyncExecutor.h
@@ -1,0 +1,27 @@
+#pragma once
+
+#include <functional>
+
+#include <Poco/SharedPtr.h>
+
+#include "util/Loggable.h"
+
+namespace BeeeOn {
+
+/**
+ * Class for asynchronous executing tasks.
+ */
+class AsyncExecutor : public Loggable {
+public:
+	typedef Poco::SharedPtr<AsyncExecutor> Ptr;
+
+	AsyncExecutor();
+	~AsyncExecutor();
+
+	/**
+	 * Add task to queue for executing
+	 */
+	virtual void invoke(std::function<void()> f) = 0;
+};
+
+}

src/util/SequentialAsyncExecutor.h
@@ -1,34 +1,26 @@
 #pragma once
 
-#include <functional>
 #include <queue>
 
 #include <Poco/AtomicCounter.h>
 #include <Poco/Event.h>
 #include <Poco/Mutex.h>
-#include <Poco/SharedPtr.h>
 
 #include "loop/StoppableRunnable.h"
-#include "util/Loggable.h"
+#include "util/AsyncExecutor.h"
 
 namespace BeeeOn {
 
-/**
- * Class for asynchronous executing tasks.
- */
 class SequentialAsyncExecutor :
 	public StoppableRunnable,
-	public Loggable {
+	public AsyncExecutor {
 public:
 	typedef Poco::SharedPtr<SequentialAsyncExecutor> Ptr;
 
 	SequentialAsyncExecutor();
 	~SequentialAsyncExecutor();
 
-	/**
-	 * Add task to queue for executing
-	 */
-	void invoke(std::function<void()> f);
+	void invoke(std::function<void()> f) override;
 
 	void run() override;
 	void stop() override;

src/CMakeLists.txt
@@ -77,6 +77,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/ssl/X509Fingerprint.cpp
 	${PROJECT_SOURCE_DIR}/util/ApplicationConfigurationLoader.cpp
 	${PROJECT_SOURCE_DIR}/util/ArgsParser.cpp
+	${PROJECT_SOURCE_DIR}/util/AsyncExecutor.cpp
 	${PROJECT_SOURCE_DIR}/util/AutoConfigurationExplorer.cpp
 	${PROJECT_SOURCE_DIR}/util/Backtrace.cpp
 	${PROJECT_SOURCE_DIR}/util/BackOff.cpp

src/util/AsyncExecutor.cpp
@@ -1,0 +1,11 @@
+#include "util/AsyncExecutor.h"
+
+using namespace BeeeOn;
+
+AsyncExecutor::AsyncExecutor()
+{
+}
+
+AsyncExecutor::~AsyncExecutor()
+{
+}

src/util/EventSource.h
@@ -7,7 +7,7 @@
 #include <Poco/Exception.h>
 #include <Poco/SharedPtr.h>
 
-#include "util/SequentialAsyncExecutor.h"
+#include "util/AsyncExecutor.h"
 #include "util/ClassInfo.h"
 #include "util/Loggable.h"
 #include "util/Once.h"
@@ -19,7 +19,7 @@ namespace BeeeOn {
  *
  * The EventSource can be inherited or used as member of another class (preferred)
  * to provide common logic for firing events. It registers listeners and fires
- * events via the provided SequentialAsyncExecutor.
+ * events via the provided AsyncExecutor.
  */
 template <typename Listener>
 class EventSource : protected Loggable {
@@ -27,8 +27,8 @@ public:
 	EventSource();
 	virtual ~EventSource();
 
-	void setAsyncExecutor(SequentialAsyncExecutor::Ptr executor);
-	SequentialAsyncExecutor::Ptr asyncExecutor() const;
+	void setAsyncExecutor(AsyncExecutor::Ptr executor);
+	AsyncExecutor::Ptr asyncExecutor() const;
 
 	void addListener(typename Listener::Ptr listener);
 
@@ -55,7 +55,7 @@ public:
 	void fireEvent(const Event &e, const Method &m);
 
 private:
-	SequentialAsyncExecutor::Ptr m_executor;
+	AsyncExecutor::Ptr m_executor;
 	std::list<typename Listener::Ptr> m_listeners;
 };
 
@@ -70,13 +70,13 @@ EventSource<Listener>::~EventSource()
 }
 
 template <typename Listener>
-void EventSource<Listener>::setAsyncExecutor(SequentialAsyncExecutor::Ptr executor)
+void EventSource<Listener>::setAsyncExecutor(AsyncExecutor::Ptr executor)
 {
 	m_executor = executor;
 }
 
 template <typename Listener>
-SequentialAsyncExecutor::Ptr EventSource<Listener>::asyncExecutor() const
+AsyncExecutor::Ptr EventSource<Listener>::asyncExecutor() const
 {
 	return m_executor;
 }

src/util/SequentialAsyncExecutor.cpp
@@ -5,6 +5,7 @@
 #include "util/SequentialAsyncExecutor.h"
 
 BEEEON_OBJECT_BEGIN(BeeeOn, SequentialAsyncExecutor)
+BEEEON_OBJECT_CASTABLE(AsyncExecutor)
 BEEEON_OBJECT_CASTABLE(StoppableRunnable)
 BEEEON_OBJECT_END(BeeeOn, SequentialAsyncExecutor)
 

test/util/EventSourceTest.cpp
@@ -6,6 +6,7 @@
 
 #include "cppunit/BetterAssert.h"
 #include "util/EventSource.h"
+#include "util/SequentialAsyncExecutor.h"
 
 using namespace Poco;
 using namespace std;

commit c0802c729d237b854dce9a141f9af491d4d08d9b

AsyncExecutor: do not inherit from Loggable
The AsyncExecutor is an interface and should not force inheriting
from Loggable. It does not use it anyway. Also, this requires to
make the ~AsyncExecutor() virtual (otherwise we fail with compilation
due to DI rules).

Fixes: c6612b6cc0ad ("AsyncExecutor: extract interface")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>

src/util/AsyncExecutor.h
@@ -4,19 +4,17 @@
 
 #include <Poco/SharedPtr.h>
 
-#include "util/Loggable.h"
-
 namespace BeeeOn {
 
 /**
  * Class for asynchronous executing tasks.
  */
-class AsyncExecutor : public Loggable {
+class AsyncExecutor {
 public:
 	typedef Poco::SharedPtr<AsyncExecutor> Ptr;
 
 	AsyncExecutor();
-	~AsyncExecutor();
+	virtual ~AsyncExecutor();
 
 	/**
 	 * Add task to queue for executing

src/util/SequentialAsyncExecutor.h
@@ -8,12 +8,14 @@
 
 #include "loop/StoppableRunnable.h"
 #include "util/AsyncExecutor.h"
+#include "util/Loggable.h"
 
 namespace BeeeOn {
 
 class SequentialAsyncExecutor :
 	public StoppableRunnable,
-	public AsyncExecutor {
+	public AsyncExecutor,
+	Loggable {
 public:
 	typedef Poco::SharedPtr<SequentialAsyncExecutor> Ptr;
 