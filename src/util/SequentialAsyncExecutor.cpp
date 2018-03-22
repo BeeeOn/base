1cac544b59f43a0773264e1c3474db183d0cb322
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-03-22 14:42:40 +0100

    SequentialAsyncExecutor: implement finalize of tasks
    When the SequentialAsyncExecutor was requested to finish, it did
    not execute the queued tasks anymore. This can have some unwanted
    effects in certain situations.
    
    Thus, implement finalization of tasks with timeout. This means that
    based on the given timeout (negative: blocking, 0: non-blocking,
    positive: timeout blocking) when it is requested to finish, it
    finalizes the tasks in the queue.
    
    The default behaviour is non-blocking - backwards compatible.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/util/SequentialAsyncExecutor.cpp
+++ src/util/SequentialAsyncExecutor.cpp
@@ -1,3 +1,4 @@
+#include <Poco/Clock.h>
 #include <Poco/Logger.h>
 #include <Poco/ScopedLock.h>
 
@@ -7,6 +8,7 @@
 BEEEON_OBJECT_BEGIN(BeeeOn, SequentialAsyncExecutor)
 BEEEON_OBJECT_CASTABLE(AsyncExecutor)
 BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_PROPERTY("stopTimeout", &SequentialAsyncExecutor::setStopTimeout)
 BEEEON_OBJECT_END(BeeeOn, SequentialAsyncExecutor)
 
 using namespace std;
@@ -14,7 +16,8 @@ using namespace Poco;
 using namespace BeeeOn;
 
 SequentialAsyncExecutor::SequentialAsyncExecutor() :
-	m_stopRequested(false)
+	m_stopRequested(false),
+	m_stopTimeout(0)
 {
 }
 
@@ -25,6 +28,17 @@ SequentialAsyncExecutor::~SequentialAsyncExecutor()
 			"deleting object with non empty queue, tasks count: "
 			+ to_string(m_taskQueue.size()));
 	}
+}
+
+void SequentialAsyncExecutor::setStopTimeout(const Timespan &timeout)
+{
+	if (timeout > 0 && timeout < 1 * Timespan::MILLISECONDS)
+		throw InvalidArgumentException("stopTimeout must be at least 1 ms");
+
+	if (timeout < 0)
+		m_stopTimeout = -1;
+	else
+		m_stopTimeout = timeout;
 }
 
 void SequentialAsyncExecutor::invoke(std::function<void()> f)
@@ -54,12 +68,36 @@ void SequentialAsyncExecutor::run()
 		execute(task);
 	}
 
+	ScopedLock<FastMutex> guard(m_queueMutex);
+
+	if (m_stopTimeout != 0)
+		finalize();
+
 	if (!m_taskQueue.empty()) {
 		poco_warning(logger(), "exiting thread with non empty queue");
 	}
 
 	m_stopRequested = false;
 	m_stoppedEvent.set();
+}
+
+void SequentialAsyncExecutor::finalize()
+{
+	const Clock started;
+	std::function<void()> task;
+
+	while (!m_taskQueue.empty()) {
+		task = m_taskQueue.front();
+		m_taskQueue.pop();
+		execute(task);
+
+		Timespan remaining = m_stopTimeout - started.elapsed();
+		if (remaining < 1 * Timespan::MILLISECONDS)
+			remaining = 1 * Timespan::MILLISECONDS;
+
+		if (m_stopTimeout >= 0 && remaining <= 0)
+			break;
+	}
 }
 
 void SequentialAsyncExecutor::execute(std::function<void()> task)
