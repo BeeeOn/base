1b425b86bb1768e5da4100f3e05ffb4054d5cdf5
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-03-26 13:55:02 +0200

    StreamConsole: extract class from StdConsole
    The StdConsole is generalized because it was only a specific case
    of a more general StreamConsole. The StreamConsole can work with
    any pair of std streams. The StdConsole is just a wrapper with DI
    support working on the standard I/O streams.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/io/StdConsole.cpp
+++ src/io/StdConsole.cpp
@@ -11,56 +11,9 @@ BEEEON_OBJECT_PROPERTY("prompt", &StdConsole::setPrompt)
 BEEEON_OBJECT_END(BeeeOn, StdConsole)
 
 using namespace std;
-using namespace Poco;
 using namespace BeeeOn;
 
-StdConsoleSessionImpl::StdConsoleSessionImpl(Semaphore &semaphore):
-	m_semaphore(semaphore)
-{
-}
-
-StdConsoleSessionImpl::~StdConsoleSessionImpl()
-{
-	m_semaphore.set();
-}
-
-string StdConsoleSessionImpl::readUntil(const char c)
-{
-	return iosReadUntil(cin, c);
-}
-
-string StdConsoleSessionImpl::readBytes(const unsigned int length)
-{
-	return iosReadBytes(cin, length);
-}
-
-void StdConsoleSessionImpl::print(const string &text, bool newline)
-{
-	iosPrint(cout, text, newline);
-}
-
-bool StdConsoleSessionImpl::eof()
-{
-	return cin.eof();
-}
-
 StdConsole::StdConsole():
-	m_semaphore(1),
-	m_close(false)
+	StreamConsole(cin, cout)
 {
-}
-
-void StdConsole::close()
-{
-	m_close = true;
-}
-
-ConsoleSessionImpl::Ptr StdConsole::openSession()
-{
-	m_semaphore.wait();
-
-	if (m_close)
-		return closedSession();
-
-	return new StdConsoleSessionImpl(m_semaphore);
 }
--- src/io/StdConsole.h
+++ src/io/StdConsole.h
@@ -1,60 +1,16 @@
 #ifndef BEEEON_STD_CONSOLE_H
 #define BEEEON_STD_CONSOLE_H
 
-#include <iosfwd>
-
-#include <Poco/AtomicCounter.h>
-#include <Poco/SharedPtr.h>
-#include <Poco/Semaphore.h>
-
-#include "io/Console.h"
+#include "io/StreamConsole.h"
 
 namespace BeeeOn {
-
-class StdConsoleSessionImpl : public ConsoleSessionImpl {
-public:
-	typedef Poco::SharedPtr<StdConsoleSessionImpl> Ptr;
-
-	StdConsoleSessionImpl(Poco::Semaphore &semaphore);
-	~StdConsoleSessionImpl();
-
-	std::string readUntil(const char c) override;
-	std::string readBytes(const unsigned int length) override;
-	void print(const std::string &text, bool newline = true) override;
-	bool eof() override;
-
-private:
-	Poco::Semaphore &m_semaphore;
-};
 
 /**
  * Standard I/O console.
  */
-class StdConsole : public Console {
+class StdConsole : public StreamConsole {
 public:
 	StdConsole();
-
-	void close() override;
-
-protected:
-	/**
-	 * Waits until the semaphore is ready and then it
-	 * creates a new instance of the StdConsoleSessionImpl.
-	 * The semaphore is set by the StdConsoleSessionImpl's
-	 * destructor to make another session available again.
-	 */
-	ConsoleSessionImpl::Ptr openSession() override;
-
-private:
-	/**
-	 * There can be only one active session of the I/O console.
-	 * The access is controlled by the semaphore.
-	 */
-	Poco::Semaphore m_semaphore;
-	/**
-	 * Signalize closing of the console.
-	 */
-	Poco::AtomicCounter m_close;
 };
 
 }
