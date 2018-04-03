BeeeOn/base

commit f0ab4dcd15a8049942df358a17366d1c513021b5

PosixSignal: define method ignore()
Provide a way how to easily ignore handling of a certain signal.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/util/PosixSignal.cpp
@@ -26,6 +26,13 @@ void PosixSignal::send(long pid, unsigned int num)
 		throw IllegalStateException(string("failed to send signal ") + strerror(errno));
 }
 
+void PosixSignal::ignore(const unsigned int num)
+{
+	signal(num, SIG_IGN);
+	if (errno)
+		throw InvalidArgumentException(strerror(errno));
+}
+
 unsigned int PosixSignal::byName(const string &name)
 {
 	if (name == "SIGTERM")
@@ -49,4 +56,9 @@ unsigned int PosixSignal::byName(const string &name)
 void PosixSignal::send(long pid, const string name)
 {
 	send(pid, byName(name));
+}
+
+void PosixSignal::ignore(const string &name)
+{
+	ignore(byName(name));
 }

src/util/PosixSignal.h
@@ -20,6 +20,12 @@ public:
 	 */
 	static void send(long pid, const std::string name);
 
+	/**
+	 * Ignore the selected signal to not influence the current process (SIG_IGN).
+	 * @param name Name of the signal to be ignored
+	 */
+	static void ignore(const std::string &name);
+
 protected:
 	/**
 	 * Translate name of a signal to its internal representation.
@@ -27,6 +33,7 @@ protected:
 	static unsigned int byName(const std::string &name);
 
 	static void send(long pid, unsigned int num);
+	static void ignore(unsigned int num);
 };
 
 }

commit 509bacec0d111c9fb7427647d4fccc37d396a95b

PosixSignal: fix testing failed signal call
The previous testing of errno was wrong. When the signal() call
succeeds it might not clear errno and thus we throw exceptions
as false positives.

The signal() call returns SIG_ERR on error. Then the errno is
valid to be checked.

Fixes: f0ab4dcd15a8 ("PosixSignal: define method ignore()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/PosixSignal.cpp
@@ -38,8 +38,7 @@ void PosixSignal::send(const Thread &thread, unsigned int num)
 
 void PosixSignal::ignore(const unsigned int num)
 {
-	signal(num, SIG_IGN);
-	if (errno)
+	if (signal(num, SIG_IGN) == SIG_ERR)
 		throw InvalidArgumentException(strerror(errno));
 }
 