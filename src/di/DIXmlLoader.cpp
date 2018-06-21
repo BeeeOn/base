BeeeOn/base

commit a0c51f2729edde8c2dccfd21f8e1280b1607ecda

DIXmlLoader: new class
The DIXmlLoader loads multiple XML documents for DependencyInjector
and merges them into a single document.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DIXmlLoader.cpp
@@ -1,0 +1,56 @@
+#include <Poco/Exception.h>
+#include <Poco/Logger.h>
+#include <Poco/DOM/Document.h>
+#include <Poco/DOM/Element.h>
+#include <Poco/DOM/NodeList.h>
+
+#include "di/DIXmlLoader.h"
+#include "util/SecureXmlParser.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::XML;
+using namespace BeeeOn;
+
+DIXmlLoader::DIXmlLoader()
+{
+}
+
+void DIXmlLoader::load(istream &in)
+{
+	SecureXmlParser parser;
+	Document *doc = parser.parse(in);
+
+	if (doc->documentElement()->localName() != "system") {
+		throw InvalidArgumentException("unexpected XML document root element: "
+				+ doc->documentElement()->localName());
+	}
+
+	Element *factory = doc->documentElement()->getChildElement("factory");
+	if (factory == NULL)
+		throw InvalidArgumentException("no element factory in the XML file");
+
+	if (m_document.isNull()) {
+		logger().debug("parsing first XML document");
+		m_document = doc;
+	}
+	else {
+		logger().debug("parsing and merging XML document");
+
+		Element *target = m_document->documentElement()->getChildElement("factory");
+		NodeList *children = factory->childNodes();
+
+		for (unsigned long i = 0; i < children->length(); ++i) {
+			Node *node = m_document->importNode(children->item(i), true);
+			if (node == NULL)
+				throw IllegalStateException("imported node is NULL");
+
+			target->appendChild(node);
+		}
+	}
+}
+
+const Document *DIXmlLoader::document() const
+{
+	return m_document;
+}

src/CMakeLists.txt
@@ -14,6 +14,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DependencyInjector.cpp
 	${PROJECT_SOURCE_DIR}/di/DIWrapper.cpp
 	${PROJECT_SOURCE_DIR}/di/DIDaemon.cpp
+	${PROJECT_SOURCE_DIR}/di/DIXmlLoader.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableRunnable.cpp
 	${PROJECT_SOURCE_DIR}/loop/StoppableLoop.cpp
 	${PROJECT_SOURCE_DIR}/loop/LoopRunner.cpp

src/di/DIXmlLoader.h
@@ -1,0 +1,34 @@
+#ifndef BEEEON_DI_XML_LOADER_H
+#define BEEEON_DI_XML_LOADER_H
+
+#include <istream>
+
+#include <Poco/AutoPtr.h>
+
+#include "util/Loggable.h"
+
+namespace Poco {
+namespace XML {
+
+class Document;
+
+}
+}
+
+namespace BeeeOn {
+
+class DIXmlLoader : Loggable {
+public:
+	DIXmlLoader();
+
+	void load(std::istream &in);
+
+	const Poco::XML::Document *document() const;
+
+private:
+	Poco::AutoPtr<Poco::XML::Document> m_document;
+};
+
+}
+
+#endif

commit b9a75d680fd9722ced5967b52ff8ea6959baeea7

DIXmlLoader: fix leaking of memory
Fixing certain leaks reported by valgrind (on BeeeOn Gateway).

HEAP SUMMARY:
 in use at exit: 174,444 bytes in 1,463 blocks
 total heap usage: 35,840 allocs, 34,377 frees, 4,842,700 bytes allocated

24 bytes in 1 blocks are definitely lost in loss record 1 of 24
 at 0x4C2D54F: operator new(unsigned long) (vg_replace_malloc.c:334)
 by 0x6799AE2: Poco::XML::AbstractNode::childNodes() const
 by 0x4F8F3FC: BeeeOn::DIXmlLoader::load(std::istream&)
 by 0x4F86AD1: BeeeOn::DIApplicationConfigurationLoader::loadXml(Poco::File const&)
 by 0x4F8696E: BeeeOn::DIApplicationConfigurationLoader::load(Poco::File const&)
 by 0x4FFB10D: BeeeOn::AutoConfigurationExplorer::handleFile(Poco::File const&, BeeeOn::FileLoader&)
 by 0x4FFB758: BeeeOn::AutoConfigurationExplorer::exploreOne(Poco::File const&, BeeeOn::FileLoader&)
 by 0x4FFB484: BeeeOn::AutoConfigurationExplorer::exploreDirectory(Poco::Path const&, BeeeOn::FileLoader&)
 by 0x4FFBA38: BeeeOn::AutoConfigurationExplorer::explore(BeeeOn::FileLoader&)
 by 0x4F881AA: BeeeOn::DIDaemon::initialize(Poco::Util::Application&)
 by 0x5F84B83: Poco::Util::Application::run()
 by 0x4F87ED6: BeeeOn::DIDaemon::up(int, char**, BeeeOn::About const&)

Fixes: a0c51f2729ed ("DIXmlLoader: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DIXmlLoader.cpp
@@ -19,7 +19,7 @@ DIXmlLoader::DIXmlLoader()
 void DIXmlLoader::load(istream &in)
 {
 	SecureXmlParser parser;
-	Document *doc = parser.parse(in);
+	AutoPtr<Document> doc = parser.parse(in);
 
 	if (doc->documentElement()->localName() != "system") {
 		throw InvalidArgumentException("unexpected XML document root element: "
@@ -38,11 +38,11 @@ void DIXmlLoader::load(istream &in)
 		logger().debug("parsing and merging XML document");
 
 		Element *target = m_document->documentElement()->getChildElement("factory");
-		NodeList *children = factory->childNodes();
+		AutoPtr<NodeList> children = factory->childNodes();
 
 		for (unsigned long i = 0; i < children->length(); ++i) {
-			Node *node = m_document->importNode(children->item(i), true);
-			if (node == NULL)
+			AutoPtr<Node> node = m_document->importNode(children->item(i), true);
+			if (node.isNull())
 				throw IllegalStateException("imported node is NULL");
 
 			target->appendChild(node);