BeeeOn/base

commit 5979a336a37096902ac057d0c81f8cc5ce36c892

GWMessage: new class
The GWMessage is abstract class representing messages (including
their contents), that are being sent between Gateway and Server.
Messages are transfered in the JSON format.

The internal representation of a GWMessage is the Poco::JSON::Object.
All subclasses should set the values directly into that instance via
accessing method json(). This leads to a lazy-validation behaviour.

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

src/gwmessage/GWMessage.cpp
@@ -1,0 +1,61 @@
+#include "gwmessage/GWMessage.h"
+#include "util/JsonUtil.h"
+
+using namespace std;
+using namespace Poco;
+using namespace BeeeOn;
+
+const string MESSAGE_TYPE_KEY = "message_type";
+
+GWMessage::GWMessage(const GWMessageType::Raw &type):
+	m_json(new JSON::Object)
+{
+	setType(type);
+}
+
+GWMessage::GWMessage(const JSON::Object::Ptr object):
+	m_json(object)
+{
+}
+
+GWMessage::~GWMessage()
+{
+}
+
+GWMessageType GWMessage::type() const
+{
+	return GWMessageType::parse(m_json->getValue<string>(MESSAGE_TYPE_KEY));
+}
+
+std::string GWMessage::toString() const
+{
+	return Dynamic::Var::toString(m_json);
+}
+
+GWMessage::Ptr GWMessage::fromJSON(const std::string &json)
+{
+	return fromJSON(JsonUtil::parse(json));
+}
+
+GWMessage::Ptr GWMessage::fromJSON(Poco::JSON::Object::Ptr object)
+{
+	const GWMessageType &type = GWMessageType::parse(
+		object->getValue<string>(MESSAGE_TYPE_KEY)
+	);
+
+	switch(type.raw()) {
+	default:
+		throw InvalidArgumentException(
+			"unsupported message type" + type.toString());
+	}
+}
+
+JSON::Object::Ptr GWMessage::json() const
+{
+	return m_json;
+}
+
+void GWMessage::setType(const GWMessageType &type)
+{
+	m_json->set(MESSAGE_TYPE_KEY, type.toString());
+}

src/CMakeLists.txt
@@ -18,6 +18,7 @@ file(GLOB SOURCES
 	${PROJECT_SOURCE_DIR}/di/DIDaemon.cpp
 	${PROJECT_SOURCE_DIR}/di/DIWrapper.cpp
 	${PROJECT_SOURCE_DIR}/di/DIXmlLoader.cpp
+	${PROJECT_SOURCE_DIR}/gwmessage/GWMessage.cpp
 	${PROJECT_SOURCE_DIR}/gwmessage/GWMessageType.cpp
 	${PROJECT_SOURCE_DIR}/io/AutoClose.cpp
 	${PROJECT_SOURCE_DIR}/io/Console.cpp

src/gwmessage/GWMessage.h
@@ -1,0 +1,81 @@
+#ifndef BEEEON_GW_MESSAGE_H
+#define BEEEON_GW_MESSAGE_H
+
+#include <string>
+
+#include <Poco/SharedPtr.h>
+#include <Poco/JSON/Object.h>
+
+#include "gwmessage/GWMessageType.h"
+
+namespace BeeeOn {
+
+/**
+ * @brief The GWMessage is abstract class representing messages (including
+ * their contents), that are being sent between Gateway and Server.
+ *
+ * The internal representation of the GWMessage is the Poco::JSON::Object.
+ * All subclasses should set the values directly into that instance
+ * via accessing method json(). This leads to a lazy-validation behaviour.
+ */
+class GWMessage {
+public:
+	typedef Poco::SharedPtr<GWMessage> Ptr;
+
+	virtual ~GWMessage();
+
+protected:
+	/**
+	 * @brief Constructs a GWMessage depending on the given type.
+	 *
+	 * The type is set to the internal JSON::Object.
+	 */
+	GWMessage(const GWMessageType::Raw &type);
+
+	/**
+	 * @brief Constructs a GWMessage from the JSON::Object.
+	 */
+	GWMessage(const Poco::JSON::Object::Ptr object);
+
+public:
+	/**
+	 * @brief Returns the type of the message.
+	 */
+	GWMessageType type() const;
+
+	/**
+	 * @brief Returns the string representation of the message.
+	 */
+	std::string toString() const;
+
+	/**
+	 * @brief Factory method for creating subclasses of the GWMessage
+	 * from the json string.
+	 *
+	 * Type of the created message is determined by the message type
+	 * in the json string.
+	 */
+	static GWMessage::Ptr fromJSON(const std::string &json);
+
+	/**
+	 * @brief Factory method for creating subclasses of the GWMessage
+	 * from the JSON::Object.
+	 *
+	 * Type of the created message is determined by the message type
+	 * in the JSON::Object.
+	 */
+	static GWMessage::Ptr fromJSON(Poco::JSON::Object::Ptr object);
+
+protected:
+	Poco::JSON::Object::Ptr json() const;
+
+private:
+	void setType(const GWMessageType &type);
+
+private:
+	Poco::JSON::Object::Ptr m_json;
+};
+
+}
+
+#endif

test/CMakeLists.txt
@@ -23,6 +23,7 @@ file(GLOB TEST_SOURCES
 	${PROJECT_SOURCE_DIR}/di/DependencyInjectorTest.cpp
 	${PROJECT_SOURCE_DIR}/di/DIDaemonTest.cpp
 	${PROJECT_SOURCE_DIR}/di/DIWrapperTest.cpp
+	${PROJECT_SOURCE_DIR}/gwmessage/GWMessageTest.cpp
 	${PROJECT_SOURCE_DIR}/io/AutoCloseTest.cpp
 	${PROJECT_SOURCE_DIR}/io/ConsoleTest.cpp
 	${PROJECT_SOURCE_DIR}/io/FdStreamTest.cpp

test/gwmessage/GWMessageTest.cpp
@@ -1,0 +1,61 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Poco/Exception.h>
+#include <Poco/JSON/JSONException.h>
+
+#include "cppunit/BetterAssert.h"
+#include "gwmessage/GWMessage.h"
+#include "util/JsonUtil.h"
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::JSON;
+
+namespace BeeeOn {
+
+class GWMessageTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(GWMessageTest);
+	CPPUNIT_TEST(testParseEmpty);
+	CPPUNIT_TEST(testParseEmptyObject);
+	CPPUNIT_TEST(testParseUnknownType);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void testParseEmpty();
+	void testParseEmptyObject();
+	void testParseUnknownType();
+protected:
+	string jsonReformat(const string &json);
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(GWMessageTest);
+
+string GWMessageTest::jsonReformat(const string &json)
+{
+	return Dynamic::Var::toString(JsonUtil::parse(json));
+}
+
+void GWMessageTest::testParseEmpty()
+{
+	CPPUNIT_ASSERT_THROW(
+		GWMessage::fromJSON(""),
+		JSONException
+	);
+}
+
+void GWMessageTest::testParseEmptyObject()
+{
+	CPPUNIT_ASSERT_THROW(
+		GWMessage::fromJSON("{}"),
+		InvalidAccessException
+	);
+}
+
+void GWMessageTest::testParseUnknownType()
+{
+	CPPUNIT_ASSERT_THROW(
+		GWMessage::fromJSON(R"({"message_type" : "unknown_type"})"),
+		InvalidArgumentException
+	);
+}
+
+}

commit aae115491ed5c19e954858a6bfe431b359d9544f

GWMessage: fix error message
Fixes: 5979a336a370 ("GWMessage: new class")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gwmessage/GWMessage.cpp
@@ -113,7 +113,7 @@ GWMessage::Ptr GWMessage::fromJSON(Poco::JSON::Object::Ptr object)
 		return new GWUnpairResponse(object);
 	default:
 		throw InvalidArgumentException(
-			"unsupported message type" + type.toString());
+			"unsupported message type " + type.toString());
 	}
 }
 