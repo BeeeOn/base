BeeeOn/base

commit 7480a7a34ee5a46397c3ab35b6d58e0fabc27611

DevicePrefix: normalize enum names
All prefix names are now lower-case in a programming-style. The enum
values are not intended to be in human-style (for UX). For UX, we should
have translation layers that performs sufficient conversions if needed.

By using the DevicePrefixNamesInitializer, we preserve backwards
compatibility until all gateways are upgraded accordingly. Thus old
enum names (except of "Invalid") are still valid for parse().

The backwards compatibility is tested inside the GWMessageTest where
the "Fitprotocol" is string is parsed. However, we always expect that
the such enum is printed as "fitp".

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/DevicePrefix.cpp
@@ -11,27 +11,43 @@ DevicePrefixNamesInitializer::DevicePrefixNamesInitializer(
 DevicePrefixNamesInitializer::NamesMap DevicePrefixNamesInitializer::customNamesMap(
 		const ValueMap &map)
 {
-	return initNamesMap(map);
+	NamesMap namesMap = initNamesMap(map);
+
+	namesMap.emplace("Fitprotocol", map.find(DevicePrefix::PREFIX_FITPROTOCOL));
+	namesMap.emplace("PressureSensor", map.find(DevicePrefix::PREFIX_PRESSURE_SENSOR));
+	namesMap.emplace("VirtualDevice", map.find(DevicePrefix::PREFIX_VIRTUAL_DEVICE));
+	namesMap.emplace("VPT", map.find(DevicePrefix::PREFIX_VPT));
+	namesMap.emplace("OpenHAB", map.find(DevicePrefix::PREFIX_OPENHAB));
+	namesMap.emplace("Bluetooth", map.find(DevicePrefix::PREFIX_BLUETOOTH));
+	namesMap.emplace("BelkinWemo", map.find(DevicePrefix::PREFIX_BELKIN_WEMO));
+	namesMap.emplace("Z-Wave", map.find(DevicePrefix::PREFIX_ZWAVE));
+	namesMap.emplace("Jablotron", map.find(DevicePrefix::PREFIX_JABLOTRON));
+	namesMap.emplace("IQRF", map.find(DevicePrefix::PREFIX_IQRF));
+	namesMap.emplace("LEDs", map.find(DevicePrefix::PREFIX_LEDS));
+	namesMap.emplace("Philips Hue", map.find(DevicePrefix::PREFIX_PHILIPS_HUE));
+	namesMap.emplace("BeeWi", map.find(DevicePrefix::PREFIX_BEEWI));
+
+	return namesMap;
 }
 
 EnumHelper<DevicePrefixEnum::Raw>::ValueMap
 &DevicePrefixEnum::valueMap()
 {
 	static EnumHelper<DevicePrefixEnum::Raw>::ValueMap valueMap = {
-		{DevicePrefixEnum::PREFIX_INVALID, "Invalid"},
-		{DevicePrefixEnum::PREFIX_FITPROTOCOL, "Fitprotocol"},
-		{DevicePrefixEnum::PREFIX_PRESSURE_SENSOR, "PressureSensor"},
-		{DevicePrefixEnum::PREFIX_VIRTUAL_DEVICE, "VirtualDevice"},
-		{DevicePrefixEnum::PREFIX_VPT, "VPT"},
-		{DevicePrefixEnum::PREFIX_OPENHAB, "OpenHAB"},
-		{DevicePrefixEnum::PREFIX_BLUETOOTH, "Bluetooth"},
-		{DevicePrefixEnum::PREFIX_BELKIN_WEMO, "BelkinWemo"},
-		{DevicePrefixEnum::PREFIX_ZWAVE, "Z-Wave"},
-		{DevicePrefixEnum::PREFIX_JABLOTRON, "Jablotron"},
-		{DevicePrefixEnum::PREFIX_IQRF, "IQRF"},
-		{DevicePrefixEnum::PREFIX_LEDS, "LEDs"},
-		{DevicePrefixEnum::PREFIX_PHILIPS_HUE, "Philips Hue"},
-		{DevicePrefixEnum::PREFIX_BEEWI, "BeeWi"},
+		{DevicePrefixEnum::PREFIX_INVALID, "invalid"},
+		{DevicePrefixEnum::PREFIX_FITPROTOCOL, "fitp"},
+		{DevicePrefixEnum::PREFIX_PRESSURE_SENSOR, "pdev"},
+		{DevicePrefixEnum::PREFIX_VIRTUAL_DEVICE, "vdev"},
+		{DevicePrefixEnum::PREFIX_VPT, "vpt"},
+		{DevicePrefixEnum::PREFIX_OPENHAB, "open_hab"},
+		{DevicePrefixEnum::PREFIX_BLUETOOTH, "bluetooth"},
+		{DevicePrefixEnum::PREFIX_BELKIN_WEMO, "belkin_wemo"},
+		{DevicePrefixEnum::PREFIX_ZWAVE, "zwave"},
+		{DevicePrefixEnum::PREFIX_JABLOTRON, "jablotron"},
+		{DevicePrefixEnum::PREFIX_IQRF, "iqrf"},
+		{DevicePrefixEnum::PREFIX_LEDS, "leds"},
+		{DevicePrefixEnum::PREFIX_PHILIPS_HUE, "philips_hue"},
+		{DevicePrefixEnum::PREFIX_BEEWI, "beewi"},
 	};
 
 	return valueMap;

test/gwmessage/GWMessageTest.cpp
@@ -639,7 +639,7 @@ void GWMessageTest::testParseDeviceList()
 
 	GWDeviceListRequest::Ptr request = message.cast<GWDeviceListRequest>();
 	CPPUNIT_ASSERT_EQUAL("495b7a34-d2e7-4cc7-afcc-0690fa5f072a", request->id().toString());
-	CPPUNIT_ASSERT_EQUAL("Fitprotocol", request->devicePrefix().toString());
+	CPPUNIT_ASSERT_EQUAL("fitp", request->devicePrefix().toString());
 }
 
 void GWMessageTest::testCreateDeviceList()
@@ -674,7 +674,7 @@ void GWMessageTest::testCreateDeviceList()
 		jsonReformat(R"({
 			"message_type": "device_list_request",
 			"id": "a08c356b-316d-4690-84d4-b77d95b403fe",
-			"device_prefix": "Fitprotocol"
+			"device_prefix": "fitp"
 		})"),
 		request->toString()
 	);

commit 2c564eb4ce91721ef7468dfe46569c220a62cd33

DevicePrefix: fix lower-case name of pressure sensor
The old lower-case name of pressure sensor was pdev. This name
does not match the name in conf files and this name is not
a shotcut to a "Pressure sensor device". The new name is psdev.

Fixes: 7480a7a3 ("DevicePrefix: normalize enum names")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Reviewed-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/DevicePrefix.cpp
@@ -15,6 +15,7 @@ DevicePrefixNamesInitializer::NamesMap DevicePrefixNamesInitializer::customNames
 
 	namesMap.emplace("Fitprotocol", map.find(DevicePrefix::PREFIX_FITPROTOCOL));
 	namesMap.emplace("PressureSensor", map.find(DevicePrefix::PREFIX_PRESSURE_SENSOR));
+	namesMap.emplace("pdev", map.find(DevicePrefix::PREFIX_PRESSURE_SENSOR));
 	namesMap.emplace("VirtualDevice", map.find(DevicePrefix::PREFIX_VIRTUAL_DEVICE));
 	namesMap.emplace("VPT", map.find(DevicePrefix::PREFIX_VPT));
 	namesMap.emplace("OpenHAB", map.find(DevicePrefix::PREFIX_OPENHAB));
@@ -36,7 +37,7 @@ EnumHelper<DevicePrefixEnum::Raw>::ValueMap
 	static EnumHelper<DevicePrefixEnum::Raw>::ValueMap valueMap = {
 		{DevicePrefixEnum::PREFIX_INVALID, "invalid"},
 		{DevicePrefixEnum::PREFIX_FITPROTOCOL, "fitp"},
-		{DevicePrefixEnum::PREFIX_PRESSURE_SENSOR, "pdev"},
+		{DevicePrefixEnum::PREFIX_PRESSURE_SENSOR, "psdev"},
 		{DevicePrefixEnum::PREFIX_VIRTUAL_DEVICE, "vdev"},
 		{DevicePrefixEnum::PREFIX_VPT, "vpt"},
 		{DevicePrefixEnum::PREFIX_OPENHAB, "open_hab"},

test/model/DevicePrefixTest.cpp
@@ -45,7 +45,7 @@ void DevicePrefixTest::doTestParseSupported()
 
 	CPPUNIT_ASSERT_EQUAL(
 		DevicePrefix::PREFIX_PRESSURE_SENSOR,
-		DevicePrefix::parse("pdev").raw());
+		DevicePrefix::parse("psdev").raw());
 
 	CPPUNIT_ASSERT_EQUAL(
 		DevicePrefix::PREFIX_VIRTUAL_DEVICE,
@@ -106,6 +106,10 @@ void DevicePrefixTest::doTestParseLegacy()
 	CPPUNIT_ASSERT_EQUAL(
 		DevicePrefix::PREFIX_PRESSURE_SENSOR,
 		DevicePrefix::parse("PressureSensor").raw());
+
+	CPPUNIT_ASSERT_EQUAL(
+		DevicePrefix::PREFIX_PRESSURE_SENSOR,
+		DevicePrefix::parse("pdev").raw());
 
 	CPPUNIT_ASSERT_EQUAL(
 		DevicePrefix::PREFIX_VIRTUAL_DEVICE,
@@ -189,7 +193,7 @@ void DevicePrefixTest::testToString()
 		DevicePrefix(DevicePrefix::PREFIX_FITPROTOCOL).toString());
 
 	CPPUNIT_ASSERT_EQUAL(
-		"pdev",
+		"psdev",
 		DevicePrefix(DevicePrefix::PREFIX_PRESSURE_SENSOR).toString());
 
 	CPPUNIT_ASSERT_EQUAL(