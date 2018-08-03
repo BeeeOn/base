BeeeOn/base

commit fbdb6dfa79f3d46d8f00ed61c3abad0375b78b5c

GWDeviceListResponse: extend by module values
The GWDeviceListResponse can now hold also recent module values. This
would allow reduce server-gateway traffic because there can be only
one request per a device prefix to collect all the needed information.
It would also simplify device managers in Gateway.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gwmessage/GWDeviceListResponse.cpp
@@ -41,3 +41,46 @@ vector<DeviceID> GWDeviceListResponse::devices() const
 	return devices;
 }
 
+void GWDeviceListResponse::setModulesValues(
+	const DeviceID &id,
+	const map<ModuleID, double> &modulesValues)
+{
+	if (!json()->has("values") || !json()->isObject("values")) {
+		JSON::Object::Ptr empty = new JSON::Object;
+		json()->set("values", empty);
+	}
+
+	JSON::Object::Ptr values = json()->getObject("values");
+
+	if (!values->has(id.toString()) || !values->isObject(id.toString())) {
+		JSON::Object::Ptr empty = new JSON::Object;
+		values->set(id.toString(), empty);
+	}
+
+	JSON::Object::Ptr device = values->getObject(id.toString());
+
+	for (const auto &pair : modulesValues)
+		device->set(pair.first.toString(), pair.second);
+}
+
+map<ModuleID, double> GWDeviceListResponse::modulesValues(
+		const DeviceID &id) const
+{
+	map<ModuleID, double> result;
+
+	JSON::Object::Ptr values = json()->getObject("values");
+	if (values.isNull())
+		return result;
+
+	JSON::Object::Ptr device = values->getObject(id.toString());
+	if (device.isNull())
+		return result;
+
+	for (const auto &pair : *device) {
+		result.emplace(
+			ModuleID::parse(pair.first),
+			pair.second.extract<double>());
+	}
+
+	return result;
+}

test/gwmessage/GWMessageTest.cpp
@@ -59,7 +59,9 @@ class GWMessageTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST(testParseLastValue);
 	CPPUNIT_TEST(testCreateLastValue);
 	CPPUNIT_TEST(testParseDeviceList);
+	CPPUNIT_TEST(testParseDeviceListWithValues);
 	CPPUNIT_TEST(testCreateDeviceList);
+	CPPUNIT_TEST(testCreateDeviceListWithValues);
 	CPPUNIT_TEST(testParseListen);
 	CPPUNIT_TEST(testCreateListen);
 	CPPUNIT_TEST(testParsePing);
@@ -93,7 +95,9 @@ public:
 	void testParseLastValue();
 	void testCreateLastValue();
 	void testParseDeviceList();
+	void testParseDeviceListWithValues();
 	void testCreateDeviceList();
+	void testCreateDeviceListWithValues();
 	void testParseListen();
 	void testCreateListen();
 	void testParsePing();
@@ -794,6 +798,54 @@ void GWMessageTest::testParseDeviceList()
 	CPPUNIT_ASSERT_EQUAL("fitp", request->devicePrefix().toString());
 }
 
+void GWMessageTest::testParseDeviceListWithValues()
+{
+	GWMessage::Ptr message = GWMessage::fromJSON(
+	R"({
+			"message_type": "device_list_response",
+			"id": "495b7a34-d2e7-4cc7-afcc-0690fa5f072a",
+			"status": 1,
+			"devices": [
+				{"device_id": "0xa15410132465788"},
+				{"device_id": "0xa15410132465789"},
+				{"device_id": "0xa15410132465790"}
+			],
+			"values": {
+				"0xa15410132465788": {
+					"0": 15.0,
+					"2": 24.4
+				},
+				"0xa15410132465790": {
+					"1": -1.0
+				}
+			}
+	})");
+
+	CPPUNIT_ASSERT_EQUAL(GWMessageType::DEVICE_LIST_RESPONSE, message->type().raw());
+	CPPUNIT_ASSERT(!message.cast<GWDeviceListResponse>().isNull());
+
+	GWDeviceListResponse::Ptr response = message.cast<GWDeviceListResponse>();
+	CPPUNIT_ASSERT_EQUAL("495b7a34-d2e7-4cc7-afcc-0690fa5f072a", response->id().toString());
+	CPPUNIT_ASSERT_EQUAL(GWResponse::Status::SUCCESS, response->status());
+
+	vector<DeviceID> devices = response->devices();
+	CPPUNIT_ASSERT_EQUAL("0xa15410132465788", devices[0].toString());
+	CPPUNIT_ASSERT_EQUAL("0xa15410132465789", devices[1].toString());
+	CPPUNIT_ASSERT_EQUAL("0xa15410132465790", devices[2].toString());
+
+	const auto &values0 = response->modulesValues(0xa15410132465788);
+	CPPUNIT_ASSERT_EQUAL(2, values0.size());
+	CPPUNIT_ASSERT_EQUAL(15.0, values0.at(0));
+	CPPUNIT_ASSERT_EQUAL(24.4, values0.at(2));
+
+	const auto &values1 = response->modulesValues(0xa15410132465789);
+	CPPUNIT_ASSERT(values1.empty());
+
+	const auto &values2 = response->modulesValues(0xa15410132465790);
+	CPPUNIT_ASSERT_EQUAL(1, values2.size());
+	CPPUNIT_ASSERT_EQUAL(-1.0, values2.at(1));
+}
+
 void GWMessageTest::testCreateDeviceList()
 {
 	GWDeviceListResponse::Ptr response(new GWDeviceListResponse);
@@ -829,6 +881,46 @@ void GWMessageTest::testCreateDeviceList()
 			"device_prefix": "fitp"
 		})"),
 		request->toString()
+	);
+}
+
+void GWMessageTest::testCreateDeviceListWithValues()
+{
+	GWDeviceListResponse::Ptr response(new GWDeviceListResponse);
+	response->setID(GlobalID::parse("a08c356b-316d-4690-84d4-b77d95b403fe"));
+	response->setStatus(GWResponse::Status::SUCCESS);
+
+	vector<DeviceID> devices;
+	devices.push_back(DeviceID::parse("0xa1001234567890a0"));
+	devices.push_back(DeviceID::parse("0xa1001234567890a1"));
+	response->setDevices(devices);
+	response->setModulesValues(
+		DeviceID::parse("0xa1001234567890a0"),
+		{{0, 20.0}, {3, 1.0}});
+	response->setModulesValues(
+		DeviceID::parse("0xa1001234567890a1"),
+		{{2, 0.0}});
+
+	CPPUNIT_ASSERT_EQUAL(
+		jsonReformat(R"({
+			"message_type": "device_list_response",
+			"id": "a08c356b-316d-4690-84d4-b77d95b403fe",
+			"status": 1,
+			"devices": [
+				{"device_id": "0xa1001234567890a0"},
+				{"device_id": "0xa1001234567890a1"}
+			],
+			"values": {
+				"0xa1001234567890a0": {
+					"0": 20.0,
+					"3": 1.0
+				},
+				"0xa1001234567890a1": {
+					"2": 0.0
+				}
+			}
+		})"),
+		response->toString()
 	);
 }
 

src/gwmessage/GWDeviceListResponse.h
@@ -1,13 +1,16 @@
 #ifndef BEEEON_GW_MESSAGE_DEVICE_LIST_RESPONSE_H
 #define BEEEON_GW_MESSAGE_DEVICE_LIST_RESPONSE_H
 
+#include <map>
 #include <vector>
 
 #include <Poco/SharedPtr.h>
+#include <Poco/JSON/Array.h>
 #include <Poco/JSON/Object.h>
 
 #include "gwmessage/GWResponse.h"
 #include "model/DeviceID.h"
+#include "model/ModuleID.h"
 
 namespace BeeeOn {
 
@@ -26,6 +29,11 @@ public:
 
 	void setDevices(const std::vector<DeviceID> &devices);
 	std::vector<DeviceID> devices() const;
+
+	void setModulesValues(
+		const DeviceID &device,
+		const std::map<ModuleID, double> &values);
+	std::map<ModuleID, double> modulesValues(const DeviceID &device) const;
 };
 
 }

commit 53b9d9180fb1aef3e78e527d6579ee785936faae

GWDeviceListResponse: fix value extraction
When extracting the value out of a GWDeviceListResponse, we must
consider that the number is either an integer or decimal. For integers,
the call to extract<double>() would fail with BadCastException.
Use convert<double>() instead.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Fixes: fbdb6dfa79f3 ("GWDeviceListResponse: extend by module values")

src/gwmessage/GWDeviceListResponse.cpp
@@ -79,7 +79,7 @@ map<ModuleID, double> GWDeviceListResponse::modulesValues(
 	for (const auto &pair : *device) {
 		result.emplace(
 			ModuleID::parse(pair.first),
-			pair.second.extract<double>());
+			pair.second.convert<double>());
 	}
 
 	return result;

test/gwmessage/GWMessageTest.cpp
@@ -769,7 +769,16 @@ void GWMessageTest::testParseDeviceList()
 			"devices": [
 				{"device_id": "0xa15410132465788"},
 				{"device_id": "0xa15410132465789"}
-			]
+			],
+			"values" : {
+			    "0xa15410132465788" : {
+			      "0" : 0,
+			      "1" : 45.5
+			    },
+			    "0xa15410132465789" : {
+			      "0" : -54.2
+			    }
+			}
 	})");
 
 	CPPUNIT_ASSERT_EQUAL(GWMessageType::DEVICE_LIST_RESPONSE, message->type().raw());
@@ -782,6 +791,15 @@ void GWMessageTest::testParseDeviceList()
 	vector<DeviceID> devices = response->devices();
 	CPPUNIT_ASSERT_EQUAL("0xa15410132465788", devices[0].toString());
 	CPPUNIT_ASSERT_EQUAL("0xa15410132465789", devices[1].toString());
+
+	const auto values0 = response->modulesValues(DeviceID::parse("0xa15410132465788"));
+	CPPUNIT_ASSERT_EQUAL(2, values0.size());
+	CPPUNIT_ASSERT_EQUAL(0.0, values0.at(0));
+	CPPUNIT_ASSERT_EQUAL(45.5, values0.at(1));
+
+	const auto values1 = response->modulesValues(DeviceID::parse("0xa15410132465789"));
+	CPPUNIT_ASSERT_EQUAL(1, values1.size());
+	CPPUNIT_ASSERT_EQUAL(-54.2, values1.at(0));
 
 	message = GWMessage::fromJSON(
 	R"({