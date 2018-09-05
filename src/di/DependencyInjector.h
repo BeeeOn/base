BeeeOn/base

commit 9982474a6128812195743c7928573733183e1754

DependencyInjector: support set of map of strings
It is now possible to create setters accepting std::map instances
and inject them via the dependency injection. The XML configuration
would like this way:

  <instance name="someMapper" class="SomeMapper">
    <set name="mapping">
      <pair key="0" text="a" />
      <pair key="1" text="b" />
      <pair key="2" text="c" />
    </set>
  </instance>

Only string -> string map is currently supported, however, the solution
is extendable. To support some other combination, it is necessary to
implement another DIWMapSetter class that performs the appropriate
conversions and DependencyInjector to read another attributes then @text.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DependencyInjector.h
@@ -44,6 +44,14 @@ class InstanceInfo;
  *   + attribute number - value to add is an integer
  *   + attribute if-yes - optional condition to avoid applying the "add"
  *
+ * * map: constructs a map of key-value pairs
+ *   + attribute name   - name of the property to inject
+ *   + attribute if-yes - optional constructs to avoid applying the "map"
+ *
+ *   * subelement: pair
+ *     + attribute key  - key of the map pair
+ *     + attribute text - value of the pair as string
+ *
  * * alias: defines alias name of an existing instance
  *   + attribute name   - name of the alias (new instance name)
  *   + attribute ref    - name of an existing instance
@@ -65,6 +73,13 @@ class InstanceInfo;
  *		<add name="listeners"   ref="errorListener" />
  *		<set name="whitelist"   list="192.168.5.5,192.168.15.1" />
  *	</instance>
+ *
+ *      <instance name="nameMap" class="BeeeOn::NameMap">
+ *		<map name="mapping">
+ *			<pair key="first" text="The First One" />
+ *			<pair key="second" text="The Second One" />
+ *		</map>
+ *      </instance>
  *
  *	<instance name="userService" class="BeeeOn::UserService" />
  *	<instance name="loginListener" class="BeeeOn::LoginListener" />
@@ -144,6 +159,10 @@ private:
 			const std::string &key,
 			const std::string &name);
 	bool tryInjectList(const InstanceInfo &info,
+			DIWrapper *target,
+			const std::string &key,
+			const std::string &name);
+	bool tryInjectMap(const InstanceInfo &info,
 			DIWrapper *target,
 			const std::string &key,
 			const std::string &name);

src/di/DIWrapper.cpp
@@ -64,6 +64,10 @@ DIWListSetter::~DIWListSetter()
 {
 }
 
+DIWMapSetter::~DIWMapSetter()
+{
+}
+
 DIWHook::~DIWHook()
 {
 }

src/di/DIWrapper.h
@@ -216,6 +216,28 @@ private:
 	Setter m_setter;
 };
 
+struct DIWMapSetter : public DIWMethodHelper {
+	virtual ~DIWMapSetter();
+	virtual void call(DIWrapper &b,
+			  const std::map<Poco::Dynamic::Var, Poco::Dynamic::Var> &m) = 0;
+};
+
+template <typename T, typename B>
+class DIWStringStringMapSetter final : public DIWMapSetter {
+public:
+	typedef void (B::*Setter)(const std::map<std::string, std::string> &);
+
+	DIWStringStringMapSetter(Setter setter):
+		m_setter(setter)
+	{
+	}
+
+	void call(DIWrapper &b,
+		  const std::map<Poco::Dynamic::Var, Poco::Dynamic::Var> &m) override;
+private:
+	Setter m_setter;
+};
+
 /**
  * Interface to a casting implementation. There is also a static
  * registry of possible casts so anybody can access it.
@@ -297,6 +319,8 @@ protected:
 			const std::string &value) = 0;
 	virtual void injectList(const std::string &name,
 			const std::list<Poco::Dynamic::Var> &l) = 0;
+	virtual void injectMap(const std::string &name,
+			const std::map<Poco::Dynamic::Var, Poco::Dynamic::Var> &l) = 0;
 	virtual void callHook(const std::string &name) = 0;
 };
 
@@ -325,6 +349,8 @@ protected:
 	void injectText(const std::string &name, const std::string &value) override;
 	void injectList(const std::string &name,
 			const std::list<Poco::Dynamic::Var> &l) override;
+	void injectMap(const std::string &name,
+		       const std::map<Poco::Dynamic::Var, Poco::Dynamic::Var> &m) override;
 	void callHook(const std::string &name) override;
 
 	template <typename B, typename I>
@@ -351,6 +377,10 @@ protected:
 	template <typename B>
 	void listSetter(const std::string &name,
 		void (B::*setter)(const std::list<std::string> &));
+
+	template <typename B>
+	void mapSetter(const std::string &name,
+		void (B::*setter)(const std::map<std::string, std::string> &));
 
 	template <typename B>
 	void hookHandler(const std::string &name, void (B::*hook)());
@@ -452,6 +482,18 @@ void DIWStringListSetter<T, B>::call(DIWrapper &b, const std::list<Poco::Dynamic
 	std::list<std::string> value;
 	for (auto &v : l)
 		value.push_back(v.toString());
+
+	B &base = extractInstance<T, B>(b);
+	(base.*m_setter)(value);
+}
+
+template <typename T, typename B>
+void DIWStringStringMapSetter<T, B>::call(DIWrapper &b,
+			const std::map<Poco::Dynamic::Var, Poco::Dynamic::Var> &m)
+{
+	std::map<std::string, std::string> value;
+	for (auto &pair : m)
+		value.emplace(pair.first.toString(), pair.second.toString());
 
 	B &base = extractInstance<T, B>(b);
 	(base.*m_setter)(value);
@@ -603,6 +645,22 @@ void AbstractDIWrapper<T>::injectList(
 }
 
 template <typename T>
+void AbstractDIWrapper<T>::injectMap(
+		const std::string &name,
+		const std::map<Poco::Dynamic::Var, Poco::Dynamic::Var> &value)
+{
+	auto entry = m_method.find(name);
+	if (entry == m_method.end()) {
+		throw Poco::NotFoundException("missing map property "
+				+ name + " for type "
+				+ typeid(T).name());
+	}
+
+	DIWMapSetter &setter = dynamic_cast<DIWMapSetter &>(*(entry->second));
+	setter.call(*this, value);
+}
+
+template <typename T>
 void AbstractDIWrapper<T>::callHook(const std::string &name)
 {
 	auto entry = m_method.find(name);
@@ -684,6 +742,14 @@ void AbstractDIWrapper<T>::listSetter(
 		void (B::*setter)(const std::list<std::string> &))
 {
 	installMethod(name, new DIWStringListSetter<T, B>(setter));
+}
+
+template <typename T> template <typename B>
+void AbstractDIWrapper<T>::mapSetter(
+		const std::string &name,
+		void (B::*setter)(const std::map<std::string, std::string> &))
+{
+	installMethod(name, new DIWStringStringMapSetter<T, B>(setter));
 }
 
 template <typename T> template <typename B>
@@ -771,6 +837,8 @@ BEEEON_WRAPPER(cls, cls##DIW)
 	textSetter(name, method);
 #define BEEEON_OBJECT_LIST(name, method) \
 	listSetter(name, method);
+#define BEEEON_OBJECT_MAP(name, method) \
+	mapSetter(name, method);
 #define BEEEON_OBJECT_HOOK(name, method) \
 	hookHandler(name, method);
 

src/di/DependencyInjector.cpp
@@ -298,6 +298,56 @@ bool DependencyInjector::tryInjectList(
 	return false;
 }
 
+bool DependencyInjector::tryInjectMap(
+		const InstanceInfo &info,
+		DIWrapper *target,
+		const string &key,
+		const string &name)
+{
+	if (!m_conf->has(key + ".pair") && !m_conf->has(key + ".pair[1]"))
+		return false;
+
+	AbstractConfiguration::Keys keys;
+	m_conf->keys(key, keys);
+
+	map<Var, Var> m;
+
+	for (auto &pair : keys) {
+		if (!m_conf->has(key + "." + pair + "[@key]")) {
+			logger().warning("missing attribute @key for "
+				+ key + "." + pair);
+			continue;
+		}
+
+		if (!m_conf->has(key + "." + pair + "[@text]")) {
+			logger().error("missing attribute @text for "
+				+ key + "." + pair);
+			return false;
+		}
+
+		const string &mapKey = m_conf->getString(
+				key + "." + pair + "[@key]");
+		const string &mapText = m_conf->getString(
+				key + "." + pair + "[@text]");
+
+		logger().debug("map pair " + mapKey + " -> " + mapText,
+			       __FILE__, __LINE__);
+
+		if (!m.emplace(mapKey, mapText).second) {
+			throw InvalidArgumentException(
+				"duplicate map key " + mapKey + " for " + key
+				+ " when injecting into " + info.name()
+			);
+		}
+	}
+
+	logger().debug("injecting map " + name + " into " + info.name(),
+			__FILE__, __LINE__);
+
+	target->injectMap(name, m);
+	return true;
+}
+
 void DependencyInjector::injectValue(
 		const InstanceInfo &info,
 		DIWrapper *target,
@@ -314,6 +364,9 @@ void DependencyInjector::injectValue(
 		return;
 
 	if (tryInjectList(info, target, key, name))
+		return;
+
+	if (tryInjectMap(info, target, key, name))
 		return;
 
 	logger().error("malformed configuration entry "

test/di/DIWrapperTest.cpp
@@ -58,6 +58,11 @@ public:
 			m_list.push_back(e);
 	}
 
+	void setMap(const map<string, string> &m)
+	{
+		m_map = m;
+	}
+
 	void call()
 	{
 		m_called = true;
@@ -68,6 +73,7 @@ public:
 	int m_offset = 0;
 	DITest *m_self = NULL;
 	vector<string> m_list;
+	map<string, string> m_map;
 	bool m_called = false;
 };
 
@@ -82,6 +88,7 @@ BEEEON_OBJECT_TEXT("char", &DITest::setChar)
 BEEEON_OBJECT_NUMBER("offset", &DITest::setOffset)
 BEEEON_OBJECT_REF("self", &DITest::setSelf)
 BEEEON_OBJECT_LIST("list", &DITest::setList)
+BEEEON_OBJECT_MAP("map", &DITest::setMap)
 BEEEON_OBJECT_HOOK("call", &DITest::call)
 BEEEON_OBJECT_END(BeeeOn, DITest)
 
@@ -92,6 +99,7 @@ BEEEON_OBJECT_TEXT("char", &DITest::setChar)
 BEEEON_OBJECT_NUMBER("offset", &DITestChild::setOffset)
 BEEEON_OBJECT_REF("self", &DITestChild::setSelf)
 BEEEON_OBJECT_LIST("list", &DITestChild::setList)
+BEEEON_OBJECT_MAP("map", &DITestChild::setMap)
 BEEEON_OBJECT_HOOK("call", &DITestChild::call)
 BEEEON_OBJECT_END(BeeeOn, DITestChild)
 
@@ -102,6 +110,7 @@ struct ProtectedAccess : DIWrapper {
 	using DIWrapper::injectNumber;
 	using DIWrapper::injectText;
 	using DIWrapper::injectList;
+	using DIWrapper::injectMap;
 	using DIWrapper::callHook;
 };
 
@@ -126,6 +135,8 @@ void DIWrapperTest::testCreate()
 	ACCESS_CALL(wrapper, injectNumber)("offset", 16);
 	ACCESS_CALL(wrapper, injectRef)("self", wrapper);
 	ACCESS_CALL(wrapper, injectList)("list", {"a", "b", "c"});
+	ACCESS_CALL(wrapper, injectMap)("map",
+			{{"a", "1"}, {"b", "2"}, {"c", "3"}});
 	ACCESS_CALL(wrapper, callHook)("call");
 
 	CPPUNIT_ASSERT_EQUAL("TEST NAME", test->m_name);
@@ -135,6 +146,9 @@ void DIWrapperTest::testCreate()
 	CPPUNIT_ASSERT_EQUAL("a", test->m_list[0]);
 	CPPUNIT_ASSERT_EQUAL("b", test->m_list[1]);
 	CPPUNIT_ASSERT_EQUAL("c", test->m_list[2]);
+	CPPUNIT_ASSERT_EQUAL("1", test->m_map["a"]);
+	CPPUNIT_ASSERT_EQUAL("2", test->m_map["b"]);
+	CPPUNIT_ASSERT_EQUAL("3", test->m_map["c"]);
 	CPPUNIT_ASSERT(test->m_called);
 }
 
@@ -181,6 +195,8 @@ void DIWrapperTest::testPolymorphicBehaviour()
 	ACCESS_CALL(*wrapper, injectNumber)("offset", 18);
 	ACCESS_CALL(*wrapper, injectRef)("self", *w);
 	ACCESS_CALL(*wrapper, injectList)("list", {"a", "b", "c"});
+	ACCESS_CALL(*wrapper, injectMap)("map",
+			{{"a", "1"}, {"b", "2"}, {"c", "3"}});
 	ACCESS_CALL(*wrapper, callHook)("call");
 
 	CPPUNIT_ASSERT_EQUAL("TEST NAME2", test->m_name);
@@ -190,6 +206,9 @@ void DIWrapperTest::testPolymorphicBehaviour()
 	CPPUNIT_ASSERT_EQUAL("a", test->m_list[0]);
 	CPPUNIT_ASSERT_EQUAL("b", test->m_list[1]);
 	CPPUNIT_ASSERT_EQUAL("c", test->m_list[2]);
+	CPPUNIT_ASSERT_EQUAL("1", test->m_map["a"]);
+	CPPUNIT_ASSERT_EQUAL("2", test->m_map["b"]);
+	CPPUNIT_ASSERT_EQUAL("3", test->m_map["c"]);
 	CPPUNIT_ASSERT(test->m_called);
 
 	delete w;
@@ -213,6 +232,8 @@ void DIWrapperTest::testInheritanceOfTarget()
 	ACCESS_CALL(wrapper, injectNumber)("offset", 19);
 	ACCESS_CALL(wrapper, injectRef)("self", wrapper);
 	ACCESS_CALL(wrapper, injectList)("list", {"a", "b", "c"});
+	ACCESS_CALL(wrapper, injectMap)("map",
+			{{"a", "1"}, {"b", "2"}, {"c", "3"}});
 	ACCESS_CALL(wrapper, callHook)("call");
 
 	CPPUNIT_ASSERT_EQUAL("TEST NAME3", test->m_name);
@@ -222,6 +243,9 @@ void DIWrapperTest::testInheritanceOfTarget()
 	CPPUNIT_ASSERT_EQUAL("a", test->m_list[0]);
 	CPPUNIT_ASSERT_EQUAL("b", test->m_list[1]);
 	CPPUNIT_ASSERT_EQUAL("c", test->m_list[2]);
+	CPPUNIT_ASSERT_EQUAL("1", test->m_map["a"]);
+	CPPUNIT_ASSERT_EQUAL("2", test->m_map["b"]);
+	CPPUNIT_ASSERT_EQUAL("3", test->m_map["c"]);
 	CPPUNIT_ASSERT(test->m_called);
 }
 

test/di/DependencyInjectorTest.cpp
@@ -79,12 +79,18 @@ public:
 		for (auto &s : l)
 			m_list.push_back(s);
 	}
+
+	void setMap(const map<string, string> &m)
+	{
+		m_map = m;
+	}
 public:
 	FakeObject *m_self;
 	string m_name;
 	string m_other;
 	int m_index;
 	vector<string> m_list;
+	map<string, string> m_map;
 };
 
 }
@@ -95,6 +101,7 @@ BEEEON_OBJECT_TEXT("name", &FakeObject::setName)
 BEEEON_OBJECT_NUMBER("index", &FakeObject::setIndex)
 BEEEON_OBJECT_TEXT("other", &FakeObject::setOther)
 BEEEON_OBJECT_LIST("list", &FakeObject::setList)
+BEEEON_OBJECT_MAP("map", &FakeObject::setMap)
 BEEEON_OBJECT_END(BeeeOn, FakeObject)
 
 namespace BeeeOn {
@@ -118,6 +125,13 @@ void DependencyInjectorTest::setUp()
 	m_config->setString("instance[1].set[3][@number]", "5");
 	m_config->setString("instance[1].set[4][@name]", "list");
 	m_config->setString("instance[1].set[4][@list]", "a,b,c");
+	m_config->setString("instance[1].set[5][@name]", "map");
+	m_config->setString("instance[1].set[5].pair[1][@key]",  "a");
+	m_config->setString("instance[1].set[5].pair[1][@text]", "1");
+	m_config->setString("instance[1].set[5].pair[2][@key]",  "b");
+	m_config->setString("instance[1].set[5].pair[2][@text]", "2");
+	m_config->setString("instance[1].set[5].pair[3][@key]",  "c");
+	m_config->setString("instance[1].set[5].pair[3][@text]", "3");
 	m_config->setString("instance[2][@name]", "variable");
 	m_config->setString("instance[2][@class]", "BeeeOn::FakeObject");
 	m_config->setString("instance[2].set[1][@name]", "name");
@@ -169,6 +183,9 @@ void DependencyInjectorTest::testSimple()
 	CPPUNIT_ASSERT_EQUAL("a", fake->m_list[0]);
 	CPPUNIT_ASSERT_EQUAL("b", fake->m_list[1]);
 	CPPUNIT_ASSERT_EQUAL("c", fake->m_list[2]);
+	CPPUNIT_ASSERT_EQUAL("1", fake->m_map["a"]);
+	CPPUNIT_ASSERT_EQUAL("2", fake->m_map["b"]);
+	CPPUNIT_ASSERT_EQUAL("3", fake->m_map["c"]);
 }
 
 /**

commit e102d77d8e9bce4e0224ccfb0fb662e8077eb006

DependencyInjector: fix doc comment about pairs
Fixes: 9982474a6128 ("DependencyInjector: support set of map of strings")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DependencyInjector.h
@@ -46,7 +46,7 @@ class InstanceInfo;
  *   + attribute number - value to add is a number
  *   + attribute if-yes - optional condition to avoid applying the "add"
  *
- * * map: constructs a map of key-value pairs
+ * * set/add: constructs a map of key-value pairs
  *   + attribute name   - name of the property to inject
  *   + attribute if-yes - optional constructs to avoid applying the "map"
  *
@@ -79,10 +79,10 @@ class InstanceInfo;
  *	</instance>
  *
  *      <instance name="nameMap" class="BeeeOn::NameMap">
- *		<map name="mapping">
+ *		<set name="mapping">
  *			<pair key="first" text="The First One" />
  *			<pair key="second" text="The Second One" />
- *		</map>
+ *		</set>
  *      </instance>
  *
  *	<instance name="userService" class="BeeeOn::UserService" />