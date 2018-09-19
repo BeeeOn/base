BeeeOn/base

commit 570b3eba07a19fe4a8e23df1975979d265b1e89a

Loggable: new static method logException()
The method logException() allows to log Poco Exceptions in a more
verbose way. It recognizes exceptions with traces (BeeeOn::WithTrace)
and also appends message from the nested exception if any.

The Message contains 2 fields in its contents:

* 'exception' - class name of the exception
* 'code' - code used for initializing of the exception (usually 0)

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Loggable.h
@@ -4,6 +4,8 @@
 #include <typeinfo>
 #include <string>
 
+#include <Poco/Message.h>
+
 namespace Poco {
 
 class Logger;
@@ -33,6 +35,13 @@ public:
 
 	static void configureSimple(
 			Poco::Logger &logger, const std::string &level);
+
+	static void logException(
+			Poco::Logger &logger,
+			const Poco::Message::Priority priority,
+			const Poco::Exception &e,
+			const char *file,
+			size_t line);
 
 protected:
 	void setupLogger(Poco::Logger *logger = 0) const;

src/util/Loggable.cpp
@@ -6,6 +6,7 @@
 
 #include "util/ClassInfo.h"
 #include "util/Loggable.h"
+#include "util/WithTrace.h"
 
 using namespace std;
 using namespace Poco;
@@ -64,4 +65,37 @@ void Loggable::configureSimple(
 	PatternFormatter *formatter = new PatternFormatter;
 	formatter->setProperty(PatternFormatter::PROP_PATTERN, "%q %t (%U:%u)");
 	logger.setChannel(new FormattingChannel(formatter, new ConsoleChannel));
+}
+
+void Loggable::logException(
+		Logger &logger,
+		Message::Priority priority,
+		const Exception &e,
+		const char *file,
+		size_t line)
+{
+	const Exception *nested = e.nested();
+	string text = e.displayText();
+
+	if (nested) {
+		if (Throwable::traceOf(e).size() > 0)
+			text += "\n";
+		else
+			text += " ";
+
+		text += "caused by ";
+		text += nested->displayText();
+		text += "";
+	}
+
+	Message msg;
+	msg.setSource(logger.name());
+	msg.setPriority(priority);
+	msg.setText(text);
+	msg.setSourceFile(file);
+	msg.setSourceLine(line);
+	msg.set("exception", ClassInfo(e).name());
+	msg.set("code", to_string(e.code()));
+
+	logger.log(msg);
 }

commit 761ac9d47bf85cc7eddc42a20e8f273783889cf0

Loggable: fix missing include of Poco/Exception.h
Fixes: 570b3eba07a1 ("Loggable: new static method logException()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/util/Loggable.h
@@ -3,6 +3,7 @@
 #include <typeinfo>
 #include <string>
 
+#include <Poco/Exception.h>
 #include <Poco/Message.h>
 
 namespace Poco {