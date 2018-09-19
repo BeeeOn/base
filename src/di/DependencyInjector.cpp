BeeeOn/base

commit 7e453aa21dab738065b40cbad923aa0d4100e2fe

DependencyInjector: destroy instances from failed createEarly()
When the call to createEarly() throws an exception from the construct,
we have to make sure that the instances are correctly destroyed in this
case.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DependencyInjector.cpp
@@ -166,8 +166,13 @@ DependencyInjector::DependencyInjector(
 		bool avoidEarly):
 	m_conf(conf)
 {
-	if (!avoidEarly)
-		createEarly();
+	if (!avoidEarly) {
+		try {
+			createEarly();
+		}
+		BEEEON_CATCH_CHAIN_ACTION(logger(),
+			destroyAll())
+	}
 }
 
 void DependencyInjector::destroyAll()

commit 45fff19907cf37c6cfde495708a88678569e8d41

DependencyInjector: rethrow when createEarly() fails
If we do not rethrow, the DependencyInjector is successfully created
and nobody notices that something bad happened.

Fixes: 7e453aa21dab ("DependencyInjector: destroy instances from failed createEarly()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/di/DependencyInjector.cpp
@@ -174,7 +174,8 @@ DependencyInjector::DependencyInjector(
 			createEarly();
 		}
 		BEEEON_CATCH_CHAIN_ACTION(logger(),
-			destroyAll())
+			destroyAll();
+			throw)
 	}
 }
 