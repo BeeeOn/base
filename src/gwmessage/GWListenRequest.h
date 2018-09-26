8028b17c823fc810b95efecead24238af8127d0f
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-09-26 13:40:56 +0200

    GWListenRequest: add example message into documentation
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/gwmessage/GWListenRequest.h
+++ src/gwmessage/GWListenRequest.h
@@ -15,6 +15,15 @@ namespace BeeeOn {
  *
  * The listening mode has typically a limited duration. After the duration
  * exceeds, the listening mode is automatically turned off.
+ *
+ * An example message:
+ * <pre>
+ * {
+ *   "id": "c149f44a-ab44-459a-b18f-a5c33d3d3c3c",
+ *   "message_type": "listen_request",
+ *   "duration": 60
+ * }
+ * </pre>
  */
 class GWListenRequest : public GWRequest {
 public:
