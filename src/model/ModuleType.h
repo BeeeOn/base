BeeeOn/base

commit c23344e1d2b324191e1745e71f22618e3163ebe4

ModuleType: support types and attributes for Jablotron
Support of types:

 * fire
 * open/close
 * security alert
 * shake

Support of attributes:

 * manual only

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/ModuleType.h
@@ -24,6 +24,10 @@ public:
 			 */
 			TYPE_INNER,
 			/**
+			 * The sensor enables to set (to modify) value only from sensor.
+			 */
+			TYPE_MANUAL_ONLY,
+			/**
 			 * The sensor/actuator is located outside.
 			 */
 			TYPE_OUTER,
@@ -45,6 +49,11 @@ public:
 			 */
 			TYPE_CO2,
 			/**
+			 * Value 0: no fire detected
+			 * Value 1: fire detected
+			 */
+			TYPE_FIRE,
+			/**
 			 * Range: 0..100
 			 * Unit: %
 			 */
@@ -59,6 +68,11 @@ public:
 			 */
 			TYPE_NOISE,
 			/**
+			 * Value 0: open
+			 * Value 1: closed
+			 */
+			TYPE_OPEN_CLOSE,
+			/**
 			 * Value 0: off
 			 * Value 1: on
 			 */
@@ -67,6 +81,17 @@ public:
 			 * Unit: hPa
 			 */
 			TYPE_PRESSURE,
+			/**
+			 * Value 0: no security alert
+			 * Value 1: security alert is active
+			 *
+			 */
+			TYPE_SECURITY_ALERT,
+			/**
+			 * Value 0: no shake detect
+			 * Value 1: shake detect
+			 */
+			TYPE_SHAKE,
 			/**
 			 * Unit: °C
 			 */

src/model/ModuleType.cpp
@@ -10,6 +10,7 @@ EnumHelper<ModuleType::AttributeEnum::Raw>::ValueMap &ModuleType::AttributeEnum:
 {
 	static EnumHelper<ModuleType::AttributeEnum::Raw>::ValueMap valueMap = {
 		{ModuleType::AttributeEnum::TYPE_INNER, "inner"},
+		{ModuleType::AttributeEnum::TYPE_MANUAL_ONLY, "manual-only"},
 		{ModuleType::AttributeEnum::TYPE_OUTER, "outer"},
 	};
 
@@ -21,11 +22,15 @@ EnumHelper<ModuleType::TypeEnum::Raw>::ValueMap &ModuleType::TypeEnum::valueMap(
 	static EnumHelper<ModuleType::TypeEnum::Raw>::ValueMap valueMap = {
 		{ModuleType::TypeEnum::TYPE_BATTERY, "battery"},
 		{ModuleType::TypeEnum::TYPE_CO2, "co2"},
+		{ModuleType::TypeEnum::TYPE_FIRE, "fire"},
 		{ModuleType::TypeEnum::TYPE_HUMIDITY, "humidity"},
 		{ModuleType::TypeEnum::TYPE_MOTION, "motion"},
 		{ModuleType::TypeEnum::TYPE_NOISE, "noise"},
 		{ModuleType::TypeEnum::TYPE_ON_OFF, "on-off"},
+		{ModuleType::TypeEnum::TYPE_OPEN_CLOSE, "open-close"},
 		{ModuleType::TypeEnum::TYPE_PRESSURE, "pressure"},
+		{ModuleType::TypeEnum::TYPE_SECURITY_ALERT, "security-alert"},
+		{ModuleType::TypeEnum::TYPE_SHAKE, "shake"},
 		{ModuleType::TypeEnum::TYPE_TEMPERATURE, "temperature"},
 	};
 

commit 062f27fbba02aeeed8036da3eb7769a3ed7f8441

ModuleType: fix of changed values
Values that marked open/close were swapped and this description
of values did not correnpond with description on server.

Fixes: c23344e1 ("ModuleType: support types and attributes for Jablotron")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/model/ModuleType.h
@@ -102,8 +102,8 @@ public:
 			 */
 			TYPE_NOISE = 11,
 			/**
-			 * Value 0: open
-			 * Value 1: closed
+			 * Value 0: closed
+			 * Value 1: open
 			 */
 			TYPE_OPEN_CLOSE = 12,
 			/**