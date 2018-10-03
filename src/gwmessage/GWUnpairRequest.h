4777b20623832c5d7eae87407ec4e2c873c70b73
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-10-03 16:12:54 +0200

    GWUnpairRequest: add example message into documentation
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/gwmessage/GWUnpairRequest.h
+++ src/gwmessage/GWUnpairRequest.h
@@ -11,6 +11,15 @@ namespace BeeeOn {
 /**
  * @brief Represents a message sent by the server to the gateway, intended
  * to unpair a paired device from the gateway.
+ *
+ * An example message:
+ * <pre>
+ * {
+ *   "id": "fe2ba7e3-5d93-4c11-bc36-8f0b2f0a7b1a",
+ *   "message_type": "unpair_request",
+ *   "device_id": "0xa300000000000002"
+ * }
+ * </pre>
  */
 class GWUnpairRequest : public GWRequest {
 public:
