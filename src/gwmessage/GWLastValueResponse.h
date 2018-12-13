1efd9f47cc8c0a4bcadb9be12d68d0135fa7e959
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-12-13 14:46:04 +0100

    GWLastValue: mark as deprecated
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/gwmessage/GWLastValueResponse.h
+++ src/gwmessage/GWLastValueResponse.h
@@ -8,7 +8,7 @@
 namespace BeeeOn {
 
 /**
- * @brief Represents a message sent by the server to the gateway as a response
+ * @brief **Deprecated.** Represents a message sent by the server to the gateway as a response
  * to the GWLastValueRequest.
  *
  * The message contains the last known value of a device module.
