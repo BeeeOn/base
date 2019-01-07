BeeeOn/base

commit f498f40c766f1063a13726f378a1acaa69c51212

GWNewDeviceRequest: serialize extended info about device
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Reviewed-by: David Bednařík <xbedna62@stud.fit.vutbr.cz>

src/gwmessage/GWNewDeviceRequest.cpp
@@ -1,7 +1,10 @@
+#include <Poco/NumberParser.h>
+
 #include "gwmessage/GWNewDeviceRequest.h"
 
 using namespace std;
 using namespace Poco;
+using namespace Poco::Net;
 using namespace BeeeOn;
 
 GWNewDeviceRequest::GWNewDeviceRequest():
@@ -76,6 +79,17 @@ void GWNewDeviceRequest::setDeviceDescription(const DeviceDescription &descripti
 	setProductName(description.productName());
 	setModuleTypes(description.dataTypes());
 	setRefreshTime(description.refreshTime());
+
+	if (!description.name().empty())
+		json()->set("name", description.name());
+	if (!description.firmware().empty())
+		json()->set("firmware", description.firmware());
+	if (!description.ipAddress().isNull())
+		json()->set("ip_address", description.ipAddress().value().toString());
+	if (!description.macAddress().isNull())
+		json()->set("mac_address", description.macAddress().value().toString());
+	if (!description.serialNumber().isNull())
+		json()->set("serial_number", to_string(description.serialNumber().value()));
 }
 
 DeviceDescription GWNewDeviceRequest::deviceDescription() const
@@ -92,6 +106,29 @@ DeviceDescription GWNewDeviceRequest::deviceDescription() const
 		builder.disabledRefreshTime();
 	else
 		builder.refreshTime(time * Timespan::SECONDS);
+
+	if (json()->has("name"))
+		builder.name(json()->getValue<string>("name"));
+	if (json()->has("firmware"))
+		builder.firmware(json()->getValue<string>("firmware"));
+
+	if (json()->has("ip_address")) {
+		builder.ipAddress(
+			IPAddress::parse(
+				json()->getValue<string>("ip_address")));
+	}
+
+	if (json()->has("mac_address")) {
+		builder.macAddress(
+			MACAddress::parse(
+				json()->getValue<string>("mac_address")));
+	}
+
+	if (json()->has("serial_number")) {
+		builder.serialNumber(
+			NumberParser::parseUnsigned(
+				json()->getValue<string>("serial_number")));
+	}
 
 	return builder.build();
 }

commit b90f06e112bdd8abf0b82c96bb7a56635b207ee7

GWNewDeviceRequest: GWNewDeviceGroupRequest: parse serial_number as unsigned 64
Serial numbers are defined as 64-bit numbers.

Fixes: f498f40c766f ("GWNewDeviceRequest: serialize extended info about device")
Fixes: f9d2d256acb0 ("GWNewDeviceGroupRequest: support extended info about device")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

src/gwmessage/GWNewDeviceGroupRequest.cpp
@@ -111,7 +111,7 @@ vector<DeviceDescription> GWNewDeviceGroupRequest::deviceDescriptions() const
 
 		if (json()->has("serial_number")) {
 			builder.serialNumber(
-				NumberParser::parseUnsigned(
+				NumberParser::parseUnsigned64(
 					json()->getValue<string>("serial_number")));
 		}
 

src/gwmessage/GWNewDeviceRequest.cpp
@@ -126,7 +126,7 @@ DeviceDescription GWNewDeviceRequest::deviceDescription() const
 
 	if (json()->has("serial_number")) {
 		builder.serialNumber(
-			NumberParser::parseUnsigned(
+			NumberParser::parseUnsigned64(
 				json()->getValue<string>("serial_number")));
 	}
 