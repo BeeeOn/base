3374c13d09482ac40e6439d4d07f0f4158231ebb
Jan Viktorin <iviktorin@fit.vutbr.cz>
2019-01-08 13:00:46 +0100

    GWResponse: describe entry status in doc
    The entry is currently a subject of changes. State it in the documentation.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- src/gwmessage/GWResponse.h
+++ src/gwmessage/GWResponse.h
@@ -17,12 +17,23 @@ class GWAck;
  * The GWResponse contains unique identifier intended to match
  * a response to a proper request.
  *
+ * The entry "status" is currently represented as integer. It is however possible
+ * to use also string representation, any of:
+ *
+ * - "accepted" (0)
+ * - "success" (1)
+ * - "failed" (2)
+ * - "success_partial" (3)
+ *
+ * The string representation should be preferred over numbers. The integer representation
+ * is treated as obsolete.
+ *
  * An example (generic) response:
  * <pre>
  * {
  *   "id": "b036b7f7-47a1-4bea-9fa3-6024e8263dcd",
  *   "message_type": "generic_response",
- *   "status": 1
+ *   "status": "success"
  * }
  * </pre>
  */
